3d36f58f939972b0bd02b15d1c3447bc921efb94
Kevin Pouget <kpouget@redhat.com>
2021-02-03 14:50:06 +0100

    toolbox: deploy_nfd_from_operatorhub.sh: new script

---
+++ toolbox/deploy_nfd_from_operatorhub.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${THIS_DIR}/_common.sh
+
+ANSIBLE_OPTS="${ANSIBLE_OPTS} -e install_nfd_operator_from_hub=yes"
+ANSIBLE_OPTS="${ANSIBLE_OPTS} -e install_gpu_operator_from_hub=no"
+ANSIBLE_OPTS="${ANSIBLE_OPTS} -e user_mode=not-ci"
+
+exec ansible-playbook ${INVENTORY_ARG} ${ANSIBLE_OPTS} playbooks/deploy-gpu-operator-from-operatorhub.yml
