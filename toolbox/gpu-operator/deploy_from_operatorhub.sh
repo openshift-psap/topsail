71af35db40fb052df1de637ddd71fe7c9115490e
Kevin Pouget <kpouget@redhat.com>
2021-02-05 11:30:15 +0100

    toolbox/gpu-operator/deploy_from_operatorhub.sh: fix path to _common.sh

--- toolbox/gpu-operator/deploy_from_operatorhub.sh
+++ toolbox/gpu-operator/deploy_from_operatorhub.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ${THIS_DIR}/_common.sh
+source ${THIS_DIR}/../_common.sh
 
 ANSIBLE_OPTS="${ANSIBLE_OPTS} -e install_nfd_operator_from_hub=no"
 ANSIBLE_OPTS="${ANSIBLE_OPTS} -e install_gpu_operator_from_hub=yes"
