df8269d1bdffee105250ff797d918ee942c77556
Kevin Pouget <kpouget@redhat.com>
2021-02-11 09:27:09 +0100

    toolbox: _common.sh: properly set openshift_release ansible variable

--- toolbox/_common.sh
+++ toolbox/_common.sh
@@ -4,9 +4,17 @@ TOP_DIR=${TOOLBOX_DIR}/..
 set -o pipefail
 set -o errexit
 set -o nounset
-set -x
 
 ANSIBLE_OPTS="${ANSIBLE_OPTS:--vvv}"
 INVENTORY_ARG="-i inventory/hosts"
 
+if [ -z "${OCP_VERSION:-}" ]; then
+    echo "Getting OpenShift version ..."
+    OCP_VERSION="$(oc version -o json | jq --raw-output '.openshiftVersion' | cut -b-3)"
+    echo "Getting OpenShift version ==> $OCP_VERSION"
+fi
+ANSIBLE_OPTS="$ANSIBLE_OPTS -e openshift_release=$OCP_VERSION"
+
 cd $TOP_DIR
+
+set -x
