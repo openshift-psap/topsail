85e48a03cc9acf6accb21a601b1a4bced0b75a4a
Kevin Pouget <kpouget@redhat.com>
2021-02-19 11:41:57 +0100

    roles/openshift_node: make more generic (remove GPU-specific aspect)
    > toolbox/scaleup_cluster.sh [new machine type]
    
    This command scales up the cluster using the optional machine type
    passed in parameter (the default value is controlled via
    `machine_instance_type` in `group_var/all.yml`).
    
    By "scaling up" here, we mean: ensure that a Machine with the given
    machine type is available in cluster.
    
    The name of the generated `MachineSet` is also updated to include the
    machine type name, so that the cluster can easily be scaled-up with
    different machine types.

--- roles/openshift_node/files/aws_machineset_json.sh
+++ roles/openshift_node/files/aws_machineset_json.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-#
-#Store the machine set name
+
 json_input=$(cat ${1})
 instance_type=${2}
-machinesetname=$(echo ${json_input} |jq -c '.metadata.name' |sed -e 's/worker/gpu/' |sed -e 's/^"//' -e 's/"$//' )
+
+# Compute the machine set name
+machinesetname=$(echo ${json_input} \
+    | jq -c '.metadata.name' \
+    | sed -e "s/worker/$(echo ${instance_type} | tr . -)/" \
+    | sed -e 's/^"//' -e 's/"$//')
 
 # Change the values for instance type and machine set name
-# clean the status key=value
-echo ${json_input} |jq --arg instance_type "${instance_type}" '.spec.template.spec.providerSpec.value.instanceType = $instance_type' \
-	|jq --arg machinesetname "${machinesetname}" '.metadata.name = $machinesetname' \
-	|jq --arg machinesetname "${machinesetname}" '.spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset" = $machinesetname' \
-	|jq -c --arg machinesetname "${machinesetname}" '.spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset" = $machinesetname' \
-	|jq -c 'del(.status)|del(.metadata.selfLink)|del(.metadata.uid)' \
-	|jq '.spec.replicas = 1' 
+# Clean the status key=value
+echo ${json_input} \
+    | jq --arg instance_type "${instance_type}" '.spec.template.spec.providerSpec.value.instanceType = $instance_type' \
+    | jq --arg machinesetname "${machinesetname}" '.metadata.name = $machinesetname' \
+    | jq --arg machinesetname "${machinesetname}" '.spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset" = $machinesetname' \
+    | jq -c --arg machinesetname "${machinesetname}" '.spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset" = $machinesetname' \
+    | jq -c 'del(.status)|del(.metadata.selfLink)|del(.metadata.uid)' \
+    | jq '.spec.replicas = 1'
--- toolbox/scaleup_gpu_node.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -e
-
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ${THIS_DIR}/_common.sh
-
-exec ansible-playbook ${INVENTORY_ARG} ${ANSIBLE_OPTS} playbooks/scaleup-gpu-node.yml
