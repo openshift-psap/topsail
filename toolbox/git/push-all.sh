b0a162327f1ba4ea683a7f30dcdb12181752fed8
Kevin Pouget <kpouget@redhat.com>
2021-02-19 15:33:51 +0100

    .github/workflows/sync-release-branches.yml: pull with --ff-only

--- toolbox/git/push-all.sh
+++ toolbox/git/push-all.sh
@@ -5,7 +5,7 @@ BASE_BRANCH=master
 for branch in $(git branch | grep release-)
 do
     git checkout $branch
-    git pull . $BASE_BRANCH
+    git pull . $BASE_BRANCH --ff-only
     git push
 done
 git checkout master
