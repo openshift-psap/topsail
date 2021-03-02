d83d99663ce2497cdff796d08ffec11db94a9dc8
Kevin Pouget <kpouget@redhat.com>
2021-03-02 18:30:31 +0100

    roles/nv_gpu: allow installing an older version of the GPU Operator from OperatorHub

--- toolbox/gpu-operator/deploy_from_operatorhub.sh
+++ toolbox/gpu-operator/deploy_from_operatorhub.sh
@@ -8,4 +8,15 @@ ANSIBLE_OPTS="${ANSIBLE_OPTS} -e install_gpu_operator_from_hub=yes"
 ANSIBLE_OPTS="${ANSIBLE_OPTS} -e undeploy_gpu_operator=no"
 ANSIBLE_OPTS="${ANSIBLE_OPTS} -e user_mode=not-ci"
 
+if [ "$#" -gt 1 ]; then
+    echo "FATAL: expected 0 or 1 parameter ... (got '$@')"
+    echo "Usage: $0 [gpu_operator_operatorhub_version]"
+    exit 1
+elif [ "$#" -eq 1 ]; then
+    ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_operator_operatorhub_version=$1"
+    echo "Deploying the GPU Operator from OperatorHub using version '$1'."
+else
+    echo "Deploying the GPU Operator from OperatorHub using the latest version available."
+fi
+
 exec ansible-playbook ${INVENTORY_ARG} ${ANSIBLE_OPTS} playbooks/deploy-gpu-operator-from-operatorhub.yml
