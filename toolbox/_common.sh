8dac6d15de1938b68db946013bae7f8a7276541c
Kevin Pouget <kpouget@redhat.com>
2021-03-11 09:12:51 +0100

    Do not set fact_caching_connection from 'ansible.cfg'

--- toolbox/_common.sh
+++ toolbox/_common.sh
@@ -12,4 +12,7 @@ if [ ! -z "${OCP_VERSION:-}" ]; then
     ANSIBLE_OPTS="$ANSIBLE_OPTS -e openshift_release=$OCP_VERSION"
 fi
 
+export ANSIBLE_CACHE_PLUGIN_CONNECTION="${ANSIBLE_CACHE_PLUGIN_CONNECTION:-/tmp/ansible/facts}"
+echo "Using ${ANSIBLE_CACHE_PLUGIN_CONNECTION} for storing ansible facts."
+
 cd $TOP_DIR
