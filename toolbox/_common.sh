0159a46e4c5ffb3b951cf896778570fb70cee00c
Kevin Pouget <kpouget@redhat.com>
2021-03-15 11:37:55 +0100

    Prepare artifact directory in toolbox/_common.sh
    This patches changes the way CI `run` script and the `toolbox` store
    their file artifacts: now, the `toolbox` scripts create a subdirectory
    inside `$ARTIFACTS_DIR` (or `/tmp/ci-artifacts_$(date +%Y%m%d)` if
    empty).
    
    The name of this `toolbox` script directory is
    `<HMS>_<script_path>__<script_name>`.
    
    Ansible logs and facts will be stored in this directory, and
    `artifact_extra_logs_dir` points directly to it. This makes it easier
    to understand what happened during every step of the deployment and
    testing.

--- toolbox/_common.sh
+++ toolbox/_common.sh
@@ -7,11 +7,47 @@ set -o nounset
 
 ANSIBLE_OPTS="${ANSIBLE_OPTS:--vv}"
 
+### OpenShift version
+
 if [ ! -z "${OCP_VERSION:-}" ]; then
     ANSIBLE_OPTS="$ANSIBLE_OPTS -e openshift_release=$OCP_VERSION"
 fi
 
-export ANSIBLE_CACHE_PLUGIN_CONNECTION="${ANSIBLE_CACHE_PLUGIN_CONNECTION:-/tmp/ansible/facts}"
-echo "Using ${ANSIBLE_CACHE_PLUGIN_CONNECTION} for storing ansible facts."
+### Artifacts directory
+
+if [ -z "${ARTIFACT_DIR:-}" ]; then
+    export ARTIFACT_DIR="/tmp/ci-artifacts_$(date +%Y%m%d)"
+    echo "No ARTIFACT_DIR configured, using $ARTIFACT_DIR to store the test artifacts"
+else
+    echo "Using '$ARTIFACT_DIR' to store the test artifacts"
+fi
+
+
+TOOLBOX_PATH="${0##*toolbox/}"
+ARTIFACT_DIRNAME="${TOOLBOX_PATH/\//__}"
+ARTIFACT_EXTRA_LOGS_DIR="${ARTIFACT_DIR}/$(date +%H%M%S)_${ARTIFACT_DIRNAME}"
+export ARTIFACT_EXTRA_LOGS_DIR
+
+mkdir -p "${ARTIFACT_EXTRA_LOGS_DIR}"
+echo "Using '${ARTIFACT_EXTRA_LOGS_DIR}' to store extra log files."
+ANSIBLE_OPTS="$ANSIBLE_OPTS -e artifact_extra_logs_dir=${ARTIFACT_EXTRA_LOGS_DIR}"
+
+### Ansible logs  directory
+
+if [ -z "${ANSIBLE_LOG_PATH:-}" ]; then
+    export ANSIBLE_LOG_PATH="${ARTIFACT_EXTRA_LOGS_DIR}/ansible_logs"
+fi
+echo "Using '${ANSIBLE_LOG_PATH}' to store ansible logs."
+mkdir -p "$(dirname "${ANSIBLE_LOG_PATH}")"
+
+# Ansible caching directory
+
+if [ -z "${ANSIBLE_CACHE_PLUGIN_CONNECTION:-}" ]; then
+    export ANSIBLE_CACHE_PLUGIN_CONNECTION="${ARTIFACT_EXTRA_LOGS_DIR}/ansible_facts"
+fi
+echo "Using '${ANSIBLE_CACHE_PLUGIN_CONNECTION}' to store ansible facts."
+mkdir -p "${ANSIBLE_CACHE_PLUGIN_CONNECTION}"
+
+###
 
 cd $TOP_DIR
