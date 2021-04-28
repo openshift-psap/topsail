6460fc89aaee6e5b0648a0d08a1dedfa51df39a0
Kevin Pouget <kpouget@redhat.com>
2021-04-28 14:25:16 +0200

    gpu_operator_set_repo-config: allow deploying RHEL beta repo

--- toolbox/gpu-operator/set_repo-config.sh
+++ toolbox/gpu-operator/set_repo-config.sh
@@ -2,14 +2,23 @@
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+RHEL_FLAG="--rhel-beta"
+
 usage() {
-    echo "Usage: $0 <repo file abs path>"
+    echo "Usage: $0 <repo file abs path|${RHEL_FLAG}>"
 }
 
+REPOFILE_USE_RHEL="false"
 REPOFILE_FILENAME=""
 
 if [ "$#" -gt 1 ]; then
     echo "FATAL: expected 1 parameter ... (got $#: '$@')"
+    usage
+    exit 1
+elif [[ "$1" == "${RHEL_FLAG}" ]]; then
+    REPOFILE_USE_RHEL="true"
+elif [[ "$1" == "--"* && "$1" != "${RHEL_FLAG}" ]]; then
+    echo "FATAL: only ${RHEL_FLAG} flag is allowed"
     usage
     exit 1
 elif [[ ! -e "$1" ]]; then
@@ -23,5 +32,6 @@ fi
 source ${THIS_DIR}/../_common.sh
 
 ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_operator_set_repo_filename=${REPOFILE_FILENAME}"
+ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_operator_set_repo_use_rhel_beta=${REPOFILE_USE_RHEL}"
 
 exec ansible-playbook ${ANSIBLE_OPTS} playbooks/gpu_operator_set_repo-config.yml
