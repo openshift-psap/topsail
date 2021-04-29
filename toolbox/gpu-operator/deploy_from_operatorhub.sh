6255ff9be28158a52bf0b41346ce08bf4ba40e82
Kevin Pouget <kpouget@redhat.com>
2021-04-29 16:15:30 +0200

    gpu_operator_deploy_from_operatorhub: allow deployment of the master bundle

--- toolbox/gpu-operator/deploy_from_operatorhub.sh
+++ toolbox/gpu-operator/deploy_from_operatorhub.sh
@@ -3,16 +3,40 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${THIS_DIR}/../_common.sh
 
-ANSIBLE_OPTS="${ANSIBLE_OPTS} -e install_nfd_operator_from_hub=no"
-ANSIBLE_OPTS="${ANSIBLE_OPTS} -e install_gpu_operator_from_hub=yes"
-ANSIBLE_OPTS="${ANSIBLE_OPTS} -e undeploy_gpu_operator=no"
-ANSIBLE_OPTS="${ANSIBLE_OPTS} -e user_mode=not-ci"
+DEPLOY_FROM_BUNDLE_FLAG="--from-bundle=master"
+
+usage() {
+    cat <<EOF
+Usage: $0 [FLAG]
+Deploys the GPU Operator from OperatorHub / OLM
+
+Flags:
+  -h, --help           Display this help message
+
+  $DEPLOY_FROM_BUNDLE_FLAG Deploy the current master-branch version from the bundle image
+                       See roles/gpu_operator_deploy_from_operatorhub/defaults/main/bundle.yml for the image path.
+
+  <empty>              Deploy the latest version available in OperatorHub
+
+  <version>            Deploy a given version from OperatorHub
+                       See toolbox/gpu-operator/list_version_from_operator_hub.sh for the version available
+EOF
+}
+
+if [[ "${1:-}" == "--help" || "${1:-}" == -h ]]; then
+    usage
+    exit 0
+fi
 
 if [ "$#" -gt 1 ]; then
     echo "FATAL: expected 0 or 1 parameter ... (got '$@')"
-    echo "Usage: $0 [gpu_operator_operatorhub_version]"
+    usage
     exit 1
-elif [ "$#" -eq 1 ]; then
+elif [[ "${1:-}" == "$DEPLOY_FROM_BUNDLE_FLAG" ]]; then
+    echo "Deploying the GPU Operator from OperatorHub using its master bundle."
+    ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_operator_deploy_from=bundle"
+
+elif [[ "$#" == 1 ]]; then
     ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_operator_operatorhub_version=$1"
     echo "Deploying the GPU Operator from OperatorHub using version '$1'."
 else
