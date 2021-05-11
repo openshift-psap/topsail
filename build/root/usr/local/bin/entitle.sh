d40ada1786c28e488c69c21d7f2ffbabb52cddce
Kevin Pouget <kpouget@redhat.com>
2021-05-11 20:14:44 +0200

    build/root/usr/local/bin/entitle.sh: update to use the new CI vault secrets

--- build/root/usr/local/bin/entitle.sh
+++ build/root/usr/local/bin/entitle.sh
@@ -28,8 +28,10 @@ if toolbox/entitlement/test.sh --no-inspect; then
     exit 0
 fi
 
-ENTITLEMENT_PEM=${ENTITLEMENT_PEM:-}
-ENTITLEMENT_RESOURCES=${ENTITLEMENT_RESOURCES:-/var/run/psap-entitlement-secret/01-cluster-wide-machineconfigs.yaml}
+ENTITLEMENT_SECRET_PATH=/var/run/psap-entitlement-secret
+ENTITLEMENT_PEM=${ENTITLEMENT_PEM:-${ENTITLEMENT_SECRET_PATH}/entitlement.pem}
+ENTITLEMENT_RESOURCES=${ENTITLEMENT_RESOURCES:-${ENTITLEMENT_SECRET_PATH}/01-cluster-wide-machineconfigs.yaml}
+ENTITLEMENT_REPO_CA=${ENTITLEMENT_REPO_CA:-${ENTITLEMENT_SECRET_PATH}/ops-mirror.pem}
 
 if [[ -e "$ENTITLEMENT_RESOURCES" && ! -e "$ENTITLEMENT_PEM" ]]; then
     echo "INFO: found entitlement resource file but no entitlement key."
@@ -42,14 +44,14 @@ if [[ -e "$ENTITLEMENT_RESOURCES" && ! -e "$ENTITLEMENT_PEM" ]]; then
     extract_entitlement_key $ENTITLEMENT_RESOURCES $ENTITLEMENT_PEM
 fi
 
-REPO_CA=""
+REPO_CA_FLAG=""
 if oc version | grep -q "Server Version: 4.8"; then
-     # Random CA file until we have the right certificate stored as a secret
-     RHEL_BETA_REPO_CA=/etc/rhsm/ca/redhat-uep.pem
-     ENTITLEMENT_REPO_CA=${ENTITLEMENT_REPO_CA:-$RHEL_BETA_REPO_CA}
-     echo "INFO: Using $ENTITLEMENT_REPO_CA as RHEL-beta repo CA"
-
-     REPO_CA="--ca $ENTITLEMENT_REPO_CA"
+    if [ ! -e "$ENTITLEMENT_REPO_CA" ]; then
+        echo "WARNING: Running with OCP 4.8 and RHEL-beta repo CA missing..."
+    else
+        echo "INFO: Using $ENTITLEMENT_REPO_CA as RHEL-beta repo CA"
+        REPO_CA_FLAG="--ca $ENTITLEMENT_REPO_CA"
+    fi
 fi
 
 if [ ! -e "$ENTITLEMENT_PEM" ]; then
@@ -63,7 +65,7 @@ if [ ! -e "$ENTITLEMENT_PEM" ]; then
 fi
 
 echo "Deploying the entitlement with PEM key from ${ENTITLEMENT_PEM}"
-toolbox/entitlement/deploy.sh --pem ${ENTITLEMENT_PEM} ${REPO_CA}
+toolbox/entitlement/deploy.sh --pem "${ENTITLEMENT_PEM}" ${REPO_CA_FLAG}
 
 if ! toolbox/entitlement/wait.sh; then
     echo "FATAL: Failed to properly entitle the cluster, cannot continue."
