3cd3a6349d2cd0d9968db4b5a1fd05952c2b9a42
Omer Tuchfeld <otuchfel@redhat.com>
2021-06-02 12:38:54 +0300

    Support entitlement on no-workers clusters
    On no-worker clusters, we want to target the masters with our machine
    configs rather than the workers, because there are no workers.
    
    We check that by looking at the Scheduler CR `.spec.mastersSchedulable`
    stanza

--- roles/entitlement_deploy/files/apply_template.py
+++ roles/entitlement_deploy/files/apply_template.py
@@ -1,19 +1,33 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 
-import fileinput
 import base64
 import sys
 
-template = sys.argv[1]
-keyname = sys.argv[2]
-filename = sys.argv[3]
-print(f"Replacing '{keyname}' with the content of '{filename}'", file=sys.stderr)
 
-with open(filename, "rb") as f:
-    file_b64 = base64.b64encode(f.read()).decode()
+def main():
+    keyname = sys.argv[1]
+    value = sys.argv[2]
 
-found = False
-for line in fileinput.input(files=[template]):
-    if keyname in line: found = True
-    print(line.replace(keyname, file_b64), end="")
-exit(0 if found else 1)
+    if value.startswith("@"):
+        filename = value[1:]  # Skip @
+        print(
+            f"Replacing '{keyname}' with the base64 of the content of '{filename}'",
+            file=sys.stderr,
+        )
+
+        with open(filename, "rb") as f:
+            value = base64.b64encode(f.read()).decode()
+    else:
+        print(f"Replacing '{keyname}' with '{value}'", file=sys.stderr)
+
+    found = False
+    for line in sys.stdin:
+        if keyname in line:
+            found = True
+        print(line.replace(keyname, value), end="")
+
+    exit(0 if found else 1)
+
+
+if __name__ == "__main__":
+    main()
