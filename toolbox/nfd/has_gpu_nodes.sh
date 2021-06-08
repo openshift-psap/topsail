d2c483eaefb49a5d4b2b87453d37d8aeb8d3ba36
Carlos Eduardo Arango Gutierrez <carangog@redhat.com>
2021-06-08 14:27:39 -0500

    Create nfd test master branch route
    Signed-off-by: Carlos Eduardo Arango Gutierrez <carangog@redhat.com>

--- toolbox/nfd/has_gpu_nodes.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -e
-
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ${THIS_DIR}/../_common.sh
-
-exec ansible-playbook ${ANSIBLE_OPTS} playbooks/nfd_test_gpu.yml
