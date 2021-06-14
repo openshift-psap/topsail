0a1e85c903be6d98b976e4455bcdd63a8de970a6
Kevin Pouget <kpouget@redhat.com>
2021-06-14 15:32:21 +0200

    gpu_operator_undeploy_custom_commit: transform into gpu_operator_cleanup_bundle_from_commit

--- toolbox/gpu-operator/cleanup_bundle_from_commit.sh
+++ toolbox/gpu-operator/cleanup_bundle_from_commit.sh
@@ -3,4 +3,4 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${THIS_DIR}/../_common.sh
 
-exec ansible-playbook ${ANSIBLE_OPTS} playbooks/gpu_operator_undeploy_custom_commit.yml
+exec ansible-playbook ${ANSIBLE_OPTS} playbooks/gpu_operator_cleanup_bundle_from_commit.yml
