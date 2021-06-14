cdba78626cc204e3c234fd0280f36baa8d5800cf
Kevin Pouget <kpouget@redhat.com>
2021-06-14 15:33:24 +0200

    toolbox: gpu-operator/deploy_from_operatorhub.sh: update to deploy any GPU Operator bundle image

--- toolbox/gpu-operator/deploy_from_operatorhub.sh
+++ toolbox/gpu-operator/deploy_from_operatorhub.sh
@@ -3,7 +3,8 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${THIS_DIR}/../_common.sh
 
-DEPLOY_FROM_BUNDLE_FLAG="--from-bundle=master"
+DEPLOY_FROM_BUNDLE_FLAG="--from-bundle="
+DEPLOY_ONLY_CLUSTERPOLICY_FLAG="--only-clusterpolicy"
 
 usage() {
     cat <<EOF
@@ -13,13 +14,15 @@ Usage:
     $0
     $0 <version> [<channel>]
     $0 $DEPLOY_FROM_BUNDLE_FLAG
+    $0 $DEPLOY_ONLY_CLUSTERPOLICY_FLAG
 
 Flags:
   -h, --help           Display this help message
 
-  $DEPLOY_FROM_BUNDLE_FLAG Deploy the current master-branch version from the bundle image
+  ${DEPLOY_FROM_BUNDLE_FLAG}master Deploy the current master-branch version from the bundle image
                        See roles/gpu_operator_deploy_from_operatorhub/defaults/main/bundle.yml for the image path.
-
+  ${DEPLOY_FROM_BUNDLE_FLAG}<path> Deploy the from the given bundle image
+  $DEPLOY_ONLY_CLUSTERPOLICY_FLAG Do not deploy the operator itself, only create the ClusterPolicy from the CSV.
   <empty>              Deploy the latest version available in OperatorHub
 
   <version>            Deploy a given version from OperatorHub
@@ -34,16 +37,28 @@ if [[ "${1:-}" == "--help" || "${1:-}" == -h ]]; then
     exit 0
 fi
 
-if [[ "${1:-}" == "$DEPLOY_FROM_BUNDLE_FLAG" ]]; then
-    echo "Deploying the GPU Operator from OperatorHub using its master bundle."
+if [ "$#" -gt 1 ]; then
+    echo "FATAL: expected 0 or 1 parameter ... (got '$@')"
+    usage
+    exit 1
+elif [[ "${1:-}" == "$DEPLOY_FROM_BUNDLE_FLAG"* ]]; then
     ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_operator_deploy_from=bundle"
 
-    if [ "$#" -gt 1 ]; then
-        shift
-        echo "FATAL: $DEPLOY_FROM_BUNDLE_FLAG expects no additional parameter (got '$@')"
-        usage
-        exit 1
+    bundle=${1##$DEPLOY_FROM_BUNDLE_FLAG}
+    if [[ "$bundle" == "master" ]]; then
+        echo "Deploying the GPU Operator from OperatorHub using its master bundle."
+    else
+        echo "Deploying the GPU Operator from OperatorHub using bundle at $bundle."
+        ANSIBLE_OPTS="${ANSIBLE_OPTS} -e deploy_bundle_image=$bundle"
     fi
+
+elif [[ "${1:-}" == "$DEPLOY_ONLY_CLUSTERPOLICY_FLAG" ]]; then
+    ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_operator_deploy_from=pre-deployed"
+    echo "Not deploying the GPU Operator, only create the ClusterPolicy from the CSV."
+elif [[ "${1:-}" == "-"* ]]; then
+    echo "FATAL: unexpected parameters ... ($@)"
+    usage
+    exit 1
 elif [[ "$#" == 0 ]]; then
     echo "Deploying the GPU Operator from OperatorHub using the latest version available."
 elif [[ "$#" == 1 || "$#" == 2 ]]; then
