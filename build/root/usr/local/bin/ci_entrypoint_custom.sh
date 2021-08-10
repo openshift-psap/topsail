6e85f6339190d82431badd7249a66b69b2b3fb0d
Omer Tuchfeld <otuchfel@redhat.com>
2021-08-10 11:30:02 +0200

    Added a '/test custom' hook to the repo

---
+++ build/root/usr/local/bin/ci_entrypoint_custom.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+set -o pipefail
+set -o errexit
+set -o nounset
+
+# This file intentionally left blank.
+
+# This file will run when you comment "/test custom" on your PR.
+# Feel free to modify this file to run whatever you want in order to test your PR
+# Please restore the file to this original state once you're done
