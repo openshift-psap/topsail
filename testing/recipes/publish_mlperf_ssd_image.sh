6ad7736d29adcd85acf73f45893a9358fac2898f
maxusmusti <meyceoz@redhat.com>
2021-12-08 00:17:25 -0500

    Updated recipe
    test-path: recipe publish_mlperf_ssd_image

--- testing/recipes/publish_mlperf_ssd_image.sh
+++ testing/recipes/publish_mlperf_ssd_image.sh
@@ -4,9 +4,9 @@ local_name="ssd"
 tag="latest"
 
 repo="https://github.com/openshift-psap/training_results_v0.7.git"
-path="training_results_v0.7/NVIDIA/benchmarks/ssd/implementations/pytorch"
+path="/NVIDIA/benchmarks/ssd/implementations/pytorch"
 quay="openshift-psap/nvidiadl-ssd-training-benchmark"
-auth="UNKNOWN"
+auth="/var/run/psap-entitlement-secret/openshift-psap-openshift-ci-secret.yml"
 
 ./run_toolbox.py gpu_operator build_push_image $local_name $tag \
     --git_repo=$repo \
