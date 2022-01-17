620840766ab05658111387bf40908b0034f5a3ed
maxusmusti <meyceoz@redhat.com>
2022-01-17 04:08:23 -0500

    Applied code review feedback
    test-path: recipes publish_mlperf_ssd_image

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -31,7 +31,7 @@ class Utils:
             dockerfile - Path/Name of Dockerfile if used as source
             git_repo - Git repo containing Dockerfile if used as source
             git_path - Path to Dockerfile in git repo
-            branch - Branch of repo to clone
+            branch - Branch of repo to clone (default='main')
             memory - Required memory to build the image (in Gb)
             quay_org_repo - Org/Repo in quay to push to
             auth_file - Auth file for quay
@@ -45,8 +45,20 @@ class Utils:
                 memory = str(float(memory)) + "Gi"
             except ValueError:
                 print("ERROR: memory must be of type float or int")
+                exit(1)
         else:
             memory = "10Mi"
+
+        if not git_repo and not dockerfile:
+            print("Either a git repo or Dockerfile is required")
+            exit(1)
+        elif git_repo and dockerfile:
+            print("Cannot have both a git repo and Dockerfile")
+            exit(1)
+
+        if git_repo and not git_path:
+            print("Must supply a git path to Dockerfile within git repo")
+            exit(1)
 
         opts = {
             "local_image_name": image_name,
@@ -54,7 +66,7 @@ class Utils:
             "docker_path": dockerfile if dockerfile else "",
             "git_repo": git_repo if git_repo else "",
             "git_path": git_path if git_path else "",
-            "branch": branch if branch else "",
+            "branch": branch if branch else "main",
             "memory": memory,
             "quay_repo": quay_org_repo if quay_org_repo else "",
             "auth_file": auth_file if auth_file else ""
