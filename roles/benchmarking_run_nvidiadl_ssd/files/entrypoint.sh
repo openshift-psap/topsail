f1904ec92c4538baf95fc6e77aa436f2f64d1eec
maxusmusti <meyceoz@redhat.com>
2022-01-18 23:38:13 -0500

    Updated entrypoint command
    test-path: recipes run_cocodataset_nvidiadl_ssd

--- roles/benchmarking_run_nvidiadl_ssd/files/entrypoint.sh
+++ roles/benchmarking_run_nvidiadl_ssd/files/entrypoint.sh
@@ -2,9 +2,10 @@
 
 ls /storage
 
-exec python -m torch.distributed.launch --nproc_per_node=1 \
-        main.py --batch-size 32 \
-          --mode benchmark-training \
-          --benchmark-warmup 100 \
-          --benchmark-iterations 100 \
-          --data /storage
+exec python -u -m bind_launch --nproc_per_node=1 \
+     train.py --epochs 80 \
+              --warmup-factor 0 \
+              --threshold=0.23 \
+              --data /storage \
+              --batch-size=32 \
+              --warmup=100
