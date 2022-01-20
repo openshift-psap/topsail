7ff1def7c62abed9ba36b4cb2c3b2d6e79779d8c
maxusmusti <meyceoz@redhat.com>
2022-01-19 23:01:51 -0500

    Significantly reduce benchmark run time
    test-path: recipes run_cocodataset_nvidiadl_ssd

--- roles/benchmarking_run_nvidiadl_ssd/files/entrypoint.sh
+++ roles/benchmarking_run_nvidiadl_ssd/files/entrypoint.sh
@@ -3,9 +3,9 @@
 ls /storage
 
 exec python -u -m bind_launch --nsockets_per_node=1 --ncores_per_socket=4 --nproc_per_node=4 \
-     train.py --epochs 100 \
+     train.py --epochs 2 \
               --warmup-factor 0 \
-              --threshold=0.23 \
+              --threshold=0.25 \
               --data /storage \
               --batch-size=32 \
-              --warmup=100
+              --warmup=2
