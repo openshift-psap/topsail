97a86e12aced945a10455ca0f6bc64a04d943665
Kevin Pouget <kpouget@redhat.com>
2022-02-09 09:50:30 +0100

    testing/mpi: add and run rootless/mpijob-osu.yaml

--- testing/mpi/rootless/run.sh
+++ testing/mpi/rootless/run.sh
@@ -10,4 +10,6 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Nothing to do to prepare rootless."
 
 cd ${THIS_DIR}/..
-exec bash ./wait_and_collect.sh rootless
+bash ./wait_and_collect.sh rootless rootless/mpijob.yaml
+
+bash ./wait_and_collect.sh rootless rootless/mpijob-osu.yaml
