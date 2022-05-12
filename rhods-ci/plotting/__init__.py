9fac5351c4f3d378e618dd6a8e66e5eca6bfd62d
Kevin Pouget <kpouget@redhat.com>
2022-05-12 18:32:14 +0200

    Update plotting/store with CI-generated filenames

--- rhods-ci/plotting/__init__.py
+++ rhods-ci/plotting/__init__.py
@@ -22,27 +22,53 @@ class Plot():
     def do_plot(self, ordered_vars, params, param_lists, variables, cfg):
 
         all_XY = defaultdict(dict)
+        info = defaultdict(dict)
+        user_count = 0
 
         for entry in common.Matrix.all_records(params, param_lists):
             for podname, podtime in sorted(entry.results.pod_time.items()):
+                user_count += 1
+
                 idx = podname.split("-")[2]
-                name = f"Pod #{int(idx):02d}"
+                name = f"User #{idx}"
 
-                all_XY["0. pod creation"][name] = (podtime.creation_time - entry.results.job_creation_time).seconds
+                all_XY["00. Test pod creation"][name] = (podtime.creation_time - entry.results.job_creation_time).seconds / 60
 
-                all_XY["1. pod scheduling"][name] = (podtime.start_time - podtime.creation_time).seconds
-                all_XY["2. container initialization"][name] = (podtime.container_started - podtime.start_time).seconds
-                all_XY["3. container execution"][name] = (podtime.container_finished - podtime.container_started).seconds
+                all_XY["01. Test pod scheduling"][name] = (podtime.start_time - podtime.creation_time).seconds / 60
+                all_XY["02. Test initialization"][name] = (podtime.container_started - podtime.start_time).seconds / 60
+                #all_XY["03. Test execution"][name] = (podtime.container_finished - podtime.container_started).seconds / 60
 
-                pass
-        all_XY["-- job execution"]["Job"] = (entry.results.job_completion_time - entry.results.job_creation_time).seconds
+                info[name]["test_container_started"] = podtime.container_started
+                info[name]["test_container_finished"] = podtime.container_finished
+
+        for entry in common.Matrix.all_records(params, param_lists):
+            for notebook_name, notebook_time in sorted(entry.results.notebook_time.items()):
+                name = f"User #{notebook_name.split('-')[2]}".replace("testuser", "")
+
+                all_XY["03. ODS-CI Test initialization"][name] = (notebook_time.creation_time - info[name]["test_container_started"]).seconds / 60
+
+                all_XY["10. Notebook scheduling"][name] = (notebook_time.creation_time - entry.results.notebook_start_time).seconds / 60
+                all_XY["11. Notebook preparation"][name] = (notebook_time.pulling - notebook_time.creation_time).seconds / 60
+                all_XY["12. Notebook image pull"][name] = (notebook_time.pulled - notebook_time.pulling).seconds / 60
+                all_XY["13. Notebook initialization"][name] = (notebook_time.started - notebook_time.pulled).seconds / 60
+                all_XY["14. Notebook execution"][name] = (notebook_time.terminated - notebook_time.started).seconds / 60
+                all_XY["20. Test termination"][name] = (info[name]["test_container_finished"] - notebook_time.terminated).seconds / 60
+
+        all_XY["-- job execution"]["Job"] = (entry.results.job_completion_time - entry.results.job_creation_time).seconds / 60
         data = []
         for legend_name, XY in sorted(all_XY.items()):
+            opacity = 0.9
+            if legend_name.startswith("0") or legend_name.startswith("--"):
+                opacity = 0.3
             data += [go.Bar(name=legend_name,
                             y=list(XY.keys()), x=list(XY.values()),
+                            opacity=opacity,
                             hoverlabel={'namelength' :-1},
                             orientation="h")]
 
         fig = go.Figure(data=data)
-        fig.update_layout(barmode='stack', title="Execution timeline of 100 Pods doing a sleep(60s)", title_x=0.5,)
+
+        fig.update_layout(barmode='stack', title=f"Execution timeline of {user_count} users launching a notebook ", title_x=0.5,)
+        fig.update_layout(yaxis_title="Notebook username")
+        fig.update_layout(xaxis_title="Timeline (in minutes)")
         return fig, ""
--- rhods-ci/store.py
+++ rhods-ci/store.py
@@ -2,29 +2,69 @@ import types
 import pathlib
 import yaml
 import datetime
+from collections import defaultdict
 
 import matrix_benchmarking.store as store
 import matrix_benchmarking.common as common
+import matrix_benchmarking.cli_args as cli_args
 
-FMT = "%Y-%m-%dT%H:%M:%SZ"
+EVT_TIME_FMT = "%Y-%m-%dT%H:%M:%S.%fZ"
+TIME_FMT = "%Y-%m-%dT%H:%M:%SZ"
+
+def parse_notebook_events(results, dirname):
+    results.notebook_time = defaultdict(types.SimpleNamespace)
+    results.notebook_start_time = datetime.datetime.now()
+
+    with open(dirname / "notebook_events.yaml") as f:
+        events = yaml.safe_load(f)
+
+    for ev in events["items"]:
+        if ev["involvedObject"]["kind"] != "Pod": continue
+        if ev["involvedObject"]["namespace"] != "rhods-notebooks": continue
+
+        podname = ev["involvedObject"]["name"]
+        reason = ev.get("reason")
+        time = ev.get("eventTime")
+        fmt = EVT_TIME_FMT
+        if not time:
+            time = ev["lastTimestamp"]
+            fmt = TIME_FMT
+
+        eventTime = datetime.datetime.strptime(time, fmt)
+
+        MAPPING_REASON_NAME = {
+            "Scheduled": "creation_time",
+            "AddedInterface": "pulling",
+            "Pulled": "pulled",
+            "Started": "started",
+            "Killing": "terminated",
+        }
+
+        evt_name = MAPPING_REASON_NAME.get(reason)
+
+        if evt_name and eventTime:
+            results.notebook_start_time = min([results.notebook_start_time, eventTime])
+
+            results.notebook_time[podname].__dict__[evt_name] = eventTime
 
 def parse_execution_times(results, dirname):
-    with open(dirname / "job.yaml") as f:
+    results.pod_time = {}
+
+    with open(dirname / "tester_job.yaml") as f:
         job = yaml.safe_load(f)
 
     results.job_creation_time = \
         datetime.datetime.strptime(
             job["status"]["startTime"],
-            FMT)
+            TIME_FMT)
     results.job_completion_time = \
         datetime.datetime.strptime(
             job["status"]["completionTime"],
-            FMT)
+            TIME_FMT)
 
-    with open(dirname / "pods.yaml") as f:
+    with open(dirname / "tester_pods.yaml") as f:
         podlist = yaml.safe_load(f)
 
-    results.pod_time = {}
     for pod in podlist["items"]:
         podname = pod["metadata"]["name"]
         results.pod_time[podname] = types.SimpleNamespace()
@@ -32,40 +72,41 @@ def parse_execution_times(results, dirname):
         results.pod_time[podname].creation_time = \
             datetime.datetime.strptime(
                 pod["metadata"]["creationTimestamp"],
-                FMT)
+                TIME_FMT)
         results.pod_time[podname].start_time = \
             datetime.datetime.strptime(
                 pod["status"]["startTime"],
-                FMT)
+                TIME_FMT)
 
         if pod["status"]["containerStatuses"][0]["state"]["terminated"]:
             results.pod_time[podname].container_started = \
                 datetime.datetime.strptime(
                     pod["status"]["containerStatuses"][0]["state"]["terminated"]["startedAt"],
-                    FMT)
+                    TIME_FMT)
 
             results.pod_time[podname].container_finished = \
                 datetime.datetime.strptime(
                     pod["status"]["containerStatuses"][0]["state"]["terminated"]["finishedAt"],
-                    FMT)
+                    TIME_FMT)
 
         elif pod["status"]["containerStatuses"][0]["state"]["running"]:
             results.pod_time[podname].container_running_since = \
                 datetime.datetime.strptime(
                     pod["status"]["containerStatuses"][0]["state"]["running"]["startedAt"],
-                    FMT)
+                    TIME_FMT)
 
         else:
             import pdb;pdb.set_trace()
 
     pass
 
-def parse_data(results_dir):
+def parse_data():
+    results_dir = pathlib.Path(".") / cli_args.kwargs["results_dirname"]
+
     settings = {"expe": "test", "value": "true"}
     results = types.SimpleNamespace()
 
-    dirname = pathlib.Path(".") / "results"
-
-    parse_execution_times(results, dirname)
+    parse_execution_times(results, results_dir)
+    parse_notebook_events(results, results_dir)
 
     store.add_to_matrix(settings, None, results, None)
