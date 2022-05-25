45f82b3e42d88639fcf6e20cbb7afa2e28afe6a5
Kevin Pouget <kpouget@redhat.com>
2022-05-25 14:00:19 +0200

    testing: ods: prepare 2-cluster testing

--- testing/ods/jh-at-scale.sh
+++ testing/ods/jh-at-scale.sh
@@ -5,51 +5,35 @@ set -o pipefail
 set -o nounset
 set -x
 
-echo "TEST DISABLED UNTIL RHODS IS INSTALLED IN THE OSD CLUSTER"
-
-exit 0
-
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source $THIS_DIR/../prow/_logging.sh
+source "$THIS_DIR/../prow/_logging.sh"
 
-#PSAP_ODS_SECRET_PATH="/var/run/psap-ods-secret-1"
-PSAP_ODS_SECRET_PATH="/var/run/psap-entitlement-secret"
-S3_LDAP_PROPS="${PSAP_ODS_SECRET_PATH}/s3_ldap.passwords"
-
-ODS_CATALOG_VERSION="quay.io/modh/qe-catalog-source"
-ODS_CATALOG_IMAGE_VERSION="v1100-6"
-
-ODS_CI_TEST_NAMESPACE=loadtest
-ODS_CI_REPO="https://github.com/openshift-psap/ods-ci.git"
-ODS_CI_REF="multiuser"
-
-ODS_CI_IMAGESTREAM="ods-ci"
-ODS_CI_TAG="latest"
-
-ODS_CI_NB_USERS=10
-ODS_CI_USER_PREFIX=testuser
-
-ODS_CI_USER_GROUP=rhods-users
-
-LDAP_IDP_NAME=RHODS_CI_LDAP
+source "$THIS_DIR/common.sh"
 
 # simulate two clusters
 
-KUBECONFIG_DRIVER=/tmp/kubeconfig_driver # cluster driving the test
-KUBECONFIG_SUTEST=/tmp/kubeconfig_sutest # system under test
+KUBECONFIG_DRIVER="$KUBECONFIG" # cluster driving the test
+KUBECONFIG_SUTEST="/tmp/kubeconfig_sutest" # system under test
 
-cp "$KUBECONFIG" "$KUBECONFIG_DRIVER"
-cp "$KUBECONFIG" "$KUBECONFIG_SUTEST"
+DRIVER_CLUSTER=driver
+SUTEST_CLUSTER=sutest
+
+switch_sutest_cluster() {
+    switch_cluster "$SUTEST_CLUSTER"
+}
+
+switch_driver_cluster() {
+    switch_cluster "$DRIVER_CLUSTER"
+}
 
 switch_cluster() {
     cluster="$1"
-    if [[ "$cluster" == "driver" ]]; then
-        echo "Switching to the 'driver' cluster"
+    echo "Switching to the '$cluster' cluster"
+    if [[ "$cluster" == "$DRIVER_CLUSTER" ]]; then
         export KUBECONFIG=$KUBECONFIG_DRIVER
-    elif [[ "$cluster" == "sutest" ]]; then
-        echo "Switching to the 'driver' cluster"
-        export KUBECONFIG=$KUBECONFIG_DRIVER
+    elif [[ "$cluster" == "$SUTEST_CLUSTER" ]]; then
+        export KUBECONFIG=$KUBECONFIG_SUTEST
     else
         echo "Requested to switch to an unknown cluster '$cluster', exiting."
         exit 1
@@ -97,12 +81,38 @@ kill_bg_processes() {
     echo "All the processes have been terminated."
 }
 
+# ---
+
+connect_sutest_cluster() {
+    osd_cluster_name=$1
+
+    touch "$KUBECONFIG_SUTEST"
+
+    switch_sutest_cluster
+
+    if [[ "$osd_cluster_name" ]]; then
+        echo "OSD cluster name is $osd_cluster_name"
+
+        ocm_login
+
+        if ! ocm_cluster_is_ready "$osd_cluster_name"
+        then
+            echo "OCM cluster '$osd_cluster_name' isn't ready ..."
+            exit 1
+        fi
+
+        ocm_oc_login "$osd_cluster_name"
+    fi
+
+    oc get clusterversion
+}
+
 prepare_driver_cluster() {
     switch_cluster "driver"
 
     oc create namespace "$ODS_CI_TEST_NAMESPACE" -oyaml --dry-run=client | oc apply -f-
 
-    ./run_toolbox.py utils build_push_image \
+    run_in_bg ./run_toolbox.py utils build_push_image \
                      "${ODS_CI_IMAGESTREAM}" "$ODS_CI_TAG" \
                      --namespace="$ODS_CI_TEST_NAMESPACE" \
                      --git-repo="$ODS_CI_REPO" \
@@ -110,77 +120,88 @@ prepare_driver_cluster() {
                      --context-dir="/" \
                      --dockerfile-path="build/Dockerfile"
 
-    ./run_toolbox.py cluster deploy_minio_s3_server "$S3_LDAP_PROPS"
+    run_in_bg ./run_toolbox.py cluster deploy_minio_s3_server "$S3_LDAP_PROPS"
+}
+
+prepare_sutest_cluster() {
+    osd_cluster_name=$1
+
+    if [[ "$osd_cluster_name" ]]; then
+        prepare_osd_sutest_cluster "$osd_cluster_name"
+    else
+       prepare_ocp_sutest_cluster
+    fi
 }
 
 prepare_osd_sutest_cluster() {
-    switch_cluster "sutest"
+    osd_cluster_name=$1
 
-    CLUSTER_NAME=$(oc whoami --show-console | cut -d. -f3)
-    echo "OSD cluster name is $CLUSTER_NAME"
+    switch_sutest_cluster
 
-    if [[ $((ocm describe cluster $CLUSTER_NAME --json || true) | jq -r .state) != "ready" ]]; then
-        echo "OCM cluster $CLUSTER_NAME isn't ready ..."
-        exit 1
-    fi
+    run_in_bg ./run_toolbox.py rhods deploy_addon "$osd_cluster_name"
 
-    ./run_toolbox.py rhods deploy_ldap \
+    run_in_bg ./run_toolbox.py rhods deploy_ldap \
               "$LDAP_IDP_NAME" "$ODS_CI_USER_PREFIX" "$ODS_CI_NB_USERS" "$S3_LDAP_PROPS" \
-              --use_ocm= \
+              --use_ocm="$osd_cluster_name" \
               --wait
 }
 
 prepare_ocp_sutest_cluster() {
-    switch_cluster "sutest"
+    switch_sutest_cluster
 
     ./run_toolbox.py cluster set-scale m5.xlarge 5 --force
 
     run_in_bg ./run_toolbox.py rhods deploy_ldap "$LDAP_IDP_NAME" "$ODS_CI_USER_PREFIX" "$ODS_CI_NB_USERS" "$S3_LDAP_PROPS"
 
     echo "Deploying ODS $ODS_CATALOG_IMAGE_VERSION (from $ODS_CATALOG_VERSION)"
+
     run_in_bg ./run_toolbox.py rhods deploy_ods "$ODS_CATALOG_VERSION" "$ODS_CATALOG_IMAGE_VERSION"
 
     oc_adm_groups_new_rhods_users "$ODS_CI_USER_GROUP" "$ODS_CI_USER_PREFIX" "$ODS_CI_NB_USERS"
 }
 
 reset_prometheus() {
-    switch_cluster "driver"
+    switch_driver_cluster
     ./run_toolbox.py cluster reset_prometheus_db
 
-    switch_cluster "sutest"
+    switch_sutest_cluster
     ./run_toolbox.py cluster reset_prometheus_db
     ./run_toolbox.py rhods reset_prometheus_db
 }
 
 collect_sutest() {
-    switch_cluster "sutest"
+    switch_sutest_cluster
     ./run_toolbox.py rhods capture_state > /dev/null || true
     ./run_toolbox.py cluster capture_environment > /dev/null || true
 }
 
-undeploy_rhods() {
-    ./run_toolbox.py rhods undeploy_ods --force=False
+delete_rhods_postgres() {
+    switch_sutest_cluster
+
+    # Destroy Postgres database to avoid AWS leaks ...
+    # See https://issues.redhat.com/browse/MGDAPI-4118
+
+    if ! oc delete postgres/jupyterhub-db-rds -n redhat-ods-applications --ignore-not-found; then
+        echo "WARNING: Postgres database could not be deleted ..."
+    fi
 }
 
 finalizers+=("kill_bg_processes")
 finalizers+=("collect_sutest")
-finalizers+=("undeploy_rhods")
+finalizers+=("delete_rhods_postgres")
 
+
+OSD_CLUSTER_NAME=$(get_osd_cluster_name)
+connect_sutest_cluster "$OSD_CLUSTER_NAME"
+
+prepare_sutest_cluster "$OSD_CLUSTER_NAME"
 prepare_driver_cluster
-
-switch_cluster "sutest"
-SUTEST_IS_OSD=$(oc get ns/openshift-config-manageda -oname --ignore-not-found)
-if [[ "$SUTEST_IS_OSD" ]]; then
-    prepare_osd_sutest_cluster
-else
-    prepare_ocp_sutest_cluster
-fi
 
 wait_bg_processes
 
 reset_prometheus
 
-switch_cluster "driver"
+switch_driver_cluster
 
 if [[ "$ODS_CI_NB_USERS" -le 5 ]]; then
     collect=all
@@ -195,6 +216,6 @@ fi
                  --sut_cluster_kubeconfig="$KUBECONFIG_SUTEST" \
                  --artifacts-collected=$collect
 
-switch_cluster "sutest"
+switch_sutest_cluster
 ./run_toolbox.py cluster dump_prometheus_db
 ./run_toolbox.py rhods dump_prometheus_db
