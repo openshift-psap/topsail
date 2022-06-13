73188c7984339801c06b2a9c98566b9343cf7de6
Kevin Pouget <kpouget@redhat.com>
2022-06-13 13:31:52 +0200

    rhods_test_jupyterlab: ensure the s3 destination exists
    This avoids a crash later on if the S3 destination doesn't not exist.
    ```
    2022-06-13 10:42:08,070 p=5184 u=psap-ci-runner n=ansible | roles/rhods_test_jupyterlab/tasks/main.yml:242
    2022-06-13 10:42:08,070 p=5184 u=psap-ci-runner n=ansible | TASK: rhods_test_jupyterlab : Export the data out of the S3 bucket
    2022-06-13 10:42:08,071 p=5184 u=psap-ci-runner n=ansible | ----- FAILED ----
    2022-06-13 10:42:08,071 p=5184 u=psap-ci-runner n=ansible | msg: non-zero return code
    2022-06-13 10:42:08,071 p=5184 u=psap-ci-runner n=ansible |
    2022-06-13 10:42:08,071 p=5184 u=psap-ci-runner n=ansible | <command> oc -n minio -c mc exec minio-deployment-8bbc57bcf-pf598 -- bash -c mkdir /artifacts/to_export && mc --config-dir /tmp cp minio/mybucket/ /artifacts/to_export --recursive
    2022-06-13 10:42:08,071 p=5184 u=psap-ci-runner n=ansible |
    2022-06-13 10:42:08,071 p=5184 u=psap-ci-runner n=ansible | <stderr> mc: <ERROR> Unable to validate source `minio/mybucket/`.
    2022-06-13 10:42:08,071 p=5184 u=psap-ci-runner n=ansible | <stderr>
    command terminated with exit code 1
    ```

--- roles/rhods_test_jupyterlab/files/entrypoint.sh
+++ roles/rhods_test_jupyterlab/files/entrypoint.sh
@@ -9,10 +9,30 @@ sed  "s/#{JOB_COMPLETION_INDEX}/${JOB_COMPLETION_INDEX}/g" /mnt/ods-ci-test-vari
 
 echo "Artifacts retention mode: $ARTIFACTS_COLLECTED"
 
+configure_s3() {
+    export S3_HOST_BASE=minio.minio.svc.cluster.local:9000
+    export S3_HOST_BUCKET=$S3_HOST_BASE
+
+    export S3_ACCESS_KEY=minio
+    # S3_SECRET_KEY: set below
+
+    # run this in a subshell to avoid printing the password in clear because of 'set -x'
+    bash -ec "eval \$(yq e .TEST_USER.PASSWORD /tmp/test-variables.yml | awk '{ print \"export S3_SECRET_KEY=\" \$1 }'); cat /mnt/s3-config/s3cfg | envsubst > ~/.s3cfg"
+
+    date > /tmp/started
+
+    s3cmd put /tmp/started \
+          s3://mybucket/ods-ci/$HOSTNAME/ \
+          --no-preserve \
+          --no-progress
+}
+
 export KUBECONFIG=/tmp/kube
 
 export K8S_API=$(yq e .OCP_API_URL /tmp/test-variables.yml)
 export USERNAME=$(yq e .TEST_USER.USERNAME /tmp/test-variables.yml)
+
+configure_s3
 
 touch "$KUBECONFIG"
 # run this in a subshell to avoid printing the password in clear because of 'set -x'
@@ -29,15 +49,6 @@ else
 fi
 
 echo "$FINISH" > /tmp/finish
-
-export S3_HOST_BASE=minio.minio.svc.cluster.local:9000
-export S3_HOST_BUCKET=$S3_HOST_BASE
-
-export S3_ACCESS_KEY=minio
-# S3_SECRET_KEY: set below
-
-# run this in a subshell to avoid printing the password in clear because of 'set -x'
-bash -ec "eval \$(yq e .TEST_USER.PASSWORD /tmp/test-variables.yml | awk '{ print \"export S3_SECRET_KEY=\" \$1 }'); cat /mnt/s3-config/s3cfg | envsubst > ~/.s3cfg"
 
 retcode=0 # always exit 0, we'll decide later if this is a success or a failure
 
