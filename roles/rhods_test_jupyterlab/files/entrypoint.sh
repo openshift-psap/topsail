55ae184054a235eca3758ceb914351cc2239055e
Kevin Pouget <kpouget@redhat.com>
2022-06-20 17:44:22 +0200

    rhods_test_jupyterlab: refactor to move s3cmd out of the main container

--- roles/rhods_test_jupyterlab/files/entrypoint.sh
+++ roles/rhods_test_jupyterlab/files/entrypoint.sh
@@ -5,34 +5,15 @@ set -o pipefail
 set -o nounset
 set -x
 
-sed  "s/#{JOB_COMPLETION_INDEX}/${JOB_COMPLETION_INDEX}/g" /mnt/ods-ci-test-variables/test-variables.yml > /tmp/test-variables.yml
+sed "s/#{JOB_COMPLETION_INDEX}/${JOB_COMPLETION_INDEX}/g" /mnt/ods-ci-test-variables/test-variables.yml > /tmp/test-variables.yml
 
-echo "Artifacts retention mode: $ARTIFACTS_COLLECTED"
+if [[ -z "{ARTIFACTS_DIR:-}" ]]; then
+    ARTIFACTS_DIR=/tmp/ods-ci
+fi
 
-configure_s3() {
-    export S3_HOST_BASE=minio.minio.svc.cluster.local:9000
-    export S3_HOST_BUCKET=$S3_HOST_BASE
+mkdir -p "${ARTIFACTS_DIR}"
 
-    export S3_ACCESS_KEY=minio
-    # S3_SECRET_KEY: set below
-
-    # run this in a subshell to avoid printing the password in clear because of 'set -x'
-    bash -ec "eval \$(yq e .TEST_USER.PASSWORD /tmp/test-variables.yml | awk '{ print \"export S3_SECRET_KEY=\" \$1 }'); cat /mnt/s3-config/s3cfg | envsubst > ~/.s3cfg"
-
-    date > /tmp/started
-
-    s3cmd put /tmp/started \
-          s3://mybucket/ods-ci/$HOSTNAME/ \
-          --no-preserve \
-          --no-progress
-}
-
-export KUBECONFIG=/tmp/kube
-
-export K8S_API=$(yq e .OCP_API_URL /tmp/test-variables.yml)
-export USERNAME=$(yq e .TEST_USER.USERNAME /tmp/test-variables.yml)
-
-configure_s3
+trap "touch $ARTIFACTS_DIR/test_exit_code" EXIT
 
 touch "$KUBECONFIG"
 # run this in a subshell to avoid printing the password in clear because of 'set -x'
@@ -43,29 +24,13 @@ bash -x ./run_robot_test.sh \
     --skip-pip-install \
     --test-variables-file /tmp/test-variables.yml \
     --skip-oclogin true \
+    --test-artifact-dir "$ARTIFACTS_DIR" \
     --test-case "$TEST_CASE" \
     || test_exit_code=$?
 
-echo "$test_exit_code" > /tmp/test_exit_code
+# /!\ the creation of this file triggers the export of the logs
+echo "$test_exit_code" > "${ARTIFACTS_DIR}/test_exit_code"
 
 echo "Test finished with $test_exit_code errors."
 
-retcode=0 # always exit 0, we'll decide later if this is a success or a failure
-
-if [[ "$ARTIFACTS_COLLECTED" == "none" ]]; then
-    exit $retcode
-fi
-
-if [[ "$ARTIFACTS_COLLECTED" == "no-image" ]]; then
-    find /tmp/ods-ci/test-output -name '*.png' -delete
-fi
-
-s3cmd put \
-      /tmp/test_edit_code /tmp/ods-ci/test-output/*/* \
-      s3://mybucket/ods-ci/$HOSTNAME/ \
-      --recursive \
-      --no-preserve \
-      --no-progress \
-      --stats
-
-exit $retcode
+exit 0 # always exit 0, we'll decide later if this is a success or a failure
