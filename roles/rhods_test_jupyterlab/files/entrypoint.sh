7cf35f3c22af14543ea54ea2a14a32e7fb603797
Kevin Pouget <kpouget@redhat.com>
2022-06-24 15:07:59 +0200

    rhods_test_jupyterlab: move the robot test-case into roles/rhods_test_jupyterlab/files/ods-ci

--- roles/rhods_test_jupyterlab/files/entrypoint.sh
+++ roles/rhods_test_jupyterlab/files/entrypoint.sh
@@ -18,6 +18,8 @@ do_oc_login() {
 
 sed "s/#{JOB_COMPLETION_INDEX}/${JOB_COMPLETION_INDEX:-1}/g" /mnt/ods-ci-test-variables/test-variables.yml > /tmp/test-variables.yml
 
+cp "/mnt/rhods-jupyterlab-entrypoint/$RUN_ROBOT_TEST_CASE" .
+
 # This isn't necessary for the testing, Keep it until
 # `run_robot_test.sh` initialization stops complaining when we provide
 # no KUBECONFIG.
