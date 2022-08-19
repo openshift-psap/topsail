ec84fb27081accf2fdec87789f156d28a5240e8c
Kevin Pouget <kpouget@redhat.com>
2022-08-19 12:16:50 +0200

    testing: recipes: run_rhods_test: new recipe

---
+++ testing/recipes/run_rhods_test.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o errtrace
+set -x
+
+#
+# Configuration the test and the environment
+#
+
+# export OSD_CLUSTER_NAME=<osd cluster name> # OCM name of the cluster, empty if OCP
+# export ODS_CI_NB_USERS=10 # number of users to simulate
+# export ODS_SLEEP_FACTOR=1 # how long to wait between user starts, in seconds
+
+BASE_ARTIFACT_DIR=/tmp/rhods_scale_test
+mkdir -p "$BASE_ARTIFACT_DIR"
+
+if [[ "${INSIDE_CI_IMAGE:-}" == "y" ]]; then
+    KUBECONFIG_DRIVER="${SHARED_DIR}/driver_kubeconfig" # cluster driving the test
+    KUBECONFIG_SUTEST="${SHARED_DIR}/sutest_kubeconfig" # system under test
+else
+    KUBECONFIG_DRIVER="${KUBECONFIG_DRIVER:-$KUBECONFIG}" # cluster driving the test
+    KUBECONFIG_SUTEST="${KUBECONFIG_SUTEST:-$KUBECONFIG}" # system under test
+fi
+
+# These environment variables are used in the scripts to refer to the
+# driver/sutest clusters
+export KUBECONFIG_DRIVER
+export KUBECONFIG_SUTEST
+
+export ARTIFACT_DIR="${BASE_ARTIFACT_DIR}/preparation"
+
+# 1. Configure the clusters
+
+# 1.0 Deploy RHODS in the sutest cluster
+
+testing/ods/jh-at-scale.sh deploy_rhods
+
+# 1.1 Deploy LDAP in the sutest cluster
+
+testing/ods/jh-at-scale.sh deploy_ldap
+
+# 1.2 Prepare the driver cluster
+
+testing/ods/jh-at-scale.sh prepare_driver_cluster
+
+#
+# 2. Run the tests in a dedicated ARTIFACT_DIR directory
+#
+
+# 2.1 Run the first test
+
+export ARTIFACT_DIR="${BASE_ARTIFACT_DIR}/test_1"
+export ODS_CI_NB_USERS=4
+export ODS_SLEEP_FACTOR=2
+
+testing/ods/jh-at-scale.sh run_jupyterlab_test
+testing/ods/jh-at-scale.sh generate_plots
+
+# 2.2 Run the second test
+
+export ARTIFACT_DIR="${BASE_ARTIFACT_DIR}/test_2"
+export ODS_CI_NB_USERS=3
+export ODS_SLEEP_FACTOR=1
+
+testing/ods/jh-at-scale.sh run_jupyterlab_test
+testing/ods/jh-at-scale.sh generate_plots
+
+#
+# 3. Cleanup the RHODS cluster
+#
+
+# 3.1 Undeploy LDAP in the sutest cluster
+
+testing/ods/jh-at-scale.sh undeploy_ldap
