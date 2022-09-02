066abb0d4493e52b557bdc8d51ccd8765429d7dc
Kevin Pouget <kpouget@redhat.com>
2022-09-02 10:21:43 +0200

    testing: ods: notebook_ux_e2e_scale_test: hardcode/enforce the default notebook size

--- testing/ods/notebook_ux_e2e_scale_test.sh
+++ testing/ods/notebook_ux_e2e_scale_test.sh
@@ -198,6 +198,11 @@ prepare_ocp_sutest_deploy_rhods() {
 sutest_wait_rhods_launch() {
     switch_sutest_cluster
     oc patch odhdashboardconfig odh-dashboard-config --type=merge -p '{"spec":{"notebookController":{"enabled":true}}}' -n redhat-ods-applications
+    oc patch odhdashboardconfig odh-dashboard-config --type=merge -p '{"spec":{"notebookController":{"pvcSize":"5Gi"}}}' -n redhat-ods-applications
+
+    oc get odhdashboardconfig/odh-dashboard-config -n redhat-ods-applications -ojson \
+        | jq '.spec.notebookSizes = [{"name": "'$ODS_NOTEBOOK_SIZE'", "resources": { "limits":{"cpu":"1", "memory":"4Gi"}, "requests":{"cpu":"1", "memory":"4Gi"}}}]' \
+        | oc apply -f-
     oc delete pod -l app.kubernetes.io/part-of=rhods-dashboard,app=rhods-dashboard  -n redhat-ods-applications
 
     oc adm groups new dedicated-admins
