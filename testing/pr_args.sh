db6c91251055ecf4b8a2978cdc1d28a6e9facaf0
Kevin Pouget <kpouget@redhat.com>
2022-09-05 08:08:26 +0200

    testing: pr_args: remove hardcoded values

--- testing/pr_args.sh
+++ testing/pr_args.sh
@@ -20,10 +20,10 @@ fi
 PR_URL="https://api.github.com/repos/openshift-psap/ci-artifacts/pulls/$PULL_NUMBER"
 PR_COMMENTS_URL="https://api.github.com/repos/openshift-psap/ci-artifacts/issues/$PULL_NUMBER/comments"
 
-author=kpouget #;$(echo "$JOB_SPEC" | jq -r .refs.pulls[0].author)
+author=$(echo "$JOB_SPEC" | jq -r .refs.pulls[0].author)
 
 JOB_NAME_PREFIX=pull-ci-openshift-psap-ci-artifacts-master
-test_name=test-pr-no-cluster #$(echo "$JOB_NAME" | sed "s/$JOB_NAME_PREFIX-//")
+test_name=$(echo "$JOB_NAME" | sed "s/$JOB_NAME_PREFIX-//")
 
 last_user_test_comment=$(curl -sSf "$PR_COMMENTS_URL" \
                              | jq '.[] | select(.user.login == "'$author'") | .body' \
