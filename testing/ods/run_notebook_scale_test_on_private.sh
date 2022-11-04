36d1975724b545a33429475e4e57cbf711abfdf9
Kevin Pouget <kpouget@redhat.com>
2022-11-04 09:17:58 +0100

    testing: ods: run_notebook_scale_test_on_private: fix following the configuration refactoring

--- testing/ods/run_notebook_scale_test_on_private.sh
+++ testing/ods/run_notebook_scale_test_on_private.sh
@@ -8,12 +8,9 @@ set -x
 
 TESTING_ODS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source "$TESTING_ODS_DIR/process_ctrl.sh"
+source "$TESTING_ODS_DIR/../process_ctrl.sh"
 source "$TESTING_ODS_DIR/../_logging.sh"
-source "$TESTING_ODS_DIR/config_common.sh"
-source "$TESTING_ODS_DIR/config_clusters.sh"
-source "$TESTING_ODS_DIR/config_load_overrides.sh"
-
+source "$TESTING_ODS_DIR/configure.sh"
 source "$TESTING_ODS_DIR/cluster_helpers.sh"
 
 export KUBECONFIG_DRIVER=$KUBECONFIG
@@ -22,38 +19,46 @@ export KUBECONFIG_SUTEST=/tmp/sutest_kubeconfig
 HOSTNAME_KEY=clusters.create.sutest.already_exists.hostname
 USERNAME_KEY=clusters.create.sutest_already_exists.username
 
-set_config_from_pr_arg 1 "$HOSTNAME_KEY"
-set_config_from_pr_arg 2 "$USERNAME_KEY"
 
-cluster_hostname=$(get_config "$HOSTNAME_KEY")
-if [[ -z "$cluster_hostname" ]]; then
-    _error "$HOSTNAME_KEY must be set with the hostname of the private cluster, or with the PR argument 0"
-fi
+main() {
+    "$TESTING_ODS_DIR/ci_init_configure.sh"
 
-cluster_username=$(get_config "$USERNAME_KEY")
-if [[ -z "$cluster_username" ]]; then
-    _error "$USERNAME_KEY must be set with the username to use to log into the private cluster, or with the PR argument 1"
-fi
+    set_config_from_pr_arg 1 "$HOSTNAME_KEY"
+    set_config_from_pr_arg 2 "$USERNAME_KEY"
 
-prepare_driver_cluster() {
-    process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" prepare_driver_cluster
-    "$TESTING_ODS_DIR/notebook_scale_test.sh" prepare_driver_cluster
+    cluster_hostname=$(get_config "$HOSTNAME_KEY")
+    if [[ "$cluster_hostname" == null ]]; then
+        _error "$HOSTNAME_KEY must be set with the hostname of the private cluster, or with the PR argument 0"
+    fi
+
+    cluster_username=$(get_config "$USERNAME_KEY")
+    if [[ "$cluster_username" == null ]]; then
+        local pr_author=$(echo "$JOB_SPEC" | jq -r .refs.pulls[0].author)
+        set_config "$USERNAME_KEY" "$pr_author"
+    fi
+
+    prepare_driver_cluster() {
+        process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" prepare_driver_cluster
+        "$TESTING_ODS_DIR/notebook_scale_test.sh" prepare_driver_cluster
+    }
+
+    action=${1:-}
+
+    "$TESTING_ODS_DIR/private_cluster.sh" connect_sutest_cluster
+
+    if [[ "$action" == "prepare" ]]; then
+        process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" prepare_sutest_cluster
+        process_ctrl::run_in_bg prepare_driver_cluster
+
+        process_ctrl::wait_bg_processes
+    else
+        "$TESTING_ODS_DIR/notebook_scale_test.sh" run_test_and_plot
+
+        if [[ "$action" != "run" ]]; then
+            process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" unprepare_sutest_cluster
+            process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" unprepare_driver_cluster
+        fi
+    fi
 }
 
-action=${1:-}
-
-"$TESTING_ODS_DIR/private_cluster.sh" connect_sutest_cluster
-
-if [[ "$action" == "prepare" ]]; then
-    process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" prepare_sutest_cluster
-    process_ctrl::run_in_bg prepare_driver_cluster
-
-    process_ctrl::wait_bg_processes
-else
-    "$TESTING_ODS_DIR/notebook_scale_test.sh" run_test_and_plot
-
-    if [[ "$action" != "run" ]]; then
-        process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" unprepare_sutest_cluster
-        process_ctrl::run_in_bg "$TESTING_ODS_DIR/private_cluster.sh" unprepare_driver_cluster
-    fi
-fi
+main "$@"
