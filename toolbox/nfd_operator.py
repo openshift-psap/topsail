438a0f94f8ffa2eca63969408d9bae242579d9d1
Kevin Pouget <kpouget@redhat.com>
2022-11-21 12:12:07 +0100

    nfd_deploy_from_operatorhub: use generate config file

--- toolbox/nfd_operator.py
+++ toolbox/nfd_operator.py
@@ -1,4 +1,4 @@
-from toolbox._common import RunAnsibleRole, AnsibleRole
+from toolbox._common import RunAnsibleRole, AnsibleRole, AnsibleConstant, AnsibleSkipConfigGeneration, AnsibleMappedParams
 
 
 class NFDOperator:
@@ -7,26 +7,25 @@ class NFDOperator:
     """
 
     @AnsibleRole("cluster_deploy_operator")
-    def deploy_from_operatorhub(self, channel=None):
+    @AnsibleSkipConfigGeneration # see cluster.deploy_operator
+    @AnsibleConstant("", "cluster_deploy_operator_catalog", "redhat-operators")
+    @AnsibleConstant("", "cluster_deploy_operator_manifest_name", "nfd")
+    @AnsibleConstant("", "cluster_deploy_operator_namespace", "openshift-nfd")
+    @AnsibleConstant("", "cluster_deploy_operator_deploy_cr", True)
+    @AnsibleMappedParams
+    def deploy_from_operatorhub(self, channel=''):
         """
         Deploys the GPU Operator from OperatorHub
 
         Args:
-            The operator hub channel to deploy. e.g. 4.7
+            channel: The operator hub channel to deploy. e.g. 4.7
         """
-        opts = {
-            "cluster_deploy_operator_catalog": "redhat-operators",
-            "cluster_deploy_operator_manifest_name": "nfd",
-            "cluster_deploy_operator_namespace": "openshift-nfd",
-            "cluster_deploy_operator_deploy_cr": True,
-        }
-
-        if channel is not None:
-            opts["cluster_deploy_operator_channel"] = channel
 
         return RunAnsibleRole(opts)
 
     @AnsibleRole("nfd_operator_undeploy_from_operatorhub")
+    @AnsibleMappedParams
+    @AnsibleSkipConfigGeneration # see cluster.undeploy_operator
     def undeploy_from_operatorhub(self):
         """
         Undeploys an NFD-operator that was deployed from OperatorHub
