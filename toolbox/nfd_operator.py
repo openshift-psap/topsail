acb1ee50042b8266a7932e6ccf62c0f373b5fb1e
Kevin Pouget <kpouget@redhat.com>
2022-11-21 12:09:00 +0100

    nfd_operator_deploy_custom_commit: remove role

--- toolbox/nfd_operator.py
+++ toolbox/nfd_operator.py
@@ -5,25 +5,6 @@ class NFDOperator:
     """
     Commands for deploying, building and testing the NFD operator in various ways
     """
-    @AnsibleRole("nfd_operator_deploy_custom_commit")
-    def deploy_from_commit(self, git_repo, git_ref, image_tag=None):
-        """
-        Deploys the NFD operator from the given git commit
-
-        Args:
-            git_rep: The git repository to deploy from, e.g. https://github.com/openshift/cluster-nfd-operator.git
-            git_ref: The git ref to deploy from, e.g. master
-            image_tag: The NFD operator image tag UID.
-        """
-        opts = {
-            "nfd_operator_git_repo": git_repo,
-            "nfd_operator_git_ref": git_ref,
-        }
-
-        if image_tag is not None:
-            opts["nfd_operator_image_tag"] = image_tag
-
-        return RunAnsibleRole(opts)
 
     @AnsibleRole("cluster_deploy_operator")
     def deploy_from_operatorhub(self, channel=None):
