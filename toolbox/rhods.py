dd24054ff0ecd611ae22744b4f945880225fdbe5
Kevin Pouget <kpouget@redhat.com>
2022-11-21 11:50:08 +0100

    toolbox: rhods: use AnsibleMappedParams and AnsibleConstants

--- toolbox/rhods.py
+++ toolbox/rhods.py
@@ -1,33 +1,25 @@
 import sys
 
-from toolbox._common import RunAnsibleRole, AnsibleRole
+from toolbox._common import RunAnsibleRole, AnsibleRole, AnsibleMappedParams, AnsibleConstant, AnsibleSkipConfigGeneration
 
 
-ODS_CATALOG_IMAGE_DEFAULT = "quay.io/modh/qe-catalog-source"
-ODS_CATALOG_IMAGE_VERSION_DEFAULT = "v160-8"
 class RHODS:
     """
     Commands relating to RHODS
     """
 
     @AnsibleRole("rhods_deploy_ods")
-    def deploy_ods(self,
-                   catalog_image=ODS_CATALOG_IMAGE_DEFAULT,
-                   version=ODS_CATALOG_IMAGE_VERSION_DEFAULT):
+    @AnsibleMappedParams
+    def deploy_ods(self, catalog_image="", tag=""):
         """
         Deploy ODS operator from its custom catalog
 
         Args:
-          catalog_image: Optional. Container image containing ODS bundle.
-          version: Optional. Version (catalog image tag) of ODS to deploy.
+          catalog_image: Container image containing the RHODS bundle.
+          version: Catalog image tag to use to deploy RHODS.
         """
 
-        opts = {
-            "rhods_deploy_ods_catalog_image": catalog_image,
-            "rhods_deploy_ods_catalog_image_tag": version,
-        }
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("rhods_wait_ods")
     @AnsibleMappedParams
@@ -41,6 +33,10 @@ class RHODS:
         return RunAnsibleRole()
 
     @AnsibleRole("ocm_deploy_addon")
+    @AnsibleConstant("Identifier of the addon that should be deployed",
+                     "ocm_deploy_addon_id", "managed-odh")
+    @AnsibleMappedParams
+    @AnsibleSkipConfigGeneration
     def deploy_addon(self,
                      cluster_name, notification_email, wait_for_ready_state=True):
         """
@@ -49,21 +45,15 @@ class RHODS:
         Args:
           cluster_name: The name of the cluster where RHODS should be deployed.
           notification_email: The email to register for RHODS addon deployment.
-          wait_for_ready_state: Optional. If true (default), will cause the role to wait until addon reports ready state. (Can time out)
+          wait_for_ready_state: If true (default), will cause the role to wait until addon reports ready state. (Can time out)
         """
 
         addon_parameters = '[{"id":"notification-email","value":"'+notification_email+'"}]'
 
-        opt = {
-            "ocm_deploy_addon_id": "managed-odh",
-            "ocm_deploy_addon_cluster_name": cluster_name,
-            "ocm_deploy_addon_wait_for_ready_state": wait_for_ready_state,
-            "ocm_deploy_addon_parameters": addon_parameters,
-        }
-
-        return RunAnsibleRole(opt)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("rhods_notebook_ux_e2e_scale_test")
+    @AnsibleMappedParams
     def notebook_ux_e2e_scale_test(self,
                                    namespace,
                                    idp_name, username_prefix, user_count: int,
@@ -115,43 +105,22 @@ class RHODS:
           ods_ci_notebook_benchmark_name: Name of the benchmark to execute in the notebook.
           ods_ci_notebook_benchmark_repeat: Number of repeats of the benchmark to perform.
           ods_ci_notebook_benchmark_number: Number of times the benchmark should be executed within one repeat.
-          state_signal_redis_server: Optional. Hostname and port of the Redis server for StateSignal synchronization (for the synchronization of the beginning of the user simulation)
-          toleration_key: Optional. Toleration key to use for the test Pods.
+          state_signal_redis_server: Hostname and port of the Redis server for StateSignal synchronization (for the synchronization of the beginning of the user simulation)
+          toleration_key: Toleration key to use for the test Pods.
         """
-
-        opts = {
-            "rhods_notebook_ux_e2e_scale_test_idp_name": idp_name,
-            "rhods_notebook_ux_e2e_scale_test_username_prefix": username_prefix,
-            "rhods_notebook_ux_e2e_scale_test_user_count": user_count,
-            "rhods_notebook_ux_e2e_scale_test_user_index_offset": user_index_offset,
-            "rhods_notebook_ux_e2e_scale_test_secret_properties": secret_properties_file,
-            "rhods_notebook_ux_e2e_scale_test_notebook_url": notebook_url,
-            "rhods_notebook_ux_e2e_scale_test_sut_cluster_kubeconfig": sut_cluster_kubeconfig,
-            "rhods_notebook_ux_e2e_scale_test_artifacts_collected": artifacts_collected,
-            "rhods_notebook_ux_e2e_scale_test_ods_sleep_factor": ods_sleep_factor,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_test_case": ods_ci_test_case,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_exclude_tags": ods_ci_exclude_tags,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_artifacts_exporter_istag": ods_ci_artifacts_exporter_istag,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_notebook_image_name": ods_ci_notebook_image_name,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_notebook_size_name": ods_ci_notebook_size_name,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_notebook_benchmark_name": ods_ci_notebook_benchmark_name,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_notebook_benchmark_number": ods_ci_notebook_benchmark_number,
-            "rhods_notebook_ux_e2e_scale_test_ods_ci_notebook_benchmark_repeat": ods_ci_notebook_benchmark_repeat,
-            "rhods_notebook_ux_e2e_scale_test_state_signal_redis_server": state_signal_redis_server,
-            "rhods_notebook_ux_e2e_scale_test_toleration_key": toleration_key,
-        }
 
         ARTIFACTS_COLLECTED_VALUES = ("all", "none", "no-screenshot", "no-screenshot-except-zero", "no-screenshot-except-failed", "no-screenshot-except-failed-and-zero")
         if artifacts_collected not in ARTIFACTS_COLLECTED_VALUES:
             print(f"ERROR: invalid value '{artifacts_collected}' for 'artifacts_collected'. Must be one of {', '.join(ARTIFACTS_COLLECTED_VALUES)}")
             sys.exit(1)
 
+        del ARTIFACTS_COLLECTED_VALUES
 
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("rhods_cleanup_notebooks")
-    def cleanup_notebooks(self,
-                          username_prefix):
+    @AnsibleMappedParams
+    def cleanup_notebooks(self, username_prefix):
         """
         Clean up the resources created along with the notebooks, during the scale tests.
 
@@ -159,13 +128,10 @@ class RHODS:
           username_prefix: Prefix of the usernames who created the resources.
         """
 
-        opts = {
-            "rhods_cleanup_notebooks_username_prefix": username_prefix,
-        }
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("rhods_notebook_api_scale_test")
+    @AnsibleMappedParams
     def notebook_api_scale_test(self,
                                 namespace,
                                 idp_name,
@@ -192,49 +158,31 @@ class RHODS:
           username_prefix: Prefix of the RHODS users.
           test_name: Test to perform.
           user_count: Number of users to run in parallel.
-          notebook_image_name: Optional. Name of the RHODS image to use when launching the notebooks.
+          notebook_image_name: Name of the RHODS image to use when launching the notebooks.
 
           run_time: Test run time (eg, 300s, 20m, 3h, 1h30m, etc.)
           spawn_rate: Rate to spawn users at (users per second)
-          sut_cluster_kubeconfig: Optional. Path of the system-under-test cluster's Kubeconfig. If provided, the RHODS endpoints will be looked up in this cluster.
-          toleration_key: Optional. Toleration key to use for the test Pods.
-          artifacts_exporter_istag: Optional. Imagestream tag of the artifacts exporter side-car container.
+          sut_cluster_kubeconfig: Path of the system-under-test cluster's Kubeconfig. If provided, the RHODS endpoints will be looked up in this cluster.
+          toleration_key: Toleration key to use for the test Pods.
+          artifacts_exporter_istag: Imagestream tag of the artifacts exporter side-car container.
         """
 
-        opts = {
-            "rhods_notebook_api_scale_test_namespace": namespace,
-            "rhods_notebook_api_scale_test_idp_name": idp_name,
-            "rhods_notebook_api_scale_test_secret_properties": secret_properties_file,
-            "rhods_notebook_api_scale_test_username_prefix": username_prefix,
-            "rhods_notebook_api_scale_test_user_count": user_count,
-            "rhods_notebook_api_scale_test_test_name": test_name,
-            "rhods_notebook_api_scale_test_run_time": run_time,
-            "rhods_notebook_api_scale_test_spawn_rate": spawn_rate,
-            "rhods_notebook_api_scale_test_sut_cluster_kubeconfig": sut_cluster_kubeconfig,
-            "rhods_notebook_api_scale_test_ods_ci_notebook_image_name": notebook_image_name,
-            "rhods_notebook_api_scale_test_toleration_key": toleration_key,
-            "rhods_notebook_api_scale_test_artifacts_exporter_istag": artifacts_exporter_istag,
-            "rhods_notebook_api_scale_test_istag": api_scale_test_istag,
-        }
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("rhods_undeploy_ods")
+    @AnsibleMappedParams
     def undeploy_ods(self,
                      namespace="redhat-ods-operator",
                      wait: bool = True):
         """
         Undeploy ODS operator
 
-        args:
-          wait: Optional. Wait for the operator full deletion.
+        Args:
+          namespace: Namespace where RHODS is installed.
+          wait: Wait for the operator full deletion.
         """
 
-        opts = {
-            "rhods_undeploy_ods_wait": wait,
-        }
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("rhods_cleanup_aws")
     @AnsibleMappedParams
@@ -246,41 +194,36 @@ class RHODS:
           openshift_installer: path of the openshift_installer to use. If empty, download it.
         """
 
-        return RunAnsibleRole(self)
+        return RunAnsibleRole()
 
     @AnsibleRole("cluster_prometheus_db")
+    @AnsibleSkipConfigGeneration # see cluster.reset_prometheus_db
+    @AnsibleConstant("", "cluster_prometheus_db_mode", "reset")
+    @AnsibleConstant("", "cluster_prometheus_db_label", "deployment=prometheus")
+    @AnsibleConstant("", "cluster_prometheus_db_namespace", "redhat-ods-monitoring")
     def reset_prometheus_db(self):
         """
         Resets RHODS Prometheus database, by destroying its Pod.
         """
 
-        opts = {
-            "cluster_prometheus_db_mode": "reset",
-            "cluster_prometheus_db_label": "deployment=prometheus",
-            "cluster_prometheus_db_namespace": "redhat-ods-monitoring",
-        }
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole()
 
     @AnsibleRole("cluster_prometheus_db")
-    def dump_prometheus_db(self,
-                           name_prefix="prometheus"):
+    @AnsibleSkipConfigGeneration # see cluster.reset_prometheus_db
+    @AnsibleConstant("", "cluster_prometheus_db_mode", "dump")
+    @AnsibleConstant("", "cluster_prometheus_db_label", "deployment=prometheus")
+    @AnsibleConstant("", "cluster_prometheus_db_namespace", "redhat-ods-monitoring")
+    @AnsibleConstant("", "cluster_prometheus_db_directory", "/prometheus/data")
+    @AnsibleMappedParams
+    def dump_prometheus_db(self, dump_name_prefix="prometheus"):
         """
         Dump Prometheus database into a file
 
         Args:
-          name_prefix: Optional. Name prefix for the archive that will be stored.
+          name_prefix: Name prefix for the archive that will be stored.c
         """
 
-        opts = {
-            "cluster_prometheus_db_mode": "dump",
-            "cluster_prometheus_db_label": "deployment=prometheus",
-            "cluster_prometheus_db_namespace": "redhat-ods-monitoring",
-            "cluster_prometheus_db_directory": "/prometheus/data",
-            "cluster_prometheus_db_dump_name_prefix": name_prefix,
-        }
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("rhods_capture_state")
     @AnsibleMappedParams
