6c47e29a87568c955e81c319deb46a8a09d3842b
Kevin Pouget <kpouget@redhat.com>
2022-11-21 13:33:41 +0100

    toolbox: benchmarking: use AnsibleMappedParams

--- toolbox/benchmarking.py
+++ toolbox/benchmarking.py
@@ -1,4 +1,4 @@
-from toolbox._common import RunAnsibleRole, AnsibleRole
+from toolbox._common import RunAnsibleRole, AnsibleRole, AnsibleMappedParams
 
 
 class Benchmarking:
@@ -7,7 +7,9 @@ class Benchmarking:
     """
 
     @AnsibleRole("benchmarking_deploy_coco_dataset")
-    def download_coco_dataset(self, node_hostname, namespace="default", pvc_name=None, storage_dir=None, s3_cred=None):
+    @AnsibleMappedParams
+    def download_coco_dataset(self, node_hostname, namespace="default",
+                              pvc_name="", storage_dir="/storage", s3_cred=""):
         """
         Downloads the COCO dataset into a PVC of the cluster
 
@@ -15,34 +17,16 @@ class Benchmarking:
             node_hostname: Hostname of the node where the download pod will be executed.
             namespace: Name of the namespace in which the resources will be created.
             pvc_name: Name of the PVC that will be create to store the dataset files.
-            s3_cred: Optional path to credentials to use for accessing the dataset s3 bucket.
+            s3_cred: Path to credentials to use for accessing the dataset s3 bucket.
+            storage_dir: Pod directory where the dataset will be downloaded
         """
-        opts = {
-            "benchmarking_node_hostname": node_hostname,
-            "benchmarking_namespace": namespace,
-        }
-        if pvc_name is not None:
-            opts["benchmarking_coco_dataset_pvc_name"] = pvc_name
-            print(
-                f"Using '{pvc_name}' as PVC name."
-            )
 
-        if storage_dir is not None:
-            opts["benchmarking_coco_dataset_storage_dir"] = storage_dir
-            print(
-                f"Using '{storage_dir}' as storage dir."
-            )
-
-        if s3_cred is not None:
-            opts["benchmarking_coco_dataset_s3_cred"] = s3_cred
-            print(
-                f"Using '{s3_cred}' as s3 credentials."
-            )
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
 
     @AnsibleRole("benchmarking_run_mlperf_ssd")
-    def run_mlperf_ssd(self, node_hostname, namespace="default", pvc_name=None, epochs=None, threshold=None):
+    @AnsibleMappedParams
+    def run_mlperf_ssd(self, node_hostname, namespace="default", pvc_name="",
+                       epochs: int = "", threshold: float = ""):
         """
         Run NVIDIA MLPerf SSD Detection training benchmark.
 
@@ -54,30 +38,23 @@ class Benchmarking:
             threshold: Benchmark threshold target value.
         """
 
-        opts = {
-            "benchmarking_node_hostname": node_hostname,
-            "benchmarking_namespace": namespace,
-        }
-
         if pvc_name is not None:
-            opts["benchmarking_coco_dataset_pvc_name"] = pvc_name
             print(
                 f"Using '{pvc_name}' as PVC where the coco dataset is stored."
             )
 
         if epochs:
             try:
-                epochs = str(int(epochs))
-                opts["benchmarking_epochs"] = epochs
+                epochs = int(epochs)
             except ValueError:
                 print("ERROR: epochs must be of type int")
                 exit(1)
+
         if threshold:
             try:
-                threshold = str(float(threshold))
-                opts["benchmarking_threshold"] = threshold
+                threshold = float(threshold)
             except ValueError:
                 print("ERROR: threshold must be of type float")
                 exit(1)
 
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
