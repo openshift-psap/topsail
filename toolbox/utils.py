bdd4453405b231498d1484c4dcd629aa87c2b884
Kevin Pouget <kpouget@redhat.com>
2022-11-21 14:00:00 +0100

    utils_build_push_image: use generated config file

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -2,7 +2,7 @@ import sys, os
 
 import secrets
 
-from toolbox._common import RunAnsibleRole, AnsibleRole
+from toolbox._common import RunAnsibleRole, AnsibleRole, AnsibleMappedParams
 
 
 class Utils:
@@ -11,10 +11,11 @@ class Utils:
     """
 
     @AnsibleRole("utils_build_push_image")
+    @AnsibleMappedParams
     def build_push_image(
             self,
-            local_image_name,
-            image_tag="",
+            image_local_name,
+            tag,
             namespace="ci-artifacts",
             remote_repo="",
             remote_auth_file="",
@@ -22,25 +23,25 @@ class Utils:
             git_ref="",
             dockerfile_path="Dockerfile",
             context_dir="/",
-            memory=""
+            memory: float = "",
     ):
         """
         Build and publish an image to quay using either a Dockerfile or
         git repo.
 
         Args:
-            local_image_name: Name of locally built image.
-            image_tag: Optional tag for the image to build. If undefined, a random tag will be generated.
-            namespace: Optional namespace where the local image will be built. Default: ci-artifacts.
-            remote_repo: Optional remote image repo to push to. If undefined, the image will not be pushed.
-            remote_auth_file: Optional auth file for the remote repository.
+            image_local_name: Name of locally built image.
+            tag: Tag for the image to build.
+            namespace: Namespace where the local image will be built.
+            remote_repo: Remote image repo to push to. If undefined, the image will not be pushed.
+            remote_auth_file: Auth file for the remote repository.
 
-            git_repo: Optional Git repo containing Dockerfile if used as source. If undefined, the local path of 'dockerfile_path' will be used.
-            git_ref: Optional Git commit ref (branch, tag, commit hash) in the git repository.
+            git_repo: Git repo containing Dockerfile if used as source. If undefined, the local path of 'dockerfile_path' will be used.
+            git_ref: Git commit ref (branch, tag, commit hash) in the git repository.
 
-            context_dir: Optional context dir inside the git repository. Default /.
-            dockerfile_path: Optional Path/Name of Dockerfile if used as source. Default: Dockerfile. If 'git_repo' is undefined, this path will be resolved locally, and the Dockerfile will be injected in the image BuildConfig.
-            memory: Optional flag to specify the required memory to build the image (in Gb).
+            context_dir: Context dir inside the git repository.
+            dockerfile_path: Path/Name of Dockerfile if used as source. If 'git_repo' is undefined, this path will be resolved locally, and the Dockerfile will be injected in the image BuildConfig.
+            memory: Flag to specify the required memory to build the image (in Gb).
         """
 
         if not git_repo and not dockerfile_path:
@@ -52,6 +53,7 @@ class Utils:
         if not both_or_none(remote_repo, remote_auth_file):
             print("ERROR: remote_repo and remote_auth_file must come together.")
             sys.exit(1)
+
         elif remote_repo:
             print(f"Using remote repo {remote_repo} and auth file {remote_auth_file} to push the image.")
         else:
@@ -60,6 +62,7 @@ class Utils:
         if not both_or_none(git_repo, git_ref):
             print("ERROR: git_repo and git_ref must come together.")
             sys.exit(1)
+
         elif git_repo:
             print(f"Using Git repo {git_repo}|{git_ref}|{context_dir}|{dockerfile_path} for building the image.")
         else:
@@ -71,35 +74,16 @@ class Utils:
 
         if memory:
             try:
-                memory = str(float(memory)) + "Gi"
+                memory = str(float(memory))
+                print(f"Requesting {memory} of memory for building the image.")
             except ValueError:
                 print("ERROR: memory must be of type float or int")
                 sys.exit(1)
-            print(f"Requesting {memory} of memory for building the image.")
-
-        if not image_tag:
-            image_tag = secrets.token_hex(4)
-            print(f"Using '{image_tag}' as image tag.")
 
         toolbox_name_suffix = os.environ.get("ARTIFACT_TOOLBOX_NAME_SUFFIX", "")
-        # use `{local_image_name}_{image_tag}` as first suffix in the directory name
-        os.environ["ARTIFACT_TOOLBOX_NAME_SUFFIX"] = f"_{local_image_name}_{image_tag}{toolbox_name_suffix}"
+        # use `{image_local_name}_{tag}` as first suffix in the directory name
+        os.environ["ARTIFACT_TOOLBOX_NAME_SUFFIX"] = f"_{image_local_name}_{tag}{toolbox_name_suffix}"
 
-        opts = {
-            "utils_build_push_image_local_name": local_image_name,
-            "utils_build_push_image_tag": image_tag,
-            "utils_build_push_image_namespace": namespace,
+        del both_or_none
 
-            "utils_build_push_image_remote_repo": remote_repo,
-            "utils_build_push_image_remote_auth_file": remote_auth_file,
-
-            "utils_build_push_image_git_repo": git_repo,
-            "utils_build_push_image_git_ref": git_ref,
-
-            "utils_build_push_image_context_dir": context_dir,
-            "utils_build_push_image_dockerfile_path": dockerfile_path,
-
-            "utils_build_push_image_memory": memory,
-        }
-
-        return RunAnsibleRole(opts)
+        return RunAnsibleRole(locals())
