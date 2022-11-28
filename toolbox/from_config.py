c4878a7806933d2f7bac1e7f0e50066671e9b9fc
Kevin Pouget <kpouget@redhat.com>
2022-11-28 10:37:26 +0100

    toolbox: from_config: fix crash when the command key is not found

--- toolbox/from_config.py
+++ toolbox/from_config.py
@@ -87,7 +87,7 @@ class FromConfig:
         try:
             command_args = command_args[command_key].copy()
         except KeyError:
-            logging.error(f"key '{config_key}' not found. Available keys: \n-"
+            logging.error(f"key '{command_key}' not found. Available keys: \n-"
                           + "\n- ".join(sorted(command_args.keys())))
             raise SystemExit(1)
 
