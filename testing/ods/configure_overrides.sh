9e040a35b472cf0d06f82078f193461ed39674b6
Kevin Pouget <kpouget@redhat.com>
2022-11-30 22:00:01 +0100

    testing: ods: add ci_presets.name configurations

--- testing/ods/configure_overrides.sh
+++ testing/ods/configure_overrides.sh
@@ -9,15 +9,53 @@ TESTING_ODS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 
 source "$TESTING_ODS_DIR/configure.sh"
 
-if ! [[ -f "${ARTIFACT_DIR:-}/variable_overrides" ]]; then
-    echo "Nothing to override."
-    exit 0
-fi
+do_override() {
+    echo "Configuration overrides:"
+    while read line; do
+        key=$(echo "$line" | cut -d= -f1)
+        value=$(echo "$line" | cut -d= -f2- | cut -d\' -f2)
+        echo "$key --> '$value'"
+        set_config "$key" "$value"
+    done < "${ARTIFACT_DIR:-}/variable_overrides"
+}
 
-echo "Configuration overrides:"
-while read line; do
-    key=$(echo "$line" | cut -d= -f1)
-    value=$(echo "$line" | cut -d= -f2- | cut -d\' -f2)
-    echo "$key --> '$value'"
-    set_config "$key" "$value"
-done < "${ARTIFACT_DIR:-}/variable_overrides"
+update_with_presets() {
+    local name=$1
+
+    local values=$(get_config "ci_presets.${name}")
+    for key in $(echo "$values" | jq -r '. | keys[]'); do
+        local value=$(echo $values | jq -r '.["'$key'"]')
+
+        if [[ "$key" == "extends" ]]; then
+            for extend_presets_name in $(echo $value | jq -r '.[]'); do
+                update_with_presets "$extend_presets_name"
+            done
+
+            continue
+        fi
+
+        echo "presets[$name] $key --> $value"
+        set_config "$key" "$value"
+    done
+}
+
+main() {
+    if ! [[ -f "${ARTIFACT_DIR:-}/variable_overrides" ]]; then
+        echo "Nothing to override."
+        exit 0
+    fi
+
+    do_override
+
+    local ci_preset_name=$(get_config ci_presets.name)
+    if [[ "$ci_preset_name" == null ]]; then
+        exit 0
+    fi
+
+    update_with_presets "$ci_preset_name"
+
+    # re-do the override, so that preset values can be overriden
+    do_override
+}
+
+main "$@"
