79969f88b6007579b75178bd525483b1cb78b4c3
Kevin Pouget <kpouget@redhat.com>
2023-01-09 09:39:48 +0100

    local_ci_run: add pr_config: Optional path to a PR config file

--- toolbox/local_ci.py
+++ toolbox/local_ci.py
@@ -22,6 +22,7 @@ class LocalCI:
             export_identifier="default",
             export_ts_id=None,
             export=True,
+            pr_config=None,
             ):
         """
         Runs a given CI command
@@ -40,6 +41,8 @@ class LocalCI:
             export_ts_id: Timestamp identifier of the test being executed (will be a dirname).
             export_command: Command to run to export the execution artifacts to a external storage.
             export: If False, do not run the export command.
+
+            pr_config: Optional path to a PR config file (avoids fetching Github PR json).
         """
 
         if not export_ts_id:
