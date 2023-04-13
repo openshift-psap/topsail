738b5e15588ee283e434833556360591bc31b9dc
Kevin Pouget <kpouget@redhat.com>
2023-04-13 09:41:19 +0200

    testing: notebooks: scale_test.sh: split into multiple files

---
+++ testing/notebooks/prepare_common.sh
@@ -0,0 +1,106 @@
+#! /bin/bash
+
+tag_spot_machineset() {
+    local cluster_role="$1"
+    local name="$2"
+
+    local spot_tags=$(get_config clusters.create.ocp.spot.tags)
+    local cluster_tags=$(get_config clusters.create.ocp.tags)
+    local tags=$((echo -e "$spot_tags\n$cluster_tags") | jq -s '.[0] * .[1]' |  jq '. | to_entries | .[]' --compact-output)
+    while read tag; do
+        local tag_key=$(echo "$tag" | jq .key -r)
+        local tag_value=$(echo "$tag" | jq .value -r)
+
+        oc get machineset "$name" -ojson -n openshift-machine-api \
+            | jq \
+                  --arg name "$tag_key" \
+                  --arg value "$tag_value" \
+                  '.spec.template.spec.providerSpec.value.tags += [{"name": $name, "value": $value}]' \
+            | oc apply -f-
+    done <<< "$tags"
+
+    oc get machineset "$name" -oyaml -n openshift-machine-api > "$ARTIFACT_DIR/${cluster_role}_machineset_spot_tagged.yaml"
+}
+
+DRIVER_CLUSTER=driver
+SUTEST_CLUSTER=sutest
+
+switch_sutest_cluster() {
+    switch_cluster "$SUTEST_CLUSTER"
+}
+
+switch_driver_cluster() {
+    switch_cluster "$DRIVER_CLUSTER"
+}
+
+switch_cluster() {
+    local cluster_role="$1"
+    echo "Switching to the '$cluster_role' cluster"
+    if [[ "$cluster_role" == "$DRIVER_CLUSTER" ]]; then
+        export KUBECONFIG=$KUBECONFIG_DRIVER
+    elif [[ "$cluster_role" == "$SUTEST_CLUSTER" ]]; then
+        export KUBECONFIG=$KUBECONFIG_SUTEST
+    else
+        echo "Requested to switch to an unknown cluster '$cluster_role', exiting."
+        exit 1
+    fi
+    export ARTIFACT_TOOLBOX_NAME_PREFIX="${cluster_role}_"
+}
+
+capture_environment() {
+    switch_sutest_cluster
+
+    ./run_toolbox.py cluster capture_environment > /dev/null || true
+
+    switch_driver_cluster
+    ./run_toolbox.py cluster capture_environment > /dev/null || true
+}
+
+prepare_ci() {
+    cluster_helpers::connect_sutest_cluster
+
+    trap "set +e; sutest_cleanup; driver_cleanup; exit 1" ERR
+}
+
+prepare_notebook_performance_without_rhods() {
+    local namespace=$(get_command_arg namespace rhods benchmark_notebook_performance)
+    oc create namespace "$namespace" -oyaml --dry-run=client | oc apply -f-
+
+    local dedicated="{}" # set the toleration/node-selector annotations
+    if ! test_config clusters.sutest.compute.dedicated; then
+        dedicated="{value: ''}" # delete the toleration/node-selector annotations, if it exists
+    fi
+
+    ./run_toolbox.py from_config cluster set_project_annotation --prefix sutest --suffix single_notebook_node_selector --extra "$dedicated"
+    ./run_toolbox.py from_config cluster set_project_annotation --prefix sutest --suffix single_notebook_toleration --extra "$dedicated"
+}
+
+prepare() {
+    prepare_notebook_performance_without_rhods
+
+    local test_flavor=$(get_config tests.notebooks.test_flavor)
+    if [[ "$test_flavor" == "notebook-performance" ]]; then
+
+        if ! test_config tests.notebooks.notebook_performance.use_rhods; then
+            _info "Skip cluster preparation (running the notebook-performance test without using RHODS)"
+
+            return
+        fi
+    fi
+
+    if [[ "${JOB_NAME_SAFE:-}" == "notebooks-light" ]]; then
+        local user_count=$(get_config tests.notebooks.users.count)
+        local light_test_user_count=$(get_config 'ci_presets.notebooks_light["tests.notebooks.users.count"]')
+        if [[ "$user_count" -gt "$light_test_user_count" ]]; then
+            _error "Job '$JOB_NAME_SAFE' shouldn't run with more than $light_test_user_count. Found $user_count."
+            exit 1 # shouldn't be reached, but just to be 100% sure.
+        fi
+    fi
+
+    process_ctrl::run_in_bg prepare_sutest_cluster
+    process_ctrl::run_in_bg prepare_driver_cluster
+
+    process_ctrl::wait_bg_processes
+
+    sutest_wait_rhods_launch
+}
---
+++ testing/notebooks/scale_test_run_tests.sh
@@ -0,0 +1,153 @@
+#! /bin/bash
+
+run_normal_tests_and_plots() {
+    local BASE_ARTIFACT_DIR="$ARTIFACT_DIR"
+
+    local test_flavor=$(get_config tests.notebooks.test_flavor)
+
+    local test_failed=0
+    local plot_failed=0
+    local test_runs=$(get_config tests.notebooks.repeat)
+
+    for idx in $(seq "$test_runs"); do
+        if [[ "$test_runs" != 1 ]]; then
+            export ARTIFACT_DIR="$BASE_ARTIFACT_DIR/$(printf "%03d" $idx)__test_run"
+        fi
+
+        mkdir -p "$ARTIFACT_DIR"
+        local pr_file="$BASE_ARTIFACT_DIR"/pull_request.json
+        local pr_comment_file="$BASE_ARTIFACT_DIR"/pull_request-comments.json
+        for f in "$pr_file" "$pr_comment_file"; do
+            [[ -f "$f" ]] && cp "$f" "$ARTIFACT_DIR" || true
+        done
+
+        run_test "$idx" && test_failed=0 || test_failed=1
+
+        generate_plots || plot_failed=1
+
+        if [[ "$test_failed" == 1 ]]; then
+            break
+        fi
+    done
+
+    if [[ "$plot_failed" != 0 ]]; then
+        echo "One of the plotting step failed :/ "
+        return "$plot_failed"
+    fi
+
+    if [[ "$test_failed" != 0 ]]; then
+        echo "One of the testing step failed :/"
+        return "$test_failed"
+    fi
+
+    return 0
+}
+
+run_locust_test() {
+    switch_driver_cluster
+    ./run_toolbox.py from_config rhods notebook_locust_scale_test
+
+    local last_test_dir=$(printf "%s\n" "$ARTIFACT_DIR"/*__*/ | tail -1)
+    set_config matbench.test_directory "$last_test_dir"
+}
+
+run_gating_tests_and_plots() {
+    local BASE_ARTIFACT_DIR="$ARTIFACT_DIR"
+
+    do_cleanup() {
+        sutest_cleanup_rhods
+    }
+
+    cp "$CI_ARTIFACTS_FROM_CONFIG_FILE" "$ARTIFACT_DIR/config.base.yaml"
+    local test_idx=0
+    local failed=0
+
+    do_cleanup
+
+    for preset in $(get_config tests.notebooks.gating_tests[])
+    do
+        test_idx=$((test_idx + 1)) # start at 1, 0 is prepare_steps
+
+        # Wait a few minutes if this isn't the first test
+        if [[ $test_idx -ne 1 ]]; then
+            local WAIT_TIME=5m
+            echo "Waiting $WAIT_TIME for the cluster to cool down before running the next test."
+            sleep 5m
+        fi
+
+        # restore the initial configuration
+        cp "$BASE_ARTIFACT_DIR/config.base.yaml" "$CI_ARTIFACTS_FROM_CONFIG_FILE"
+
+        apply_preset "$preset"
+
+        export ARTIFACT_DIR="$BASE_ARTIFACT_DIR/$(printf "%03d" $test_idx)__$preset/000__prepare_steps"
+
+        prepare_failed=0
+        if ! prepare; then
+            prepare_failed=1
+        else
+            prepare_failed=0
+        fi
+
+        do_cleanup
+
+        export ARTIFACT_DIR="$BASE_ARTIFACT_DIR/$(printf "%03d" $test_idx)__$preset"
+
+        if [[ "$prepare_failed" == 1 ]]; then
+            ARTIFACT_DIR="$BASE_ARTIFACT_DIR" _warning "Gating preset '$preset' preparation failed :/"
+            failed=1
+            continue
+        fi
+
+        if ! run_normal_tests_and_plots; then
+            ARTIFACT_DIR="$BASE_ARTIFACT_DIR" _warning "Gating preset '$preset' test failed :/"
+            failed=1
+        fi
+    done
+
+    export ARTIFACT_DIR="$BASE_ARTIFACT_DIR"
+    if [[ "$failed" == 1 ]]; then
+        _warning "Gating test failed :/"
+    fi
+
+    return $failed
+}
+
+run_tests_and_plots() {
+    local test_flavor=$(get_config tests.notebooks.test_flavor)
+
+    sutest_cleanup_rhods
+
+    if [[ "$test_flavor" == "gating" ]]; then
+        run_gating_tests_and_plots
+    else
+        run_normal_tests_and_plots
+    fi
+}
+
+run_test() {
+    local repeat_idx=${1:-}
+
+    local test_flavor=$(get_config tests.notebooks.test_flavor)
+    if [[ "$repeat_idx" ]]; then
+        mkdir -p "$ARTIFACT_DIR"
+        echo "repeat=$repeat_idx" > "$ARTIFACT_DIR/settings.repeat"
+    fi
+    if [[ "$test_flavor" == "ods-ci" ]]; then
+        run_ods_ci_test || return 1
+    elif [[ "$test_flavor" == "locust" ]]; then
+        run_locust_test "$repeat_idx" || return 1
+    elif [[ "$test_flavor" == "notebook-performance" ]]; then
+        run_single_notebook_tests "$repeat_idx" || return 1
+    elif [[ "$test_flavor" == "gating" ]]; then
+        # 'gating' testing is handled higher in the call stack, before the 'repeat' (in run_gating_tests_and_plots)
+        _error "Test flavor cannot be '$test_flavor' in function run_test."
+    else
+        _error "Unknown test flavor: $test_flavor"
+    fi
+
+    switch_sutest_cluster
+    if ! ./run_toolbox.py rhods capture_state > /dev/null; then
+        _warning "rhods capture state failed :("
+    fi
+}
