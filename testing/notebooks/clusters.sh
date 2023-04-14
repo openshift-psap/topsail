193d7a40e7cfaaf250be0e942e600a09bcaa2ff5
Kevin Pouget <kpouget@redhat.com>
2023-04-14 09:03:47 +0200

    testing: notebooks: cluster: export a size-checking function

--- testing/notebooks/clusters.sh
+++ testing/notebooks/clusters.sh
@@ -10,5 +10,27 @@ TESTING_NOTEBOOKS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 
 TESTING_UTILS_DIR="$TESTING_NOTEBOOKS_DIR/../utils"
 
 source "$TESTING_NOTEBOOKS_DIR/configure.sh"
+source "$TESTING_UTILS_DIR/logging.sh"
+
+clusters_create__check_test_size() {
+    if [[ "${OPENSHIFT_CI:-}" == true ]]; then
+        if [[ "$JOB_NAME_SAFE" != *-long ]]; then
+            # not running in a long test
+
+            if [[ "$(get_config tests.notebooks.test_flavor)" == gating ]]; then
+                _error "refusing to run the notebook gating scale test outside of a '-long' test. (JOB_NAME_SAFE=$JOB_NAME_SAFE)"
+                return 1
+            fi
+
+            local user_count=$(get_config tests.notebooks.users.count)
+            if [[ "$user_count" -gt 300 ]]; then
+                _error "refusing to run the notebook scale test with $user_count users outside of a '-long' test. (JOB_NAME_SAFE=$JOB_NAME_SAFE)"
+                return 1
+            fi
+        fi
+    fi
+}
+
+export -f clusters_create__check_test_size
 
 exec "$TESTING_UTILS_DIR/openshift_clusters/clusters.sh" "$@"
