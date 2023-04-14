40249186c11496f46304127b7105538f95e58b91
Kevin Pouget <kpouget@redhat.com>
2023-04-14 09:04:03 +0200

    testing: pipelines: cluster: export a size-checking function

--- testing/pipelines/clusters.sh
+++ testing/pipelines/clusters.sh
@@ -11,4 +11,10 @@ TESTING_UTILS_DIR="$TESTING_PIPELINES_DIR/../utils"
 
 source "$TESTING_PIPELINES_DIR/configure.sh"
 
+clusters_create__check_test_size() {
+    echo "Nothing to do yet to check the size of the Pipelines cluster. All good."
+}
+
+export -f clusters_create__check_test_size
+
 exec "$TESTING_UTILS_DIR/openshift_clusters/clusters.sh" "$@"
