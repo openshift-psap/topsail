8bab7fa4e16f5d538511d381eee14520b02b28f5
Kevin Pouget <kpouget@redhat.com>
2023-04-21 16:42:11 +0200

    testing: utils: generate_plot_index: properly generate the links

--- testing/utils/generate_plot_index.py
+++ testing/utils/generate_plot_index.py
@@ -4,24 +4,29 @@ import glob
 import sys, os
 import pathlib
 
+ARTIFACT_DIR = pathlib.Path(os.environ["ARTIFACT_DIR"])
+
+def add_entry(file_path):
+    print(f"<li><a href='{file_path.relative_to(ARTIFACT_DIR)}'>{file_path.name}</a></li>")
+
 def report_index_to_html(report_index):
     report_dir = report_index.parent
 
     report_parent = report_dir.parent
     print()
-    print(f"<h1>{report_parent}</h1>")
+    print(f"<h1>{report_parent.relative_to(ARTIFACT_DIR)}</h1>")
     print("<ul>")
     for report_file in sorted(report_dir.glob("*.html")):
         if report_file.name == "reports_index.html": continue
-        print(f"<li><a href='{report_file}'>{report_file.name}</a></li>")
+        add_entry(report_file)
+
 
     print("<br>")
     for json_file in sorted(report_dir.glob("*.json")):
-        print(f"<li><a href='{json_file}'>{json_file.name}</a></li>")
+        add_entry(json_file)
     print("</ul>")
 
 def main():
-    ARTIFACT_DIR = pathlib.Path(os.environ["ARTIFACT_DIR"])
     for report_index in sorted(ARTIFACT_DIR.glob('**/reports_index.html')):
         report_index_to_html(pathlib.Path(report_index))
 
