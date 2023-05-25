d8d44cd6ca9ddd74c90b3620c9fc2c744756f21c
Kevin Pouget <kpouget@redhat.com>
2023-05-25 10:48:35 +0200

    testing: utils/local-ci: update with the new location 'utils local-ci'

--- testing/utils/local-ci/configure.sh
+++ testing/utils/local-ci/configure.sh
@@ -1,13 +1,13 @@
 if [ -n "$BASH_VERSION" ]; then
     # assume Bash
-    TESTING_LOCAL_CI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    TESTING_UTILS_LOCALCI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 elif [ -n "$ZSH_VERSION" ]; then
     # assume ZSH
-    TESTING_LOCAL_CI_DIR=${0:a:h}
-elif [[ -z "${TESTING_LOCAL_CI_DIR:-}" ]]; then
-     echo "Shell isn't bash nor zsh, please expose the directory of this file with TESTING_LOCAL_CI_DIR."
+    TESTING_UTILS_LOCALCI_DIR=${0:a:h}
+elif [[ -z "${TESTING_UTILS_LOCALCI_DIR:-}" ]]; then
+     echo "Shell isn't bash nor zsh, please expose the directory of this file with TESTING_UTILS_LOCALCI_DIR."
      false
 fi
 
-export CI_ARTIFACTS_FROM_COMMAND_ARGS_FILE=${TESTING_LOCAL_CI_DIR}/command_args.yaml
-export CI_ARTIFACTS_FROM_CONFIG_FILE=${TESTING_LOCAL_CI_DIR}/config.yaml
+export CI_ARTIFACTS_FROM_COMMAND_ARGS_FILE=${TESTING_UTILS_LOCALCI_DIR}/command_args.yaml
+export CI_ARTIFACTS_FROM_CONFIG_FILE=${TESTING_UTILS_LOCALCI_DIR}/config.yaml
