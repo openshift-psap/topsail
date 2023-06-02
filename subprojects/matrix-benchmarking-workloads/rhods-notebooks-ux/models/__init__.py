85fbd60d93f27e38754a021b56e3fdf72e889bf3
Keith Valin <kvalin@redhat.com>
2023-06-02 10:34:56 -0400

    All data fields added for the notebook scale test

--- subprojects/matrix-benchmarking-workloads/rhods-notebooks-ux/models/__init__.py
+++ subprojects/matrix-benchmarking-workloads/rhods-notebooks-ux/models/__init__.py
@@ -1,6 +1,6 @@
 from . import user
 
-from typing import List
+from typing import List, Dict
 
 import matrix_benchmarking.models as models
 
@@ -8,6 +8,12 @@ from pydantic import BaseModel
 
 class NotebookScaleData(BaseModel):
     users: List[user.UserData]
+    config: BaseModel
+    metrics: Dict[str, models.PrometheusMetric]
+    thresholds: BaseModel
+    ocp_version: models.SemVer
+    rhods_version: models.SemVer
+
 
 class NotebookScalePayload(models.PSAPPayload):
     data: NotebookScaleData
