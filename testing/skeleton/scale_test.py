828aa160bc3e93b48f426bb5bbcc439072e87c56
Kevin Pouget <kpouget@redhat.com>
2023-06-06 15:32:11 +0200

    testing: skeleton: new CI entrypoint

---
+++ testing/skeleton/scale_test.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+import sys, os
+import pathlib
+import subprocess
+import logging
+logging.getLogger().setLevel(logging.INFO)
+import datetime
+import time
+import functools
+
+import yaml
+import fire
+
+TESTING_THIS_DIR = pathlib.Path(__file__).absolute().parent
+TESTING_UTILS_DIR = TESTING_THIS_DIR.parent / "utils"
+PSAP_ODS_SECRET_PATH = pathlib.Path(os.environ.get("PSAP_ODS_SECRET_PATH", "/env/PSAP_ODS_SECRET_PATH/not_set"))
+LIGHT_PROFILE = "light"
+
+sys.path.append(str(TESTING_THIS_DIR.parent))
+from common import env, config, run, rhods, visualize
+
+
+initialized = False
+def init(ignore_secret_path=False, apply_preset_from_pr_args=True):
+    global initialized
+    if initialized:
+        logging.debug("Already initialized.")
+        return
+    initialized = True
+
+    env.init()
+    config.init(TESTING_THIS_DIR)
+
+    if apply_preset_from_pr_args:
+        config.ci_artifacts.apply_preset_from_pr_args()
+
+    if not ignore_secret_path and not PSAP_ODS_SECRET_PATH.exists():
+        raise RuntimeError("Path with the secrets (PSAP_ODS_SECRET_PATH={PSAP_ODS_SECRET_PATH}) does not exists.")
+
+    if os.environ.get("JOB_NAME_SAFE", "").endswith("-light"):
+        logging.info(f"Running a light test, applying the '{LIGHT_PROFILE}' profile")
+        config.ci_artifacts.apply_preset(LIGHT_PROFILE)
+
+
+def entrypoint(ignore_secret_path=False, apply_preset_from_pr_args=True):
+    def decorator(fct):
+        @functools.wraps(fct)
+        def wrapper(*args, **kwargs):
+            init(ignore_secret_path, apply_preset_from_pr_args)
+            fct(*args, **kwargs)
+
+        return wrapper
+    return decorator
+
+# ---
+
+@entrypoint()
+def prepare_ci():
+    """
+    Prepares the cluster and the namespace for running Load-Aware scale tests
+    """
+
+    logging.info("Nothing to do to prepare the cluster.")
+
+    run.run("./run_toolbox.py from_config cluster capture_environment --suffix sample")
+
+
+
+def _run_test(test_artifact_dir_p):
+    """
+    Runs the Load-Aware scale test from the CI
+    """
+
+    next_count = env.next_artifact_index()
+    with env.TempArtifactDir(env.ARTIFACT_DIR / f"{next_count:03d}__dummy_test"):
+        test_artifact_dir_p[0] = env.ARTIFACT_DIR
+
+        with open(env.ARTIFACT_DIR / "settings", "w") as f:
+            print(f"dummy=true", file=f)
+
+        with open(env.ARTIFACT_DIR / "config.yaml", "w") as f:
+            yaml.dump(config.ci_artifacts.config, f, indent=4)
+
+        failed = True
+        try:
+            run.run("./run_toolbox.py cluster reset_prometheus_db")
+
+            logging.info("Waiting 5 minutes to capture some metrics in Prometheus ...")
+            time.sleep(5 * 60)
+
+            run.run("./run_toolbox.py cluster dump_prometheus_db")
+            failed = False
+        finally:
+            with open(env.ARTIFACT_DIR / "exit_code", "w") as f:
+                print("1" if failed else "0", file=f)
+
+            run.run("./run_toolbox.py from_config cluster capture_environment --suffix sample")
+
+@entrypoint()
+def test_ci():
+    """
+    Runs the Load-Aware scale test from the CI
+    """
+
+    try:
+        test_artifact_dir_p = [None]
+        _run_test(test_artifact_dir_p)
+    finally:
+        if test_artifact_dir_p[0] is not None:
+            next_count = env.next_artifact_index()
+            with env.TempArtifactDir(env.ARTIFACT_DIR / f"{next_count:03d}__plots"):
+                visualize.prepare_matbench()
+                generate_plots(test_artifact_dir_p[0])
+        else:
+            logging.warning("Not generating the visualization as the test artifact directory hasn't been created.")
+
+
+@entrypoint(ignore_secret_path=True, apply_preset_from_pr_args=False)
+def generate_plots_from_pr_args():
+    """
+    Generates the Load-Aware plots from the PR arguments
+    """
+
+    visualize.download_and_generate_visualizations()
+
+
+@entrypoint()
+def cleanup_cluster():
+    """
+    Restores the cluster to its original state
+    """
+    # _Not_ executed in OpenShift CI cluster (running on AWS). Only required for running on bare-metal environments.
+
+    logging.info("Nothing to do to cleanup the cluster.")
+
+
+@entrypoint(ignore_secret_path=True)
+def generate_plots(results_dirname):
+    visualize.generate_from_dir(str(results_dirname))
+
+
+# ---
+
+class Entrypoint:
+    """
+    Commands for launching the Load-Aware CI tests
+    """
+
+    def __init__(self):
+        self.cleanup_cluster_ci = cleanup_cluster
+
+        self.prepare_ci = prepare_ci
+        self.test_ci = test_ci
+
+        self.generate_plots_from_pr_args = generate_plots_from_pr_args
+        self.generate_plots = generate_plots
+
+def main():
+    # Print help rather than opening a pager
+    fire.core.Display = lambda lines, out: print(*lines, file=out)
+
+    fire.Fire(Entrypoint())
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Command '{e.cmd}' failed --> {e.returncode}")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print() # empty line after ^C
+        logging.error(f"Interrupted.")
+        sys.exit(1)
