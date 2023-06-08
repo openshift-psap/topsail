8db0851107d069d696b13011a781d33147355a12
Kevin Pouget <kpouget@redhat.com>
2023-06-08 23:03:14 +0200

    [matrix-benchmarking-workloads/rhods-pipelines] define LTS metrics

--- subprojects/matrix-benchmarking-workloads/rhods-pipelines/models/lts.py
+++ subprojects/matrix-benchmarking-workloads/rhods-pipelines/models/lts.py
@@ -1,7 +1,8 @@
-import matrix_benchmarking.models as matbench_models
-
 from typing import Any, Dict, List, Optional, Union
 
+from pydantic import BaseModel, Field
+
+import matrix_benchmarking.models as matbench_models
 from .. import models as pipelines_models
 
 class PipelinesScaleTestMetadata(matbench_models.Metadata):
@@ -12,9 +13,16 @@ class PipelinesScaleTestMetadata(matbench_models.Metadata):
     user_count: int
     config: Any
 
+class PipelinesScaleTestSutestMetrics(matbench_models.ExclusiveModel):
+    control_plane_node_cpu_idle: matbench_models.PrometheusValues = Field(..., alias="Sutest Control Plane Node CPU idle")
+    control_plane_node_cpu_usage: matbench_models.PrometheusValues = Field(..., alias="Sutest Control Plane Node CPU usage")
+    api_server_request_server_errors: matbench_models.PrometheusValues = Field(..., alias="Sutest API Server Requests (server errors)")
+
+class PipelinesScaleTestMetrics(matbench_models.ExclusiveModel):
+    sutest: PipelinesScaleTestSutestMetrics
+
 class PipelinesScaleTestResults(matbench_models.ExclusiveModel):
-    # nothing for now
-    ...
+    metrics: PipelinesScaleTestMetrics
 
 class PipelinesScaleTestPayload(matbench_models.ExclusiveModel):
     metadata: PipelinesScaleTestMetadata
--- subprojects/matrix-benchmarking-workloads/rhods-pipelines/store/lts.py
+++ subprojects/matrix-benchmarking-workloads/rhods-pipelines/store/lts.py
@@ -26,7 +26,9 @@ def build_lts_payloads():
         )
 
         lts_results = models_lts.PipelinesScaleTestResults(
-            # nothing for now
+            metrics = models_lts.PipelinesScaleTestMetrics(
+                sutest = _gather_prom_metrics(entry.results.metrics.sutest, models_lts.PipelinesScaleTestSutestMetrics),
+            ),
         )
 
         lts_payload = models_lts.PipelinesScaleTestPayload(
@@ -42,3 +44,9 @@ def _parse_lts_dir(add_to_matrix, dirname, import_settings):
 
 def register_lts_metric(cluster_role, metric):
     pass
+
+def _gather_prom_metrics(metrics, model) -> dict:
+    data = {metric_name: metrics[metric_name]
+            for metric_name in model.schema()["properties"].keys()}
+
+    return model(**data)
