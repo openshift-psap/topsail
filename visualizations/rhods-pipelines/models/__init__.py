602f22b564ed845e659832d9553762a7d688a805
Kevin Pouget <kpouget@redhat.com>
2023-06-14 20:57:00 +0200

    visualizations: rhods-pipelines: models: make all the fields as Optional

--- visualizations/rhods-pipelines/models/__init__.py
+++ visualizations/rhods-pipelines/models/__init__.py
@@ -5,29 +5,28 @@ import pathlib
 
 from typing import Any, Dict, List, Optional, Union
 from pydantic import BaseModel, Field, constr
-
+import pydantic
 
 import matrix_benchmarking.models as matbench_models
 
-
-class PipelinesScaleMetadata(matbench_models.Metadata):
+class PipelinesScaleMetadata(matbench_models.Metadata, metaclass=matbench_models.AllOptional):
     settings: Dict[str, str]
     ocp_version: matbench_models.SemVer
     rhods_version: matbench_models.SemVer
     config: BaseModel
 
 
-class PipelinesScaleResults(matbench_models.ExclusiveModel):
+class PipelinesScaleResults(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     results: BaseModel
 
 
-class FromLocalEnv(matbench_models.ExclusiveModel):
+class FromLocalEnv(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     artifacts_basedir: pathlib.Path
     source_url: Union[str, pathlib.Path, None]
     is_interactive: bool
 
 
-class NodeItem(matbench_models.ExclusiveModel):
+class NodeItem(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     name: str
     sutest_cluster: bool
     managed: bool
@@ -38,7 +37,7 @@ class NodeItem(matbench_models.ExclusiveModel):
     infra: bool
 
 
-class RhodsClusterInfo(matbench_models.ExclusiveModel):
+class RhodsClusterInfo(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     node_count: List[NodeItem]
     control_plane: List[NodeItem]
     infra: List[NodeItem]
@@ -46,35 +45,35 @@ class RhodsClusterInfo(matbench_models.ExclusiveModel):
     test_pods_only: List[NodeItem]
 
 
-class RhodsInfo(matbench_models.ExclusiveModel):
+class RhodsInfo(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     version: str
     createdAt_raw: str
     createdAt: datetime.datetime
     full_version: str
 
-class UserDataEntry(matbench_models.ExclusiveModel):
+class UserDataEntry(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     artifact_dir: pathlib.Path
     exit_code: int
     progress: Dict[str, datetime.datetime]
 
 
-class TesterJob(matbench_models.ExclusiveModel):
+class TesterJob(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     creation_time: datetime.datetime
     completion_time: datetime.datetime
     env: Dict[str, str]
 
 
-class MetricEntry(matbench_models.ExclusiveModel):
+class MetricEntry(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     metric: Dict[str, str]
     values: List[List[Union[int, str]]]
 
 
-class Metrics(matbench_models.ExclusiveModel):
+class Metrics(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     sutest: matbench_models.PrometheusNamedMetricValues
     driver: matbench_models.PrometheusNamedMetricValues
 
 
-class PodTime(matbench_models.ExclusiveModel):
+class PodTime(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     user_index: int
     pod_name: str
     pod_friendly_name: str
@@ -90,7 +89,7 @@ class PodTime(matbench_models.ExclusiveModel):
     is_dspa: bool = Field(default_factory=lambda: False)
     is_pipeline_task: bool = Field(default_factory=lambda: False)
 
-class ParsedResultsModel(matbench_models.ExclusiveModel):
+class ParsedResultsModel(matbench_models.ExclusiveModel, metaclass=matbench_models.AllOptional):
     parser_version: str
     artifacts_version: str
     from_local_env: FromLocalEnv
