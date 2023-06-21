dbb2310345547c20fa807fee77bd8a1c8379e331
Kevin Pouget <kpouget@redhat.com>
2023-06-21 09:03:37 +0200

    testing: middleware: simplify the skeleton

--- testing/middleware/test.py
+++ testing/middleware/test.py
@@ -39,9 +39,6 @@ def init(ignore_secret_path=False, apply_preset_from_pr_args=True):
         raise RuntimeError("Path with the secrets (PSAP_ODS_SECRET_PATH={PSAP_ODS_SECRET_PATH}) does not exists.")
 
 
-    config.ci_artifacts.detect_apply_light_profile(LIGHT_PROFILE)
-
-
 def entrypoint(ignore_secret_path=False, apply_preset_from_pr_args=True):
     def decorator(fct):
         @functools.wraps(fct)
@@ -62,35 +59,6 @@ def prepare_ci():
 
     logging.info("Nothing to do to prepare the cluster.")
 
-    run.run("./run_toolbox.py from_config cluster capture_environment --suffix sample")
-
-
-
-def _run_test(test_artifact_dir_p):
-    next_count = env.next_artifact_index()
-    with env.TempArtifactDir(env.ARTIFACT_DIR / f"{next_count:03d}__dummy_test"):
-        test_artifact_dir_p[0] = env.ARTIFACT_DIR
-
-        with open(env.ARTIFACT_DIR / "settings", "w") as f:
-            print(f"dummy=true", file=f)
-
-        with open(env.ARTIFACT_DIR / "config.yaml", "w") as f:
-            yaml.dump(config.ci_artifacts.config, f, indent=4)
-
-        failed = True
-        try:
-            run.run("./run_toolbox.py cluster reset_prometheus_db")
-
-            logging.info("Waiting 5 minutes to capture some metrics in Prometheus ...")
-            time.sleep(5 * 60)
-
-            run.run("./run_toolbox.py cluster dump_prometheus_db")
-            failed = False
-        finally:
-            with open(env.ARTIFACT_DIR / "exit_code", "w") as f:
-                print("1" if failed else "0", file=f)
-
-            run.run("./run_toolbox.py from_config cluster capture_environment --suffix sample")
 
 @entrypoint()
 def test_ci():
@@ -98,28 +66,8 @@ def test_ci():
     Runs the test from the CI
     """
 
-    try:
-        test_artifact_dir_p = [None]
-        _run_test(test_artifact_dir_p)
-    finally:
-        if test_artifact_dir_p[0] is not None:
-            next_count = env.next_artifact_index()
-            with env.TempArtifactDir(env.ARTIFACT_DIR / f"{next_count:03d}__plots"):
-                visualize.prepare_matbench()
-                generate_plots(test_artifact_dir_p[0])
-        else:
-            logging.warning("Not generating the visualization as the test artifact directory hasn't been created.")
-
-        if config.ci_artifacts.get_config("clusters.cleanup_on_exit"):
-            cleanup_cluster()
-
-@entrypoint(ignore_secret_path=True, apply_preset_from_pr_args=False)
-def generate_plots_from_pr_args():
-    """
-    Generates the visualization reports from the PR arguments
-    """
-
-    visualize.download_and_generate_visualizations()
+    if config.ci_artifacts.get_config("clusters.cleanup_on_exit"):
+        cleanup_cluster()
 
 
 @entrypoint()
@@ -130,11 +78,6 @@ def cleanup_cluster():
     # _Not_ executed in OpenShift CI cluster (running on AWS). Only required for running in bare-metal environments.
 
     logging.info("Nothing to do to cleanup the cluster.")
-
-
-@entrypoint(ignore_secret_path=True)
-def generate_plots(results_dirname):
-    visualize.generate_from_dir(str(results_dirname))
 
 
 # ---
@@ -149,9 +92,6 @@ class Entrypoint:
 
         self.prepare_ci = prepare_ci
         self.test_ci = test_ci
-
-        self.generate_plots_from_pr_args = generate_plots_from_pr_args
-        self.generate_plots = generate_plots
 
 def main():
     # Print help rather than opening a pager
