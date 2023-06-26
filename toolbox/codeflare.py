b8fe6a7d7ab7f842ed2c394beeb84246b14aa774
Kevin Pouget <kpouget@redhat.com>
2023-06-26 15:05:03 +0200

    codeflare_generate_mcad_load: add target_states and fail_if_states

--- toolbox/codeflare.py
+++ toolbox/codeflare.py
@@ -10,12 +10,16 @@ class Codeflare:
 
     @AnsibleRole("codeflare_generate_mcad_load")
     @AnsibleMappedParams
-    def generate_mcad_load(self, namespace):
+    def generate_mcad_load(self, namespace,
+                           target_states=["Completed"],
+                           fail_if_states=["Failed"]):
         """
         Generate MCAD load
 
         Args:
           namespace: name of the namespace where the MCAD load will be generated
+          target_states: list of expected target states
+          fail_if_states: list of states that fail the test
         """
 
         return RunAnsibleRole(locals())
