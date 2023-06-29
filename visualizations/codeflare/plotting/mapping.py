3a736d89a2150c0cff6c3cfd1db1f99ad0ee2eac
Kevin Pouget <kpouget@redhat.com>
2023-06-29 09:31:37 +0200

    visualizations: codeflare: plotting/mapping: show the last state until the end of the test

--- visualizations/codeflare/plotting/mapping.py
+++ visualizations/codeflare/plotting/mapping.py
@@ -132,7 +132,14 @@ class AppWrappersTimeline():
                     ))
                 current_name = condition_name
                 current_start = condition_ts
-                # last one, completed, is ignored, as it stays "active" forever
+
+            if current_name:
+                data.append(dict(
+                    Name=resource_times.name,
+                    State=current_name,
+                    Start=current_start,
+                    Finish=entry.results.start_end_time[1], # last state lives until the end of the test
+                ))
 
         for pod_time in entry.results.pod_times:
             finish = pod_time.container_finished or entry.results.start_end_time[1]
