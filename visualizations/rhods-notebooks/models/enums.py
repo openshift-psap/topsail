7ee8e623040e55b3b7879301d0c90b63cf142692
Keith Valin <kvalin@redhat.com>
2023-06-30 09:07:57 -0400

    notebooks: Expaned StepName enum to include Jupyter test step names

--- visualizations/rhods-notebooks/models/enums.py
+++ visualizations/rhods-notebooks/models/enums.py
@@ -9,6 +9,8 @@ class StepName(matbench_models.PSAPEnum):
     Login_to_RHODS_Dashboard = auto()
     Go_to_RHODS_Dashboard = auto()
     Go_to_the_Project_page = auto()
+    Go_to_Jupyter_Page = auto()
+    Wait_for_the_Notebook_Spawn = auto()
     Create_and_Start_the_Workbench = auto()
     Login_to_JupyterLab_Page = auto()
     Go_to_JupyterLab_Page = auto()
