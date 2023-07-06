8197d8846e11f5f4353e5a1ea4f60615c18e3c38
Kevin Pouget <kpouget@redhat.com>
2023-07-06 14:10:26 +0200

    [mcad-workload-generator] visualize_schedule: plot the histogram of the distance between the arrivals

--- subprojects/mcad-workload-generator/visualize_schedule.py
+++ subprojects/mcad-workload-generator/visualize_schedule.py
@@ -10,8 +10,12 @@ logging.getLogger().setLevel(logging.INFO)
 IMAGE_HEIGHT = 650
 IMAGE_WIDTH = 1200
 
-def save(fig, name, name_suffix, artifact_dir):
+def save(fig, name, name_suffix, artifact_dir, xtitle=None):
     logging.info(f"Saving the {name} into schedule_result_{name_suffix}.{{html,png}} ...")
+
+    if xtitle:
+        fig.update_xaxes(title=xtitle)
+
     fig.write_html(artifact_dir / f"schedule_result_{name_suffix}.html")
     fig.write_image(artifact_dir / f"schedule_result_{name_suffix}.png", width=IMAGE_WIDTH, height=IMAGE_HEIGHT)
 
@@ -35,6 +39,22 @@ def main(artifact_dir=pathlib.Path("."), schedule_result=None):
     save(px.line(df, y="delay"),
          "scheduling timeline", "timeline",
          artifact_dir)
+
+    logging.info("Generating the distance histogram ...")
+    distance_data = []
+    current_time = 0
+    for index, row in df.iterrows():
+        distance_data.append(dict(
+            distance = row["delay"] - current_time
+        ))
+        current_time = row["delay"]
+
+    distance_df = pd.DataFrame(distance_data)
+    save(px.histogram(distance_df, x="distance"),
+         "distance distribution histogram", "dist_hist",
+         artifact_dir,
+         "Duration between two resource creations, in seconds",
+         )
 
 if __name__ == "__main__":
     sys.exit(main())
