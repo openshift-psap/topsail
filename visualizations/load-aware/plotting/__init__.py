4c4dcffa657f1276a807124c4d8856af98023264
Drew Ripberger <drew.ripberger@gmail.com>
2023-07-07 17:21:28 +0100

    added file for pod times

--- visualizations/load-aware/plotting/__init__.py
+++ visualizations/load-aware/plotting/__init__.py
@@ -1,9 +1,11 @@
 from . import error_report
 from ..store import prom
 from . import prom_report
+from . import pod_times
 
 def register():
     error_report.register()
     report.register()
     prom.register()
     prom_report.register()
+    pod_times.register()
--- visualizations/load-aware/store/parsers.py
+++ visualizations/load-aware/store/parsers.py
@@ -22,8 +22,7 @@ ANSIBLE_LOG_DATE_TIME_FMT = "%Y-%m-%d %H:%M:%S"
 
 IMPORTANT_FILES = [
     "config.yaml",
-    "001__load__aware__scale__test/all_default_pods.json",
-    "001__load__aware__scale__test/all_trimaran_pods.json",
+    "001__load__aware__scale__test/pods.json",
     "002__cluster__capture_environment/nodes.json",
     "002__cluster__capture_environment/ocp_version.yml",
     "003__cluster__dump_prometheus_db/prometheus.t*"
@@ -50,6 +49,7 @@ def _parse_always(results, dirname, import_settings):
     results.test_config = _parse_test_config(dirname)
 
 def _parse_once(results, dirname):
+    results.pods_info = _parse_pod_times(dirname) or {}
     results.nodes_info = _parse_nodes_info(dirname) or {}
     results.cluster_info = _extract_cluster_info(results.nodes_info)
     results.sutest_ocp_version = _parse_ocp_version(dirname)
@@ -132,6 +132,60 @@ def _parse_test_config(dirname):
 
     return test_config
 
+
+@ignore_file_not_found
+def _parse_pod_times(dirname):
+    filename = artifact_paths.LOAD_AWARE_SCHEDULER_TEST_DIR / "all_pods.json"
+
+    with open(register_important_file(dirname, filename)) as f:
+        try:
+            json_file = json.load(f)
+        except Exception as e:
+            logging.error(f"Couldn't parse JSON file '{filename}': {e}")
+            return
+
+    pod_times = []
+    for pod in json_file["items"]:
+      pod_time = types.SimpleNamespace()
+      pod_times.append(pod_time)
+
+      pod_time.pod_name = pod["metadata"]["name"]
+      pod_friendly_name = pod["metadata"]["labels"]["job-name"]
+
+      pod_time.pod_friendly_name = pod_friendly_name
+      pod_time.hostname = pod["spec"].get("nodeName")
+
+      pod_time.creation_time = datetime.datetime.strptime(
+              pod["metadata"]["creationTimestamp"], K8S_TIME_FMT)
+
+      start_time_str = pod["status"].get("startTime")
+      pod_time.start_time = None if not start_time_str else \
+          datetime.datetime.strptime(start_time_str, K8S_TIME_FMT)
+
+      for condition in pod["status"].get("conditions", []):
+          last_transition = datetime.datetime.strptime(condition["lastTransitionTime"], K8S_TIME_FMT)
+
+          if condition["type"] == "ContainersReady":
+              pod_time.containers_ready = last_transition
+
+          elif condition["type"] == "Initialized":
+              pod_time.pod_initialized = last_transition
+          elif condition["type"] == "PodScheduled":
+              pod_time.pod_scheduled = last_transition
+
+      for containerStatus in pod["status"].get("containerStatuses", []):
+          try:
+              finishedAt =  datetime.datetime.strptime(
+                  containerStatus["state"]["terminated"]["finishedAt"],
+                  K8S_TIME_FMT)
+          except KeyError: continue
+
+          # take the last container_finished found
+          if ("container_finished" not in pod_time.__dict__
+              or pod_time.container_finished < finishedAt):
+              pod_time.container_finished = finishedAt
+
+    return pod_times
 
 @ignore_file_not_found
 def _parse_nodes_info(dirname, sutest_cluster=True):
