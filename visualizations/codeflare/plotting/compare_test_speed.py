73acce9ed2a6fb1040c8b7a373917f0f1fdde3d2
Kevin Pouget <kpouget@redhat.com>
2023-07-13 13:40:40 +0200

    visualizations: codeflare: plotting: compare_test_speed: use the correct resource name (Pod or AppWrapper)
    Otherwise using 'Pod' in the plots was confusing for the readers

--- visualizations/codeflare/plotting/compare_test_speed.py
+++ visualizations/codeflare/plotting/compare_test_speed.py
@@ -39,33 +39,33 @@ class CompareTestSpeed():
         variable = list(variables)[0]
 
         text = []
-
         data = []
         for entry in common.Matrix.all_records(settings, setting_lists):
+            schedule_object_kind = "Job" if entry.results.test_case_properties.job_mode else "AppWrapper"
 
             test_cfg_setting = entry.settings.__dict__[variable]
 
             test_duration = (entry.results.test_start_end_time.end - entry.results.test_start_end_time.start).total_seconds() / 60
 
-            text += [html.Code(f"{variable}={test_cfg_setting})")]
+            text += [html.Code(f"{variable}={test_cfg_setting}")]
             def add_data(what, speed):
                 data.append(dict(
                     speed = speed,
                     what = what,
                 ))
                 data[-1][variable] = test_cfg_setting
-                text.append(f" | {what}: {speed:.2f} Pods/minutes")
+                text.append(f" | {what}: {speed:.2f} {schedule_object_kind}s/minutes")
 
-            launch_speed = entry.results.test_case_properties.total_pod_count / entry.results.test_case_properties.launch_duration
+            launch_speed = entry.results.test_case_properties.aw_count / entry.results.test_case_properties.launch_duration
             add_data("Launch speed", launch_speed)
 
-            processing_speed = entry.results.test_case_properties.total_pod_count / test_duration
+            processing_speed = entry.results.test_case_properties.aw_count / test_duration
             add_data("Processing speed", processing_speed)
 
             if entry.results.pod_times:
                 last_scheduled_pod_time = sorted(entry.results.pod_times, key=lambda t: t.pod_scheduled)[-1]
                 start_to_last_scheduled_duration = (last_scheduled_pod_time.pod_scheduled - entry.results.test_start_end_time.start).total_seconds() / 60
-                last_scheduled_speed = entry.results.test_case_properties.total_pod_count / start_to_last_scheduled_duration
+                last_scheduled_speed = entry.results.test_case_properties.aw_count / start_to_last_scheduled_duration
                 add_data("Speed to last schedule", last_scheduled_speed)
 
             text += [html.Br()]
@@ -74,7 +74,7 @@ class CompareTestSpeed():
         fig = px.line(df, x=variable, y="speed", color="what", markers=True)
 
         fig.update_layout(title=f"Comparison of the processing speed<br>over different {variable} values", title_x=0.5,)
-        fig.update_layout(yaxis_title="Processing speed, in Pod/minutes ▸")
+        fig.update_layout(yaxis_title=f"Processing speed, in {schedule_object_kind}s/minutes ▸")
         fig.update_layout(xaxis_title=variable)
 
         return fig, html.P(text)
