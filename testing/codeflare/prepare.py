58c334dd97984aec4d37b894a62a5fa3802fa3dd
Kevin Pouget <kpouget@redhat.com>
2023-07-18 21:41:40 +0200

    testing: codeflare: prepare: apply the CRD from github
    so that the last version of MCAD works properly

--- testing/codeflare/prepare.py
+++ testing/codeflare/prepare.py
@@ -49,6 +49,10 @@ def prepare_odh_customization():
         logging.info(f"Setting MCAD controller image to {image}:{tag} ...")
         run.run(f"oc set image deploy/mcad-controller-mcad mcad-controller={image}:{tag} -n {odh_namespace}")
 
+        run.run("oc delete appwrappers -A --all # delete all appwrappers")
+        run.run("oc delete crd appwrappers.mcad.ibm.com")
+        run.run("oc apply -f https://raw.githubusercontent.com/project-codeflare/multi-cluster-app-dispatcher/main/config/crd/bases/mcad.ibm.com_appwrappers.yaml")
+
     if customized:
         run.run("./run_toolbox.py from_config rhods wait_odh")
 
