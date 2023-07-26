d51615c85f4e9010a7ccea61ac0f00274e7382d5
Kevin Pouget <kpouget@redhat.com>
2023-07-26 17:31:40 +0200

    visualizations: load-aware: add multi-expe comparison

--- visualizations/load-aware/plotting/__init__.py
+++ visualizations/load-aware/plotting/__init__.py
@@ -4,6 +4,7 @@ from . import prom_report
 from . import pod_times
 from . import scheduler_report
 from . import mapping
+from . import comparison
 
 def register():
     error_report.register()
@@ -13,3 +14,4 @@ def register():
     pod_times.register()
     scheduler_report.register()
     mapping.register()
+    comparison.register()
