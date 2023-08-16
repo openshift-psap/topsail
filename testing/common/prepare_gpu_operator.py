4f1ec695bc17aaf3e85a37157fadbe09c62b1208
Kevin Pouget <kpouget@redhat.com>
2023-08-16 21:18:17 +0200

    testing: common: prepare_gpu_operator: new script to deploy the GPU operator on demand

---
+++ testing/common/prepare_gpu_operator.py
@@ -0,0 +1,19 @@
+import logging
+import pathlib
+
+from common import env, config, run
+
+
+def prepare_gpu_operator():
+    run.run("./run_toolbox.py nfd_operator deploy_from_operatorhub")
+    run.run("./run_toolbox.py gpu_operator deploy_from_operatorhub")
+    run.run("./run_toolbox.py from_config gpu_operator enable_time_sharing")
+
+
+def cleanup_gpu_operator():
+    if run.run(f'oc get project -oname nvidia-gpu-operator 2>/dev/null', check=False).returncode != 0:
+        run.run("oc delete ns nvidia-gpu-operator")
+        run.run("oc delete clusterpolicy --all")
+
+    if run.run(f'oc get project -oname openshift-nfd 2>/dev/null', check=False).returncode != 0:
+        run.run("oc delete ns openshift-nfd")
