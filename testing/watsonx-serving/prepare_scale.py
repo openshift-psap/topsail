137d582b65a0e88bb032f43fc2bcc35215384d59
Kevin Pouget <kpouget@redhat.com>
2023-08-22 20:32:04 +0200

    testing: watsonx-serving: add prepare_watsonx_serving entrypoint
    so that it can be easily reused outside of the scale test

--- testing/watsonx-serving/prepare_scale.py
+++ testing/watsonx-serving/prepare_scale.py
@@ -2,7 +2,8 @@ import logging
 import pathlib
 import os
 
-from common import env, config, run, sizing, prepare_gpu_operator, prepare_user_pods, rhods
+from common import env, config, run, sizing, prepare_gpu_operator, prepare_user_pods
+import prepare_watsonx_serving
 
 def compute_sutest_node_requirement():
     user_count = config.ci_artifacts.get_config("tests.scale.user_count")
@@ -19,18 +20,12 @@ def compute_sutest_node_requirement():
     return min(user_count, machine_count)
 
 
-PSAP_ODS_SECRET_PATH = pathlib.Path(os.environ.get("PSAP_ODS_SECRET_PATH", "/env/PSAP_ODS_SECRET_PATH/not_set"))
-
 def prepare():
     """
     Prepares the cluster and the namespace for running the Watsonx scale tests
     """
 
-    if not PSAP_ODS_SECRET_PATH.exists():
-        raise RuntimeError(f"Path with the secrets (PSAP_ODS_SECRET_PATH={PSAP_ODS_SECRET_PATH}) does not exists.")
-
-    token_file = PSAP_ODS_SECRET_PATH / config.ci_artifacts.get_config("secrets.brew_registry_redhat_io_token_file")
-    rhods.install(token_file)
+    prepare_watsonx_serving.prepare()
 
     if not config.ci_artifacts.get_config("clusters.driver.is_metal"):
         node_count = compute_sutest_node_requirement()
@@ -40,11 +35,6 @@ def prepare():
 
     if config.ci_artifacts.get_config("tests.want_gpu"):
         prepare_gpu.prepare_gpu_operator()
-
-    for operator in config.ci_artifacts.get_config("prepare.operators"):
-        run.run(f"./run_toolbox.py cluster deploy_operator {operator['catalog']} {operator['name']} {operator['namespace']}")
-
-    run.run("testing/watsonx-serving/poc/prepare.sh | tee -a $ARTIFACT_DIR/000_prepare_sh.log")
 
     namespace = config.ci_artifacts.get_config("base_image.namespace")
     prepare_user_pods.prepare_user_pods(namespace)
---
+++ testing/watsonx-serving/prepare_watsonx_serving.py
@@ -0,0 +1,18 @@
+import pathlib
+import os
+
+from common import env, config, run, rhods
+
+PSAP_ODS_SECRET_PATH = pathlib.Path(os.environ.get("PSAP_ODS_SECRET_PATH", "/env/PSAP_ODS_SECRET_PATH/not_set"))
+
+def prepare():
+    if not PSAP_ODS_SECRET_PATH.exists():
+        raise RuntimeError(f"Path with the secrets (PSAP_ODS_SECRET_PATH={PSAP_ODS_SECRET_PATH}) does not exists.")
+
+    token_file = PSAP_ODS_SECRET_PATH / config.ci_artifacts.get_config("secrets.brew_registry_redhat_io_token_file")
+    rhods.install(token_file)
+
+    for operator in config.ci_artifacts.get_config("prepare.operators"):
+        run.run(f"./run_toolbox.py cluster deploy_operator {operator['catalog']} {operator['name']} {operator['namespace']}")
+
+    run.run("testing/watsonx-serving/poc/prepare.sh | tee -a $ARTIFACT_DIR/000_prepare_sh.log")
