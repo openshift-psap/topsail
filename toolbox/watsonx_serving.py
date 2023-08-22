c45ce6d18691203abf87ced1255a8c88e84a4be0
Kevin Pouget <kpouget@redhat.com>
2023-08-22 12:15:46 +0200

    watsonx_serving_capture_state: new role to capture the state of a namespace

--- toolbox/watsonx_serving.py
+++ toolbox/watsonx_serving.py
@@ -6,3 +6,15 @@ class Watsonx_Serving:
     """
     Commands relating to WatsonX Serving stack
     """
+
+    @AnsibleRole("watsonx_serving_capture_state")
+    @AnsibleMappedParams
+    def capture_state(self, namespace=""):
+        """
+        Captures the state of the WatsonX serving stack in a given namespace
+
+        Args:
+          namespace: the namespace in which the Serving stack was deployed. If empty, use the current project.
+        """
+
+        return RunAnsibleRole(locals())
