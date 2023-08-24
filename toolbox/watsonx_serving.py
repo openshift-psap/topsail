57188217b21d517a2a95f7702b04c23361f4a82e
Kevin Pouget <kpouget@redhat.com>
2023-08-24 13:41:38 +0200

    watsonx_serving_deploy_model: new role to deploy a WatsonX-Serving model

--- toolbox/watsonx_serving.py
+++ toolbox/watsonx_serving.py
@@ -7,6 +7,34 @@ class Watsonx_Serving:
     Commands relating to WatsonX Serving stack
     """
 
+    @AnsibleRole("watsonx_serving_deploy_model")
+    @AnsibleMappedParams
+    def deploy_model(self,
+                     namespace,
+                     serving_runtime_name, serving_runtime_image, serving_runtime_resource_request,
+                     inference_service_name,
+                     storage_uri,
+                     sa_name):
+        """
+        Deploy a WatsonX-Serving model
+
+        Args:
+          name: the name of the resource to create
+          namespace: the namespace in which the model should be deployed
+
+          serving_runtime_name: the name to give to the serving runtime
+          serving_runtime_image: the image of the serving runtime
+          serving_runtime_resource_request: the resource request of the serving runtime
+
+          inference_service_name: the name to give to the inference service
+
+          sa_name: name of the service account to use for running the Pod
+          storage_uri: [S3] URI where the model is stored
+        """
+
+        return RunAnsibleRole(locals())
+
+
     @AnsibleRole("watsonx_serving_capture_state")
     @AnsibleMappedParams
     def capture_state(self, namespace=""):
