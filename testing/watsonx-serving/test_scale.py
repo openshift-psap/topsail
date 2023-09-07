924c11e5f5b10943718c1259dec1438c844825fc
Kevin Pouget <kpouget@redhat.com>
2023-09-07 09:47:53 +0200

    testing: watsonx-serving: test_scale: run_one: refactor to watch for for terminating in background
    without it, GRPC calls may never succeed and we'll wait forever for them

--- testing/watsonx-serving/test_scale.py
+++ testing/watsonx-serving/test_scale.py
@@ -137,7 +137,6 @@ secrets:
 
     save_and_create("ServiceAccount.yaml", service_account, namespace)
 
-
 def run_one():
     logging.info("Runs one WatsonX user scale test")
 
@@ -151,31 +150,62 @@ def run_one():
         job_index = 0
 
     namespace = config.ci_artifacts.get_config("tests.scale.namespace")
+    sync_file = pathlib.Path("/tmp/test_done")
+    sync_file.unlink(missing_ok=True)
+
     try:
         prepare_user_namespace(namespace)
-        run.run('echo "namespace_prepared: $(date)" >> "${ARTIFACT_DIR}/progress_ts.yaml"')
-        deploy_storage_configuration(namespace)
-        run.run('echo "storage_configured: $(date)" >> "${ARTIFACT_DIR}/progress_ts.yaml"')
 
-        models_per_namespace = config.ci_artifacts.get_config("tests.scale.models_per_namespace")
-        inference_service_basename = config.ci_artifacts.get_config("watsonx_serving.inference_service.name")
-        all_inference_service_names = []
-        for model_idx in range(models_per_namespace):
-            inference_service_name = f"{inference_service_basename}-u{job_index}-m{model_idx}"
+        def watch_failures(namespaces):
+            logging.info(f"watch_internal: launching the watch loop")
+            WATCH_INTERNAL = 5 # seconds
+            while not sync_file.exists():
+                pod_status = run.run(f"oc get pods -n {namespace} --no-headers", capture_stdout=True, log_command=False).stdout
+                if "Terminating" in pod_status:
+                    msg = f"Pod being terminated detected in namespace {namespace}. Aborting the test."
+                    logging.error(msg)
+                    logging.info("Pod status:\n{pod_status}")
+                    raise RuntimeError(msg)
 
-            extra = dict(inference_service_name=inference_service_name)
-            run.run(f"ARTIFACT_TOOLBOX_NAME_SUFFIX=_{inference_service_name} ./run_toolbox.py from_config watsonx_serving deploy_model --extra \"{extra}\"")
-            run.run(f'echo "model_{model_idx}_deployed: $(date)" >> "$ARTIFACT_DIR/progress_ts.yaml"')
+                NL = "\n"
+                logging.info(f"watch_failures: no Pod being terminated out of {len(pod_status.split(NL)) - 1}")
+                time.sleep(WATCH_INTERNAL)
+            logging.info(f"watch_internal: {sync_file} has been created, exiting the watch loop")
 
-            extra = dict(inference_service_names=[inference_service_name])
-            run.run(f"ARTIFACT_TOOLBOX_NAME_SUFFIX=_{inference_service_name} ./run_toolbox.py from_config watsonx_serving validate_model --extra \"{extra}\"")
-            run.run(f'echo "model_{model_idx}_validated: $(date)" >> "$ARTIFACT_DIR/progress_ts.yaml"')
+        def test_and_mark_as_done(*args, **kwargs):
+            try: run_one_test(*args, **kwargs)
+            finally: sync_file.touch()
 
-            all_inference_service_names += [inference_service_name]
-
-            extra = dict(inference_service_names=all_inference_service_names)
-        run.run(f"ARTIFACT_TOOLBOX_NAME_SUFFIX=_all ./run_toolbox.py from_config watsonx_serving validate_model --extra \"{extra}\"")
-        run.run(f'echo "model_all_validated: $(date)" >> "$ARTIFACT_DIR/progress_ts.yaml"')
+        with run.Parallel("test_and_watch_failures", dedicated_dir=False) as parallel:
+            parallel.delayed(test_and_mark_as_done, namespace, job_index)
+            parallel.delayed(watch_failures, namespace)
 
     finally:
         run.run(f"./run_toolbox.py watsonx_serving capture_state {namespace} > /dev/null")
+        sync_file.unlink(missing_ok=True)
+
+def run_one_test(namespace, job_index):
+    run.run('echo "namespace_prepared: $(date)" >> "${ARTIFACT_DIR}/progress_ts.yaml"')
+    deploy_storage_configuration(namespace)
+    run.run('echo "storage_configured: $(date)" >> "${ARTIFACT_DIR}/progress_ts.yaml"')
+
+    models_per_namespace = config.ci_artifacts.get_config("tests.scale.models_per_namespace")
+    inference_service_basename = config.ci_artifacts.get_config("watsonx_serving.inference_service.name")
+    all_inference_service_names = []
+    for model_idx in range(models_per_namespace):
+        inference_service_name = f"{inference_service_basename}-u{job_index}-m{model_idx}"
+
+        extra = dict(inference_service_name=inference_service_name)
+        run.run(f"ARTIFACT_TOOLBOX_NAME_SUFFIX=_{inference_service_name} ./run_toolbox.py from_config watsonx_serving deploy_model --extra \"{extra}\"")
+        run.run(f'echo "model_{model_idx}_deployed: $(date)" >> "$ARTIFACT_DIR/progress_ts.yaml"')
+
+        extra = dict(inference_service_names=[inference_service_name])
+        run.run(f"ARTIFACT_TOOLBOX_NAME_SUFFIX=_{inference_service_name} ./run_toolbox.py from_config watsonx_serving validate_model --extra \"{extra}\"")
+        run.run(f'echo "model_{model_idx}_validated: $(date)" >> "$ARTIFACT_DIR/progress_ts.yaml"')
+
+        all_inference_service_names += [inference_service_name]
+
+        extra = dict(inference_service_names=all_inference_service_names)
+
+    run.run(f"ARTIFACT_TOOLBOX_NAME_SUFFIX=_all ./run_toolbox.py from_config watsonx_serving validate_model --extra \"{extra}\"")
+    run.run(f'echo "model_all_validated: $(date)" >> "$ARTIFACT_DIR/progress_ts.yaml"')
