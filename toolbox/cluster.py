73f601928800c92d2f223c8002f2f2f97c82a2ea
Kevin Pouget <kpouget@redhat.com>
2023-09-14 14:08:02 +0200

    cluster_deploy_kubemark: split into cluster_deploy_kubemark_capi_provider and cluster_deploy_kubemark_nodes roles

--- toolbox/cluster.py
+++ toolbox/cluster.py
@@ -478,13 +478,31 @@ class Cluster:
         return RunAnsibleRole(locals())
 
 
-    @AnsibleRole("cluster_deploy_kubemark")
+    @AnsibleRole("cluster_deploy_kubemark_capi_provider")
     @AnsibleMappedParams
-    def deploy_kubemark(self):
+    def deploy_kubemark_capi_provider(self):
         """
         Deploy the Kubemark Cluster-API provider
 
         Args:
+        """
+
+        return RunAnsibleRole(locals())
+
+
+    @AnsibleRole("cluster_deploy_kubemark_nodes")
+    @AnsibleMappedParams
+    def deploy_kubemark_nodes(self,
+                              namespace="openshift-cluster-api",
+                              deployment_name="kubemark-md",
+                              count=4):
+        """
+        Deploy a set of Kubemark nodes
+
+        Args:
+          namespace: the namespace in which the MachineDeployment will be created
+          deployment_name: the name of the MachineDeployment
+          count: the number of nodes to deploy
         """
 
         return RunAnsibleRole(locals())
