f67ddbfe4c269f8b159015742f6e9f68d12144bb
Kevin Pouget <kpouget@redhat.com>
2023-09-22 11:23:44 +0200

    toolbox: add new 'llm_load_test' group

---
+++ toolbox/llm_load_test.py
@@ -0,0 +1,9 @@
+import sys
+
+from toolbox._common import RunAnsibleRole, AnsibleRole, AnsibleMappedParams, AnsibleConstant, AnsibleSkipConfigGeneration
+
+
+class Llm_load_test:
+    """
+    Commands relating to llm-load-test
+    """
