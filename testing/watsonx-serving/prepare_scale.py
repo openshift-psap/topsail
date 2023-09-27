08d8983844b64e11b56804af1f7f94afb17e2495
Kevin Pouget <kpouget@redhat.com>
2023-09-27 14:28:57 +0200

    testing: watsonx-serving: prepare_scale: improve model consolidation
    for more flexibility

--- testing/watsonx-serving/prepare_scale.py
+++ testing/watsonx-serving/prepare_scale.py
@@ -122,9 +122,16 @@ def consolidate_model_config(config_location=None, _model_name=None, index=None,
         else {}
 
     model_name = _model_name or test_config.get("name")
+    if isinstance(model_name, dict):
+        __model_name = list(model_name.keys())[0]
+        test_config = merge_dicts(test_config, model_name[__model_name])
+        model_name = test_config.get("name")
+        test_config["name"] = __model_name
+    else:
+        test_config["name"] = model_name
+
     if not model_name:
         raise RuntimeError(f"Couldn't find a name for consolidating the model configuration ... {config_location}={test_config} and model_name={_model_name}")
-
 
     if model_name == "help":
         logging.info("")
@@ -154,7 +161,6 @@ def consolidate_model_config(config_location=None, _model_name=None, index=None,
     # base += test
     model_config = merge_dicts(model_config, test_config)
 
-    model_config["name"] = model_name
     if index is not None:
         model_config["index"] = index
 
