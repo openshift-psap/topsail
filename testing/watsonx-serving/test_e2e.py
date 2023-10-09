5d4f3e499597ff535f8729d4df4d8143bfa3bd84
Kevin Pouget <kpouget@redhat.com>
2023-10-09 11:16:10 +0200

    testing: watsonx-serving: test_e2e: properly open the FAILED flag file
    Crashes as the file does not exists otherwise

--- testing/watsonx-serving/test_e2e.py
+++ testing/watsonx-serving/test_e2e.py
@@ -203,7 +203,7 @@ def deploy_and_test_models_sequentially(locally=False):
                 launch_test_consolidated_model(consolidated_model)
             except Exception as e:
                 failed += [consolidated_model['name']]
-                with open(env.ARTIFACT_DIR / "FAILED") as f:
+                with open(env.ARTIFACT_DIR / "FAILED", "w") as f:
                     print(f"{consolidated_model['name']} failed: {e.__class__.__name__}: {e}", file=f)
                 exc = e
 
