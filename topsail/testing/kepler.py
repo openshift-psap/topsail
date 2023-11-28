a2732a49d759b9c3bba532f8fc57be57504f75e1
Kevin Pouget <kpouget@redhat.com>
2023-11-28 07:20:04 +0100

    [kepler] Relocate the 'kepler' files

---
+++ topsail/testing/kepler.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from topsail._common import RunAnsibleRole, AnsibleRole, AnsibleMappedParams, AnsibleConstant, AnsibleSkipConfigGeneration
+
+
+class Kepler:
+    """
+    Commands relating to kepler deployment
+    """
+
+    @AnsibleRole("cluster_deploy_kepler")
+    @AnsibleMappedParams
+    def deploy_kepler(self):
+        """
+        Deploy the Kepler operator and monitor to track energy consumption
+
+        Args:
+        """
+
+        return RunAnsibleRole(locals())
+
+    @AnsibleRole("cluster_undeploy_kepler")
+    @AnsibleMappedParams
+    def undeploy_kepler(self):
+        """
+        Cleanup the Kepler operator and associated resources
+
+        Args:
+        """
+
+        return RunAnsibleRole(locals())
