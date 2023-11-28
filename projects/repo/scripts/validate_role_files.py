56b1126a9f2200db3d786d4b1402abda719e0827
Kevin Pouget <kpouget@redhat.com>
2023-11-28 09:52:39 +0100

    [repo] scripts: validate_role_files: make more resilient

--- projects/repo/scripts/validate_role_files.py
+++ projects/repo/scripts/validate_role_files.py
@@ -7,78 +7,115 @@ import os
 import sys
 import yaml
 import pathlib
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
-THIS_DIR = pathlib.Path(__file__).absolute().parent
-TOP_DIR = THIS_DIR.parent.parent
-# go to top directory
-os.chdir(TOP_DIR)
+SCRIPT_THIS_DIR = pathlib.Path(__file__).absolute().parent
+TOPSAIL_DIR = SCRIPT_THIS_DIR.parent.parent.parent
 
-ROLES_VARS_GLOB = "roles/*/*/vars/*/*"
+ROLES_VARS_GLOB = "projects/*/roles/*/vars/*/*"
 FILE_PREFIX = "roles/"
 
-def validate_role_vars_files(yaml_doc):
-    errors = 0
-
+def validate_role_vars_files(dirname, yaml_doc):
+    errors = []
+    messages = []
     for key, value in yaml_doc.items():
-        try:
-            if not value.startswith(FILE_PREFIX):
-                print()
-                print(f"{key}: {value} --> not starting with '{FILE_PREFIX}', ignoring.")
-                continue
-        except AttributeError: # value.startswith
-            print()
-            print(f"{key}:{value} --> not a string ({value.__class__.__name__}), ignoring.")
+        if not isinstance(value, str):
+            messages.append(f"{key}:{value} --> not a string ({value.__class__.__name__}), ignoring.")
             continue
 
-        if not pathlib.Path(value).exists():
-            errors += 1
-            print()
-            print(f"ERROR: {key}: {value} --> not found")
+        if (dirname / value).exists():
+            # file exists inside the role, continue
             continue
 
-        # file found, nothing to do
+        if (TOPSAIL_DIR / value).exists():
+            # file exists in topsail topdir, continue
+            continue
 
-    return errors
+        if value.startswith("/"):
+            messages.append(f"{key}:{value} --> absolute path, ignoring")
+            continue
+
+        if not "/" in value and not "." in value:
+            messages.append(f"{key}:{value} --> likely not a path, ignoring")
+            continue
+
+        errors.append(f"{key}: {value} --> not found")
+        continue
+
+    return errors, messages
 
 def traverse_role_vars():
     errors = 0
-    for filename in TOP_DIR.glob(ROLES_VARS_GLOB):
-        msg = ["", f"### {filename.relative_to(TOP_DIR)}"]
+    successes = 0
+    for filename in TOPSAIL_DIR.glob(ROLES_VARS_GLOB):
 
         with open(filename) as f:
             try:
                 yaml_doc = yaml.safe_load(f)
             except yaml.YAMLError as e:
-                msg.append(f"--> invalid YAML file ({e})")
+                logging.warning(f"{filename} --> invalid YAML file ({e})")
                 continue
+            except Exception as e:
+                logging.warning(f"{filename} --> invalid YAML file ({e})")
+                errors += 1
 
         if yaml_doc is None:
-            msg.append(f"--> empty file")
+            continue
+            logging.warning(f"{filename} --> empty file)")
             continue
 
-        file_errors = validate_role_vars_files(yaml_doc)
-        errors += file_errors
+        dirname = filename.parent
+        while dirname.name != "vars":
+            dirname = dirname.parent
+        dirname = dirname.parent
 
-        msg.append(f"--> found {file_errors} error{'s' if file_errors > 1 else ''} in {filename.relative_to(TOP_DIR)}")
+        file_errors, messages = validate_role_vars_files(dirname, yaml_doc)
+        errors += len(file_errors)
+
+        if not file_errors:
+            successes += 1
+
+        if not file_errors and not messages:
+            continue
+
+        logging.info(f"### {filename.relative_to(TOPSAIL_DIR)}")
+
+        for msg in file_errors:
+            logging.error(msg)
         if file_errors:
-            print("\n".join(msg))
+            logging.warning(f"--> found {len(file_errors)} error{'s' if len(file_errors) > 1 else ''} in {filename.relative_to(TOPSAIL_DIR)}")
+        for msg in messages:
+            logging.info(msg)
 
-    return errors
+        logging.info("\n")
+
+    return successes, errors
+
 
 def main():
     if "-h" in sys.argv or "--help" in sys.argv:
-        print("""\
+        logging.info("""\
 This script ensures that all the Ansible variables defining a
 filepath (`roles/`) do point to an existing file.""")
         return 0
 
-    print(f"INFO: Searching for missing files in '{ROLES_VARS_GLOB}'")
-    errors = traverse_role_vars()
+    logging.info(f"Searching for missing files in '{ROLES_VARS_GLOB}'")
+    successes, errors = traverse_role_vars()
 
-    print()
-    print(f"{'ERROR' if errors else 'INFO'}: found {errors} missing file{'s' if errors > 1 else ''}")
+    if errors:
+        logging.fatal(f"Found {errors} missing file{'s' if errors > 1 else ''}")
+        return 1
 
-    return 1 if errors else 0
+
+    if successes == 0:
+        logging.fatal(f"Found no role file :/")
+        logging.info(f"Search pattern: {ROLES_VARS_GLOB}")
+
+        return 1
+
+    logging.info("All good :)")
+    return 0
 
 if __name__ == "__main__":
     exit(main())
