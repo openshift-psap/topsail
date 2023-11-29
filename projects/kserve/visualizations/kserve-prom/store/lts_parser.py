3ee1eab8742351b115d25f6baebcdc35f62ab6a2
Kevin Pouget <kpouget@redhat.com>
2023-11-29 16:11:55 +0100

    [kserve] visualizations: kserve-prom: move the payload.metadata creation into a dedicated function

--- projects/kserve/visualizations/kserve-prom/store/lts_parser.py
+++ projects/kserve/visualizations/kserve-prom/store/lts_parser.py
@@ -10,21 +10,27 @@ def generate_lts_payload(results, lts_results, import_settings, must_validate=Fa
     # _=[print(m) for m in results.metrics["sutest"].keys()]
 
     lts_payload = types.SimpleNamespace()
-    lts_payload.metadata = types.SimpleNamespace()
-    start_ts = results.metrics["sutest"]["watsonx-e2e.* CPU usage"][0].values[0][0]
-    end_ts = results.metrics["sutest"]["watsonx-e2e.* CPU usage"][0].values[-1][0]
-    lts_payload.metadata.start = datetime.datetime.utcfromtimestamp(start_ts)
-    lts_payload.metadata.end = datetime.datetime.utcfromtimestamp(end_ts)
-
-    lts_payload.metadata.presets = results.test_config.get("ci_presets.names") or ["no_preset_defined"]
-    lts_payload.metadata.config = results.test_config.yaml_file
-    lts_payload.metadata.settings = dict(import_settings)
-
+    lts_payload.metadata = generate_lts_metadata(results, import_settings)
     lts_payload.results = lts_results
 
     lts.validate_lts_payload(lts_payload, import_settings, reraise=must_validate)
 
     return lts_payload
+
+
+def generate_lts_metadata(results, import_settings):
+    metadata = types.SimpleNamespace()
+
+    start_ts = results.metrics["sutest"]["watsonx-e2e.* CPU usage"][0].values[0][0]
+    end_ts = results.metrics["sutest"]["watsonx-e2e.* CPU usage"][0].values[-1][0]
+    metadata.start = datetime.datetime.utcfromtimestamp(start_ts)
+    metadata.end = datetime.datetime.utcfromtimestamp(end_ts)
+
+    metadata.presets = results.test_config.get("ci_presets.names") or ["no_preset_defined"]
+    metadata.config = results.test_config.yaml_file
+    metadata.settings = dict(import_settings)
+
+    return metadata
 
 
 def generate_lts_results(results):
