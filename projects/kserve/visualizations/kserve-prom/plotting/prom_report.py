cb87479cd7c17d295726b560a0d91a91d85cc9b1
Kevin Pouget <kpouget@redhat.com>
2023-12-08 16:58:59 +0100

    [kserve] visualizations: kserve-prom: reorganize reports
    - move `ControlPlaneReport` to prom_report`
    - remove `UserProgressReport` and UserProgressDetailsReport`

--- projects/kserve/visualizations/kserve-prom/plotting/prom_report.py
+++ projects/kserve/visualizations/kserve-prom/plotting/prom_report.py
@@ -14,6 +14,8 @@ def register():
     SutestCpuMemoryReport()
     GpuUsageReport()
     RhoaiFootprintReport()
+    ControlPlaneReport()
+
 
 def add_pod_cpu_mem_usage(header, what, args, mem_only=False, cpu_only=False):
     if mem_only:
@@ -133,3 +135,77 @@ class RhoaiFootprintReport():
             header += [report.Plot(f"Prom: Namespace {namespace}: Mem usage", args_as_timeline)]
 
         return None, header
+
+
+
+class ControlPlaneReport():
+    def __init__(self):
+        self.name = "report: Control Plane Nodes Load"
+        self.id_name = self.name.lower().replace(" ", "_")
+        self.no_graph = True
+        self.is_report = True
+
+        table_stats.TableStats._register_stat(self)
+
+    def do_plot(self, *args):
+        header = []
+        if error_report:
+            header += error_report._get_all_tests_setup(args)
+
+        header += [html.H1("Control Plane Nodes Load")]
+
+        for cluster_role in ["sutest"]:
+            header += [html.H1(f"{cluster_role.title()} cluster")]
+
+            header += ["These plots shows the CPU and memory usage of the Kubernetes API Server and ETCD, running on the Control Plane nodes of the cluster."]
+
+            for pod_name in ["ApiServer", "ETCD"]:
+                header += [html.H2(f"{pod_name} subsystem")]
+
+                for what in ["CPU", "Mem"]:
+                    header += report.Plot_and_Text(f"Prom: {cluster_role.title()} {pod_name}: {what} usage", args)
+                    header += html.Br()
+                    header += html.Br()
+
+            if cluster_role != "sutest": continue
+
+            header += [html.H2(f"CPU usage")]
+
+            header += ["These plots shows the CPU usage of the Control Plane nodes.",
+                       html.Br(),
+                       "The first plot show all the available modes, while the second one shows only the idle time (higher is better).",
+                       html.Br(),
+                       "The Y scale is arbitrary, but for a given node, the sum of all the modes at a given time indicate 100% of the CPU."
+                       ]
+
+            header += report.Plot_and_Text(f"Prom: {cluster_role.title()} Control Plane Node CPU usage", args)
+            header += html.Br()
+            header += html.Br()
+
+            header += report.Plot_and_Text(f"Prom: {cluster_role.title()} Control Plane Node CPU idle", args)
+            header += html.Br()
+            header += html.Br()
+
+            header += report.Plot_and_Text(f"Prom: {cluster_role.title()} Worker Node CPU usage", args)
+            header += html.Br()
+            header += html.Br()
+
+            header += report.Plot_and_Text(f"Prom: {cluster_role.title()} Worker Node CPU idle", args)
+            header += html.Br()
+            header += html.Br()
+
+            header += [html.H2(f"APIServer requests duration")]
+            for verb in ["LIST", "GET", "PUT", "PATCH"]:
+                header += report.Plot_and_Text(f"Prom: {cluster_role.title()} API Server {verb} Requests duration", args)
+                header += html.Br()
+                header += html.Br()
+
+            header += [html.H2(f"API Server HTTP return codes")]
+            for what in ["successes", "client errors", "server errors"]:
+                header += report.Plot_and_Text(f"Prom: {cluster_role.title()} API Server Requests ({what})", args)
+                header += html.Br()
+                header += html.Br()
+
+        return None, header
+
+
