c1772ca7882d133f8d35498f29050b8a7804e5f4
Kevin Pouget <kpouget@redhat.com>
2024-01-09 09:54:51 +0100

    [notebooks] visualizations: rhods-notebooks-performance: add 'LTS Documentation' report

--- projects/notebooks/visualizations/rhods-notebooks-performance/plotting/__init__.py
+++ projects/notebooks/visualizations/rhods-notebooks-performance/plotting/__init__.py
@@ -2,9 +2,11 @@ from . import report
 from . import notebook_performance_comparison
 from . import gating_report
 from . import notebook_performance
+from . import lts_documentation
 
 def register():
     report.register()
     notebook_performance_comparison.register()
     gating_report.register()
     notebook_performance.register()
+    lts_documentation.register()
