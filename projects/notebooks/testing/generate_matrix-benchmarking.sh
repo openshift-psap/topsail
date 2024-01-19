425fad5b668265a7e581de0291b4dddb4886394f
Kevin Pouget <kpouget@redhat.com>
2024-01-19 09:12:17 +0100

    [notebooks] testing: generate_matrix-benchmarking: remove unnecessary copy of prometheus.yml

--- projects/notebooks/testing/generate_matrix-benchmarking.sh
+++ projects/notebooks/testing/generate_matrix-benchmarking.sh
@@ -128,7 +128,6 @@ generate_matbench::generate_visualization() {
 
     generate_url="stats=$(echo -n "$generate_list" | tr '\n' '&' | sed 's/&/&stats=/g')"
 
-    cp -f /tmp/prometheus.yml "." || true
     if ! matbench parse --output-matrix $ARTIFACT_DIR/internal_matrix.json |& tee > "$ARTIFACT_DIR/_matbench_parse.log"; then
         _warning "An error happened during the parsing of the results (or no results were available) in $MATBENCH_RESULTS_DIRNAME, aborting."
         return 1
