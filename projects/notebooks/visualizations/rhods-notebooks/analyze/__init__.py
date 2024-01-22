9b8488271fce90a95b4a7686a3742372471140ed
Kevin Pouget <kpouget@redhat.com>
2024-01-22 16:05:52 +0100

    [notebooks] visualizations: rhods-notebooks: add the analyze stub

---
+++ projects/notebooks/visualizations/rhods-notebooks/analyze/__init__.py
@@ -0,0 +1,19 @@
+import matrix_benchmarking.common as common
+import logging
+
+def run():
+    logging.info("Hello world :)")
+
+    for entry in common.Matrix.all_records():
+        lts_payload = entry.results
+        kpis = lts_payload.kpis
+
+        print(kpis.tostr())
+        print()
+        print("---")
+        print()
+        pass
+
+    number_of_failures = 0
+
+    return number_of_failures
