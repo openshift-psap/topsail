c784c85232e4d249fd10dce44877492713b1a84e
Kevin Pouget <kpouget@redhat.com>
2024-01-22 13:27:40 +0100

    [skeleton] visualizations: skeleton: move the payload metadata generation to a dedicated function

--- projects/skeleton/visualizations/skeleton/store/lts_parser.py
+++ projects/skeleton/visualizations/skeleton/store/lts_parser.py
@@ -4,26 +4,33 @@ from .. import models
 from ..models import lts as models_lts
 from . import lts
 
+
 def generate_lts_payload(results, lts_results, import_settings, must_validate=False):
     # To know the available metrics:
-    # _=[print(m) for m in results.metrics["sutest"].keys()]
+    # _=[print(m) for m in results.metrics["sutest"].keys()]g
 
     lts_payload = types.SimpleNamespace()
-    lts_payload.metadata = types.SimpleNamespace()
-    lts_payload.metadata.start = results.test_start_end_time.start
-    lts_payload.metadata.end = results.test_start_end_time.end
-
-    lts_payload.metadata.presets = results.test_config.get("ci_presets.names") or ["no_preset_defined"]
-    lts_payload.metadata.config = results.test_config.yaml_file
-    lts_payload.metadata.ocp_version = results.sutest_ocp_version
-    lts_payload.metadata.settings = dict(import_settings)
-    lts_payload.metadata.test_uuid = results.test_uuid
-
+    lts_payload.metadata = generate_lts_metadata(results, import_settings)
     lts_payload.results = lts_results
 
     lts.validate_lts_payload(lts_payload, import_settings, reraise=must_validate)
 
     return lts_payload
+
+
+def generate_lts_metadata(results, import_settings):
+    metadata = types.SimpleNamespace()
+
+    metadata.start = results.test_start_end_time.start
+    metadata.end = results.test_start_end_time.end
+
+    metadata.presets = results.test_config.get("ci_presets.names") or ["no_preset_defined"]
+    metadata.config = results.test_config.yaml_file
+    metadata.ocp_version = results.sutest_ocp_version
+    metadata.settings = dict(import_settings)
+    metadata.test_uuid = results.test_uuid
+
+    return metadata
 
 
 def generate_lts_results(results):
