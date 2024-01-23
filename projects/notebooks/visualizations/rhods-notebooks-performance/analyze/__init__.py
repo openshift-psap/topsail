8d01060d024a97064e177c7f5d1525de57cff278
Kevin Pouget <kpouget@redhat.com>
2024-01-23 13:47:06 +0100

    [notebooks] visualizations: rhods-notebooks-performance: analyze: update the POC

--- projects/notebooks/visualizations/rhods-notebooks-performance/analyze/__init__.py
+++ projects/notebooks/visualizations/rhods-notebooks-performance/analyze/__init__.py
@@ -2,17 +2,11 @@ import matrix_benchmarking.common as common
 import logging
 
 def run():
-    logging.info("Hello world :)")
+    logging.info(f"Received {common.Matrix.count_records()} new entries")
+    for entry in common.Matrix.all_records(): pass
 
-    for entry in common.Matrix.all_records():
-        lts_payload = entry.results.lts
-        kpis = lts_payload.kpis
-
-        print(kpis.tostr())
-        print()
-        print("---")
-        print()
-        pass
+    logging.info(f"Received {common.LTS_Matrix.count_records()} historic LTS entries")
+    for lts_entry in common.LTS_Matrix.all_records(): pass
 
     number_of_failures = 0
 
