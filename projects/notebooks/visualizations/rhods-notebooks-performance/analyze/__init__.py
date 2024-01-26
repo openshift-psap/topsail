65fcc1914f723a1a935cd5dead2d3b0274e3c236
Drew Ripberger <drew.ripberger@gmail.com>
2024-01-26 12:25:51 -0500

    tolerate missing settings

--- projects/notebooks/visualizations/rhods-notebooks-performance/analyze/__init__.py
+++ projects/notebooks/visualizations/rhods-notebooks-performance/analyze/__init__.py
@@ -11,6 +11,7 @@ def run():
 
     logging.info(f"Received {common.LTS_Matrix.count_records()} historic LTS entries")
     lts_entries = []
+    # Take the first result if they were gathered
     for entry in common.LTS_Matrix.all_records():
         if entry.results and type(entry.results) is list:
             entry.results = entry.results[0]
@@ -21,20 +22,28 @@ def run():
     logging.info(f"Received {common.Matrix.count_records()} new entries")
 
     number_of_failures = 0
+    settings_to_check = ["version", "repeat"]
     for entry in common.Matrix.all_records():
         regression_results_dest = entry.location / "regression.json"
-        regression_results = []
-        controlled_settings = entry.get_settings()
-        controlled_settings.pop("repeat") # Looking for regressions over the version, repeat is a placeholder
-        controlled_lts_entries = list(
-            filter(
-                lambda x: regression.dict_part_eq(controlled_settings, x.get_settings()),
-                lts_entries
+        regression_results = {}
+        for check_setting in settings_to_check:
+            controlled_settings = entry.get_settings()
+
+            try:
+                controlled_settings.pop(check_setting)
+            except KeyError:
+                logging.warning(f"Couldn't find {check_setting} setting for entry={entry.location}, skipping...")
+                continue
+
+            controlled_lts_entries = list(
+                filter(
+                    lambda x: regression.dict_part_eq(controlled_settings, x.get_settings()),
+                    lts_entries
+                )
             )
-        )
-        zscore = regression.ZScoreIndicator(entry, controlled_lts_entries)
-        regression_results = zscore.analyze()
-        number_of_failures += sum(map(lambda x: x["regression"]["status"], regression_results))
+            zscore = regression.ZScoreIndicator(entry, controlled_lts_entries)
+            regression_results[check_setting] = zscore.analyze()
+            number_of_failures += sum(map(lambda x: x["regression"]["status"], regression_results[check_setting]))
 
         logging.info(f"Saving the regression results in {regression_results_dest}")
         with open(regression_results_dest, "w") as f:
