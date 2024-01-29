038369a220af5ab497beb9d829474ce16743a631
Kevin Pouget <kpouget@redhat.com>
2024-01-29 11:51:13 +0100

    [notebooks] visualizations: rhods-notebooks-performance: integrate LTS historical record visualization

--- projects/notebooks/visualizations/rhods-notebooks-performance/plotting/__init__.py
+++ projects/notebooks/visualizations/rhods-notebooks-performance/plotting/__init__.py
@@ -3,6 +3,7 @@ from . import notebook_performance_comparison
 from . import gating_report
 from . import notebook_performance
 from . import lts_documentation
+from . import lts_kpis
 
 def register():
     report.register()
@@ -10,3 +11,4 @@ def register():
     gating_report.register()
     notebook_performance.register()
     lts_documentation.register()
+    lts_kpis.register()
