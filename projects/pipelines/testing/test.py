bf72bc644b225e15bcecf3aca56547f2697d7f6f
Kevin Pouget <kpouget@redhat.com>
2024-02-05 11:18:19 +0100

    [pipelines] testing: test: auto-detect the metal environments

--- projects/pipelines/testing/test.py
+++ projects/pipelines/testing/test.py
@@ -22,6 +22,7 @@ TESTING_UTILS_DIR = TESTING_THIS_DIR.parent / "utils"
 
 PSAP_ODS_SECRET_PATH = pathlib.Path(os.environ.get("PSAP_ODS_SECRET_PATH", "/env/PSAP_ODS_SECRET_PATH/not_set"))
 LIGHT_PROFILE = "light"
+METAL_PROFILE = "metal"
 
 initialized = False
 def init(ignore_secret_path=False, apply_preset_from_pr_args=True):
@@ -48,6 +49,7 @@ def init(ignore_secret_path=False, apply_preset_from_pr_args=True):
         config.ci_artifacts.apply_preset(ICELAKE_PROFILE)
 
     config.ci_artifacts.detect_apply_light_profile(LIGHT_PROFILE)
+    config.ci_artifacts.detect_apply_metal_profile(METAL_PROFILE)
 
 
 def entrypoint(ignore_secret_path=False, apply_preset_from_pr_args=True):
