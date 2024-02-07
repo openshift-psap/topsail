d2d47dceafa0cc70457a0e3eb4297c811f10836d
Drew Ripberger <drew.ripberger@gmail.com>
2024-02-07 15:28:14 -0500

    missing pipelines path

--- projects/pipelines/testing/configure.sh
+++ projects/pipelines/testing/configure.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 
 if [ -n "$BASH_VERSION" ]; then
-    # assume Bash
-    TESTING_THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+	# assume Bash
+	TESTING_THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 elif [ -n "$ZSH_VERSION" ]; then
-    # assume ZSH
-    TESTING_THIS_DIR=${0:a:h}
+	# assume ZSH
+	TESTING_THIS_DIR=${0:a:h}
 elif [[ -z "${TESTING_THIS_DIR:-}" ]]; then
-     echo "Shell isn't bash nor zsh, please expose the directory of this file with TESTING_THIS_DIR."
-     false
+	echo "Shell isn't bash nor zsh, please expose the directory of this file with TESTING_THIS_DIR."
+	false
 fi
 
-TOPSAIL_DIR="$(cd "$TESTING_THIS_DIR/../../.." >/dev/null 2>&1 && pwd )"
+TOPSAIL_DIR="$(cd "$TESTING_THIS_DIR/../../.." >/dev/null 2>&1 && pwd)"
 TESTING_UTILS_DIR="$TOPSAIL_DIR/testing/utils"
 
-
+TESTING_PIPELINES_DIR="$TOPSAIL_DIR/projects/pipelines"
 TESTING_UTILS_DIR="$TESTING_PIPELINES_DIR/../utils"
 
 export CI_ARTIFACTS_FROM_COMMAND_ARGS_FILE=${TESTING_PIPELINES_DIR}/command_args.yml.j2
 
 if [[ -z "${CI_ARTIFACTS_FROM_CONFIG_FILE:-}" ]]; then
-    export CI_ARTIFACTS_FROM_CONFIG_FILE=${TESTING_PIPELINES_DIR}/config.yaml
+	export CI_ARTIFACTS_FROM_CONFIG_FILE=${TESTING_PIPELINES_DIR}/config.yaml
 fi
 echo "Using '$CI_ARTIFACTS_FROM_CONFIG_FILE' as configuration file."
 
