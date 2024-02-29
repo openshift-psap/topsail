64683513d99d6ec18bca4ecbd1fb3133a9161bb5
Nikhil Palaskar <npalaska@redhat.com>
2024-02-29 18:11:13 -0500

    cleanup cluster entrypoint

--- projects/kserve/testing/prepare_kserve.py
+++ projects/kserve/testing/prepare_kserve.py
@@ -175,12 +175,11 @@ def undeploy_operator(operator, mute=True):
 
 
 def cleanup(mute=True):
-    return
-    # rhods.uninstall(mute)
-    #
-    # with run.Parallel("cleanup_kserve") as parallel:
-    #     for operator in config.ci_artifacts.get_config("prepare.operators"):
-    #         undeploy_operator(operator)
+    rhods.uninstall(mute)
+
+    with run.Parallel("cleanup_kserve") as parallel:
+        for operator in config.ci_artifacts.get_config("prepare.operators"):
+            undeploy_operator(operator)
 
 
 def update_serving_runtime_images():
