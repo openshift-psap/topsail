322da84225cd6fb58119e2f83d698ef5fea9f82c
Nikhil Palaskar <npalaska@redhat.com>
2024-03-03 03:40:17 -0500

    revert back default limit and request settings

--- projects/kserve/toolbox/kserve.py
+++ projects/kserve/toolbox/kserve.py
@@ -27,7 +27,7 @@ class Kserve:
                      secret_env_file_key=None,
                      sr_mute_logs=False,
                      delete_others=True,
-                     limits_equals_requests=False,
+                     limits_equals_requests=True,
                      raw_deployment=False,
                      ):
         """
