5a481f0e2d1810cddef3322a66dd2ee6c7973b53
Kevin Pouget <kpouget@redhat.com>
2024-03-25 13:20:23 +0100

    [skeleton] visualizations: skeleton: use the centralized code

--- projects/skeleton/visualizations/skeleton/models/lts.py
+++ projects/skeleton/visualizations/skeleton/models/lts.py
@@ -5,12 +5,28 @@ from typing import Any, List, Optional
 from pydantic import BaseModel, Field
 
 import matrix_benchmarking.models as matbench_models
+from . import kpi
 
-
-class Metadata(matbench_models.Metadata):
-    presets: List[str]
-    config: Any
+KPI_SETTINGS_VERSION = "1.1"
+class Settings(matbench_models.ExclusiveModel):
+    kpi_settings_version: str
     ocp_version: matbench_models.SemVer
+
+    ci_engine: str
+    run_id: str
+    test_path: str
+    urls: Optional[dict[str, str]]
+
+
+LTS_SCHEMA_VERSION = "1.0"
+class Metadata(matbench_models.Metadata):
+    lts_schema_version: str
+    settings: Settings
+
+    presets: List[str]
+    config: str
+    ocp_version: matbench_models.SemVer
+
 
 class Metrics(matbench_models.ExclusiveModel):
     control_plane_node_cpu_idle: matbench_models.PrometheusValues = \
@@ -26,6 +42,11 @@ class Results(matbench_models.ExclusiveModel):
     metrics: Metrics
 
 
+class SkeletonKPI(matbench_models.KPI, Settings): pass
+
+SkeletonKPIs = matbench_models.getKPIsModel("SkeletonKPIs", __name__, kpi.KPIs, SkeletonKPI)
+
 class Payload(matbench_models.ExclusiveModel):
     metadata: Metadata
     results: Results
+    kpis: SkeletonKPIs
