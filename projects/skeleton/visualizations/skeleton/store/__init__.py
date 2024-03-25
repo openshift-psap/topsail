cacefae50c4ac3cad7989a8c6676e7c1186feb35
Kevin Pouget <kpouget@redhat.com>
2024-03-25 13:20:23 +0100

    [skeleton] visualizations: skeleton: use the centralized code

--- projects/skeleton/visualizations/skeleton/store/__init__.py
+++ projects/skeleton/visualizations/skeleton/store/__init__.py
@@ -8,127 +8,34 @@ import os
 import matrix_benchmarking.store as store
 import matrix_benchmarking.store.simple as store_simple
 
+import projects.core.visualizations.helpers.store as core_helpers_store
+
 from . import parsers
-from . import lts
 from . import lts_parser
-
-CACHE_FILENAME = "cache.pickle"
-
-IMPORTANT_FILES = parsers.IMPORTANT_FILES
-
+from ..models import kpi as models_kpi
 from ..models import lts as models_lts
 
-def is_mandatory_file(filename):
-    return filename.name in ("settings", "exit_code", "config.yaml") or filename.name.startswith("settings.")
+CACHE_FILENAME = "cache.pickle"
+IMPORTANT_FILES = parsers.IMPORTANT_FILES
 
+local_store = core_helpers_store.BaseStore(
+    cache_filename=CACHE_FILENAME, important_files=IMPORTANT_FILES,
+    artifact_dirnames=parsers.artifact_dirnames, artifact_paths=parsers.artifact_paths,
+    parse_always=parsers.parse_always, parse_once=parsers.parse_once,
 
-def is_important_file(filename):
-    if str(filename) in IMPORTANT_FILES:
-        return True
+    lts_payload_model=models_lts.Payload,
+    generate_lts_payload=lts_parser.generate_lts_payload,
 
-    for important_file in IMPORTANT_FILES:
-        if "*" not in important_file: continue
-
-        if fnmatch.filter([str(filename)], important_file):
-            return True
-
-    return False
-
-
-def is_cache_file(filename):
-    return filename.name == CACHE_FILENAME
-
-
-def resolve_artifact_dirnames(dirname, artifact_dirnames):
-    artifact_paths = types.SimpleNamespace()
-    for artifact_dirname, unresolved_dirname in artifact_dirnames.__dict__.items():
-        direct_resolution = dirname / unresolved_dirname
-        resolutions = list(dirname.glob(unresolved_dirname))
-        resolved_dir = None
-
-        if direct_resolution.exists():
-            # all good
-            resolved_dir = direct_resolution
-        elif not resolutions:
-            logging.warning(f"Cannot resolve {artifact_dirname} glob '{unresolved_dirname}' in '{dirname}'")
-        else:
-            if len(resolutions) > 1:
-                logging.error(f"Could multiple resolutions for {artifact_dirname} glob '{unresolved_dirname}' in '{dirname}': {resolutions}. Taking the first one")
-
-            resolved_dir = resolutions[0]
-
-        if resolved_dir:
-            resolved_dir = resolved_dir.relative_to(dirname)
-
-        artifact_paths.__dict__[artifact_dirname] = resolved_dir
-
-    return artifact_paths
-
-
-def register_important_file(base_dirname, filename):
-    if not is_important_file(filename):
-        logging.warning(f"File '{filename}' not part of the important file list :/")
-        if pathlib.Path(filename).is_absolute():
-            logging.warning(f"File '{filename}' is an absolute path. Should be relative to {base_dirname}.")
-    return base_dirname / filename
-
-parsers.register_important_file = register_important_file
+    models_kpis=models_kpi.KPIs,
+    get_kpi_labels=lts_parser.get_kpi_labels,
+)
 
 
 def _rewrite_settings(settings_dict):
     return settings_dict
 
-
-def load_cache(dirname):
-    with open(dirname / CACHE_FILENAME, "rb") as f:
-        return pickle.load(f)
-
-
-def _parse_results(fn_add_to_matrix, dirname, import_settings):
-    parsers.artifact_paths = resolve_artifact_dirnames(dirname, parsers.artifact_dirnames)
-
-    ignore_cache = os.environ.get("MATBENCH_STORE_IGNORE_CACHE", False) in ("yes", "y", "true", "True")
-    if not ignore_cache:
-        try:
-            results = load_cache(dirname)
-        except FileNotFoundError:
-            results = None # Cache file doesn't exit, ignore and parse the artifacts
-    else:
-        logging.info("MATBENCH_STORE_IGNORE_CACHE is set, not processing the cache file.")
-        results = None
-
-
-    if results:
-        parsers._parse_always(results, dirname, import_settings)
-
-        fn_add_to_matrix(results)
-
-        return
-
-    results = types.SimpleNamespace()
-
-    parsers._parse_always(results, dirname, import_settings)
-    parsers._parse_once(results, dirname)
-
-    lts_results = lts_parser.generate_lts_results(results)
-    results.lts = lts_parser.generate_lts_payload(results, lts_results, import_settings, must_validate=False)
-
-    fn_add_to_matrix(results)
-
-    with open(dirname / CACHE_FILENAME, "wb") as f:
-        get_config = results.test_config.get
-        results.test_config.get = None
-
-        pickle.dump(results, f)
-
-        results.test_config.get = get_config
-
-    logging.info("parsing done :)")
-
+build_lts_payloads = local_store.build_lts_payloads
+parsers.register_important_file = local_store.register_important_file
 # delegate the parsing to the simple_store
 store.register_custom_rewrite_settings(_rewrite_settings)
-store.register_lts_schema(models_lts.Payload)
-
-store_simple.register_custom_parse_results(_parse_results)
-
-build_lts_payloads = lts.build_lts_payloads
+store_simple.register_custom_parse_results(local_store.parse_directory)
