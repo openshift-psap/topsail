d19bfc8298ae6879b9cf16225a0609a244b3bd49
Kevin Pouget <kpouget@redhat.com>
2024-03-27 09:49:44 +0100

    [kserve] visualizations: kserve-prom: make the KPIs more generic

--- projects/kserve/visualizations/kserve-prom/models/kpi.py
+++ projects/kserve/visualizations/kserve-prom/models/kpi.py
@@ -11,11 +11,11 @@ KPIs = {} # populated by the @matbench_models.KPIMetadata decorator
 
 @matbench_models.KPIMetadata(help="Max CPU usage of the Kserve container | container_cpu_usage_seconds_total", unit="cores")
 def kserve_container_cpu_usage_max(lts_payload):
-    return prom_parsing.single_max(lts_payload.results.metrics.kserve_container_cpu_usage)[0]
+    return prom_parsing.all_max(lts_payload.results.metrics.kserve_container_cpu_usage)[0]
 
 @matbench_models.KPIMetadata(help="Max memory usage of the Kserve container | container_memory_usage_bytes", unit="bytes")
 def kserve_container_memory_usage_max(lts_payload):
-    return prom_parsing.single_max(lts_payload.results.metrics.kserve_container_memory_usage)[0]
+    return prom_parsing.all_max(lts_payload.results.metrics.kserve_container_memory_usage)[0]
 
 # ---
 
@@ -32,20 +32,26 @@ def footprint_rhoai_namespace_memory_request(lts_payload):
 @matbench_models.KPIMetadata(help="Max GPU memory usage, per GPU | DCGM_FI_DEV_FB_USED", unit="list(bytes)")
 def gpu_memory_usage_max_per_gpu(lts_payload):
     metrics = lts_payload.results.metrics.gpu_memory_used
+
     if not metrics:
-        return [0]
+        return [-1]
+
     return prom_parsing.all_max(metrics)
 
 @matbench_models.KPIMetadata(help="Max GPU memory usage, for any of the GPUs | DCGM_FI_DEV_FB_USED", unit="bytes")
 def gpu_memory_usage_max_all_gpus(lts_payload):
     metrics = lts_payload.results.metrics.gpu_memory_used
+
     if not metrics:
-        return 0
+        return -1
+
     return prom_parsing.max_max(metrics)
 
 @matbench_models.KPIMetadata(help="Max total GPU memory usage, for all the GPUs | DCGM_FI_DEV_FB_USED", unit="bytes")
 def gpu_total_memory_usage_max(lts_payload):
     metrics = lts_payload.results.metrics.gpu_total_memory_used
+
     if not metrics:
-        return 0
+        return -1
+
     return prom_parsing.single_max(metrics)[0]
