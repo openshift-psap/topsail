45dda3f603cc9fec7c9b5d99512d8e7d6fc0584f
Kevin Pouget <kpouget@redhat.com>
2024-04-15 16:59:54 +0200

    topsail: testing: rhods: uninstall ServiceMesh on cleanup

--- topsail/testing/rhods.py
+++ topsail/testing/rhods.py
@@ -46,12 +46,13 @@ def uninstall(mute=True):
         logging.info("RHODS is not installed.")
         # make sure that no core RHODS namespace is still there
         run.run('oc get ns redhat-ods-applications redhat-ods-monitoring redhat-ods-operator --ignore-not-found')
-        return
+    else:
+        if run.run(f'oc get datasciencecluster -oname | grep .', check=False).returncode == 0:
+            run.run_toolbox("rhods", "update_datasciencecluster")
 
-    if run.run(f'oc get datasciencecluster -oname | grep .', check=False).returncode == 0:
-        run.run_toolbox("rhods", "update_datasciencecluster")
+        run.run_toolbox("rhods", "undeploy_ods", mute_stdout=mute)
 
-    run.run_toolbox("rhods", "undeploy_ods", mute_stdout=mute)
+    uninstall_servicemesh(mute)
 
 
 def uninstall_ldap(mute=True):
@@ -62,3 +63,38 @@ def uninstall_ldap(mute=True):
         return
 
     run.run_toolbox_from_config("cluster", "undeploy_ldap", mute_stdout=mute)
+
+
+def uninstall_servicemesh(mute=True):
+    operator = dict(
+        name = "servicemeshoperator",
+        namespace = "all",
+    )
+    cleanup = dict(
+        namespaces = ["istio-system"],
+        crds = [
+            "servicemeshmemberrolls.maistra.io",
+            "servicemeshcontrolplanes.maistra.io",
+        ],
+    )
+
+    manifest_name = operator["name"]
+    namespace = "openshift-operators"
+
+    for crd in cleanup.get("crds", []):
+        run.run(f"oc delete {crd} --all -A", check=False)
+
+    installed_csv_cmd = run.run(f"oc get csv -oname -n {namespace} "
+                                f"-loperators.coreos.com/{manifest_name}.{namespace}", capture_stdout=mute)
+
+    if not installed_csv_cmd.stdout:
+        logging.info(f"{manifest_name} operator is not installed")
+
+    run.run(f"oc delete sub/{manifest_name} -n {namespace} --ignore-not-found")
+    run.run(f"oc delete csv -n {namespace} -loperators.coreos.com/{manifest_name}.{namespace}")
+
+    for crd in cleanup.get("crds", []):
+        run.run(f"oc delete crd/{crd} --ignore-not-found")
+
+    for ns in cleanup.get("namespaces", []):
+        run.run(f"timeout 300 oc delete ns {ns} --ignore-not-found")
