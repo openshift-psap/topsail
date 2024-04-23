69e10143165565c047b8e27b807cda8bb531fa27
Kevin Pouget <kpouget@redhat.com>
2024-04-23 10:21:44 +0200

    testing: utils: deploy Image for accessing ODH nightlies

--- testing/utils/brew.registry.redhat.io/setup.sh
+++ testing/utils/brew.registry.redhat.io/setup.sh
@@ -10,6 +10,7 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OCP_PULL_SECRET="secret/pull-secret -n openshift-config"
 SERVER=brew.registry.redhat.io
 IMAGE_CONTENT_SOURCE_POLICY="$THIS_DIR/brew-registry-icsp.yaml"
+IMAGE_CONFIG="$THIS_DIR/image-config.yaml"
 
 echo "Checking if the ICSP already exists ..."
 if oc get -f ./brew-registry-icsp.yaml 2>/dev/null; then
@@ -43,5 +44,9 @@ oc set data $OCP_PULL_SECRET --from-file=.dockerconfigjson=<(echo "$updated_pull
 echo "Applying the $SERVER ImageContentSourcePolicy"
 
 oc apply -f "$IMAGE_CONTENT_SOURCE_POLICY"
+
+# to access ODH nightlies in registry-proxy.engineering.redhat.com
+# without hitting certificate errors
+oc apply -f "$IMAGE_CONFIG"
 
 echo "Done."
