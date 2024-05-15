3ec8e67e2e956043db36a1777aae3a81198fdba1
Drew Ripberger <drew.ripberger@gmail.com>
2024-05-15 17:09:16 -0400

    [finetuning] adapting role for evaluation

--- projects/fine_tuning/roles/fine_tuning_evaluate_model/files/entrypoint/entrypoint.sh
+++ projects/fine_tuning/roles/fine_tuning_evaluate_model/files/entrypoint/entrypoint.sh
@@ -9,33 +9,36 @@ set -x
 echo "Source dataset: $DATASET_SOURCE"
 
 if [[ "${DATASET_TRANSFORM:-}" ]]; then
-    echo "Dataset transformation: $DATASET_TRANSFORM"
+	echo "Dataset transformation: $DATASET_TRANSFORM"
 
-    python "$DATASET_TRANSFORM" "$DATASET_SOURCE" "$DATASET_DEST"
+	python "$DATASET_TRANSFORM" "$DATASET_SOURCE" "$DATASET_DEST"
 else
-    cp "$DATASET_SOURCE" "$DATASET_DEST"
+	cp "$DATASET_SOURCE" "$DATASET_DEST"
 fi
 
 if [[ "${DATASET_REPLICATION:-1}" != 1 ]]; then
-    echo "Dataset replication factor: $DATASET_REPLICATION"
-    python /mnt/entrypoint/convert_replicate.py "$DATASET_DEST" /tmp/temp_ds.json "$DATASET_REPLICATION"
-    mv /tmp/temp_ds.json "$DATASET_DEST"
+	echo "Dataset replication factor: $DATASET_REPLICATION"
+	python /mnt/entrypoint/convert_replicate.py "$DATASET_DEST" /tmp/temp_ds.json "$DATASET_REPLICATION"
+	mv /tmp/temp_ds.json "$DATASET_DEST"
 fi
 
 echo "SFT-Trainer configuration:"
 cat "$SFT_TRAINER_CONFIG_JSON_PATH"
 
 if [[ -e /dev/nvidiactl ]]; then
-    echo "# GPU available:"
-    nvidia-smi -L
+	echo "# GPU available:"
+	nvidia-smi -L
 else
-    echo "No GPU seem to be available."
+	echo "No GPU seem to be available."
 fi
 
 if [[ -z "${NUM_GPUS:-1}" || "${NUM_GPUS:-1}" == 1 ]]; then
-    echo "Running with a single process"
-    exec python launch_training.py
+	echo "Running with a single process"
+	exec python launch_training.py
 fi
 
 echo "Running with $NUM_GPUS GPUs"
-exec python /app/accelerate_launch.py
+echo "Running evaluation script"
+echo "model=$MODEL_PATH"
+echo "data_path=$DATASET_DEST"
+exec python /tmp/fms-hf-tuning/scripts/run_evaluation.py --model $MODEL_PATH
--- projects/fine_tuning/toolbox/fine_tuning.py
+++ projects/fine_tuning/toolbox/fine_tuning.py
@@ -64,3 +64,52 @@ class Fine_Tuning:
         """
 
         return RunAnsibleRole(locals())
+
+    @AnsibleRole("fine_tuning_evaluate_model")
+    @AnsibleMappedParams
+    def run_fine_tuning_job(
+            self,
+            name,
+            namespace,
+            pvc_name,
+
+            model_name,
+            dataset_name,
+            dataset_replication=1,
+            dataset_transform=None,
+            container_image="quay.io/modh/fms-hf-tuning:01b3824c9aba22d9d0695399681e6f0507840e7f",
+
+            gpu=0,
+            memory=10,
+            cpu=1,
+            request_equals_limits=False,
+
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
+            use_flash_attn=False,
+
+    ):
+        """
+        Run a simple fine-tuning Job.
+
+        Args:
+          name: the name of the fine-tuning job to create
+          namespace: the name of the namespace where the scheduler load will be generated
+          pvc_name: the name of the PVC where the model and dataset are stored
+
+          model_name: the name of the model to use inside the /dataset directory of the PVC
+          dataset_name: the name of the dataset to use inside the /model directory of the PVC
+          dataset_replication: number of replications of the dataset to use, to artificially extend or reduce the fine-tuning effort
+          dataset_transform: name of the transformation to apply to the dataset
+          container_image: the image to use for the fine-tuning container
+          gpu: the number of GPUs to request for the fine-tuning job
+          memory: the number of RAM gigs to request for to the fine-tuning job (in Gigs)
+          cpu: the number of CPU cores to request for the fine-tuning job (in cores)
+          request_equals_limits: if True, sets the 'limits' of the job with the same value as the request.
+
+          per_device_train_batch_size: batch size to use for the model fine-tuning training
+          per_device_eval_batch_size: batch size to use for the model fine-tuning evaluation
+          use_flash_attn: Enables/disables Flash Attention
+        """
+
+        return RunAnsibleRole(locals())
