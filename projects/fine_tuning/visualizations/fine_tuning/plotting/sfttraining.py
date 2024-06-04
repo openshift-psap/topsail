8b8de46270451f50c1b5fc9790147b4136718a10
Kevin Pouget <kpouget@redhat.com>
2024-06-04 11:17:54 +0200

    [fine_tuning] visualizations: fine_tuning: use the line plot more often

--- projects/fine_tuning/visualizations/fine_tuning/plotting/sfttraining.py
+++ projects/fine_tuning/visualizations/fine_tuning/plotting/sfttraining.py
@@ -4,6 +4,7 @@ import logging
 import datetime
 import math
 import copy
+import numbers
 
 import statistics as stats
 
@@ -107,8 +108,20 @@ class SFTTraining():
         elif cfg__efficiency:
             y_key += "_efficiency"
 
+
         if has_gpu or has_speedup:
-            fig = px.line(df, hover_data=df.columns, x=x_key, y=y_key, color="name")
+            do_line_plot = True
+
+        elif len(variables) == 1:
+            do_line_plot = all(isinstance(v, numbers.Number) for v in list(variables.values())[0])
+
+        else:
+            do_line_plot = False
+
+
+        if do_line_plot:
+            color = None if len(variables) == 1 else "name"
+            fig = px.line(df, hover_data=df.columns, x=x_key, y=y_key, color=color)
 
             for i in range(len(fig.data)):
                 fig.data[i].update(mode='lines+markers+text')
