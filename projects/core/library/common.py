354efd4266335abcc3e03d8d1946d1efdeeb0930
Kevin Pouget <kpouget@redhat.com>
2024-06-19 16:30:48 +0200

    [core] library: common: include the busy-cluster cleanup

--- projects/core/library/common.py
+++ projects/core/library/common.py
@@ -1,3 +1,6 @@
+import logging
+
+
 from . import run
 
 def prepare_light_cluster():
@@ -5,4 +8,13 @@ def prepare_light_cluster():
 
 
 def cleanup_cluster():
-    pass
+    busy_cluster_ns_label_key = "busy-cluster.topsail"
+    busy_cluster_ns_label_value = "yes"
+    has_busy_cluster_ns = run.run(f"oc get ns -oname -l{busy_cluster_ns_label_key}={busy_cluster_ns_label_value}", capture_stdout=True).stdout
+    if not has_busy_cluster_ns:
+        logging.info("No busy-cluster namespace detected, nothing to cleanup.")
+        return
+
+    logging.info(f"Detected {len(has_busy_cluster_ns.split())} busy-cluster namespaces. Running the cleanup.")
+
+    run.run_toolbox("busy_cluster", "cleanup")
