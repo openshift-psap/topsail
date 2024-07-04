517093b0ed82d09ab830b37346e718cadd512646
Kevin Pouget <kpouget@redhat.com>
2024-07-04 16:08:56 +0200

    [fine_tuning] roles: fine_tuning_run_fine_tuning_job: include dataset filtering based on the max_seq_lengh

---
+++ projects/fine_tuning/roles/fine_tuning_run_fine_tuning_job/files/entrypoint/convert_dataset_helper.py
@@ -0,0 +1,56 @@
+from transformers import AutoTokenizer
+import logging
+import json
+import os
+
+logger = logging.getLogger("sft_trainer")
+
+def load_fms_hf_tuning_configuration():
+    config_file = os.environ.get("SFT_TRAINER_CONFIG_JSON_PATH")
+    if not config_file:
+        logging.warn("No SFT_TRAINER_CONFIG_JSON_PATH available...")
+        return None
+
+    with open(config_file) as f:
+        config = json.load(f)
+
+    return config
+
+
+def get_tokenizer(path_model):
+    tokenizer = AutoTokenizer.from_pretrained(path_model)
+
+    special_tokens_dict = dict()
+
+    DEFAULT_PAD_TOKEN = "<PAD>"
+    DEFAULT_EOS_TOKEN = "</s>"
+    DEFAULT_BOS_TOKEN = "<s>"
+    DEFAULT_UNK_TOKEN = "<unk>"
+
+    if tokenizer.pad_token is None:
+        logger.warning("PAD token set to default, missing in tokenizer")
+        special_tokens_dict["pad_token"] = DEFAULT_PAD_TOKEN
+    if tokenizer.eos_token is None:
+        logger.warning("EOS token set to default, missing in tokenizer")
+        special_tokens_dict["eos_token"] = DEFAULT_EOS_TOKEN
+    if tokenizer.bos_token is None:
+        logger.warning("BOS token set to default, missing in tokenizer")
+        special_tokens_dict["bos_token"] = DEFAULT_BOS_TOKEN
+    if tokenizer.unk_token is None:
+        logger.warning("UNK token set to default, missing in tokenizer")
+        special_tokens_dict["unk_token"] = DEFAULT_UNK_TOKEN
+
+    tokenizer.add_special_tokens(special_tokens_dict)
+
+    return tokenizer
+
+
+def get_tokens(tokenizer, line):
+    data = json.loads(line)
+    decoded = tokenizer.encode(data["output"], padding=True)
+
+    return decoded
+
+
+def get_token_count(tokenizer, line):
+    return len(get_tokens(tokenizer, line))
--- projects/fine_tuning/roles/fine_tuning_run_fine_tuning_job/files/entrypoint/convert_replicate.py
+++ projects/fine_tuning/roles/fine_tuning_run_fine_tuning_job/files/entrypoint/convert_replicate.py
@@ -3,11 +3,19 @@
 import sys
 import pathlib
 
+import convert_dataset_helper
+
+fms_config = convert_dataset_helper.load_fms_hf_tuning_configuration()
+
+max_seq_length = fms_config["max_seq_length"]
+tokenizer = convert_dataset_helper.get_tokenizer(fms_config["model_name_or_path"])
+
 src = pathlib.Path(sys.argv[1])
 dst = pathlib.Path(sys.argv[2])
 FACTOR = float(sys.argv[3])
 
 print(f"Replicating {src} with a factor of {FACTOR}...")
+print(f"Filtering out samples with more than {max_seq_length=} tokens")
 
 with open(src) as src_f:
     orig_length = len(src_f.readlines())
@@ -16,12 +24,17 @@ with open(src) as src_f:
 dst.unlink(missing_ok=True)
 
 factor = FACTOR
-
+samples_too_long = 0
 while factor >= 1:
     print(f"Saving 1x {src} ...")
     with open(src) as src_f, open(dst, "a") as dst_f:
         for line in src_f.readlines():
+            if convert_dataset_helper.get_token_count(tokenizer, line) > max_seq_length:
+                if factor == FACTOR: # count them only once
+                    samples_too_long += 1
+                continue
             print(line.strip(), file=dst_f)
+
     factor -= 1
 
     with open(dst) as dst_f:
@@ -35,9 +48,22 @@ if 0 < factor < 1:
 
     with open(src) as src_f:
         with open(dst, "a") as dst_f:
-            for _ in range(newline_count):
-                print(src_f.readline().strip(), file=dst_f)
+            lines = 0
+            while lines < newline_count:
+                line = src_f.readline()
+
+                if convert_dataset_helper.get_token_count(tokenizer, line) > max_seq_length:
+                    if factor == FACTOR: # count them only once
+                        samples_too_long += 1
+                    continue
+
+                print(line.strip(), file=dst_f)
+                lines += 1
+
 
 with open(dst) as dst_f:
     new_length = len(dst_f.readlines())
-    print(f"Length of {dst}: {new_length} lines")
+
+
+print(f"Length of {dst}: {new_length} lines")
+print(f"Removed {samples_too_long} samples longer than {max_seq_length=} tokens.")
