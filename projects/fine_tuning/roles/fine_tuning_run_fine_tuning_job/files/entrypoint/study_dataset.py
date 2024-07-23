1f3632df36f5fb8058bf44b1af537f69afe35b1e
Kevin Pouget <kpouget@redhat.com>
2024-07-23 09:08:35 +0200

    [fine_tuning] roles: fine_tuning_run_fine_tuning_job: files/entrypoint/study_dataset: take into account the max_seq_length in the token count

--- projects/fine_tuning/roles/fine_tuning_run_fine_tuning_job/files/entrypoint/study_dataset.py
+++ projects/fine_tuning/roles/fine_tuning_run_fine_tuning_job/files/entrypoint/study_dataset.py
@@ -11,21 +11,28 @@ import convert_dataset_helper
 try:
     tokenizer_name = sys.argv[1]
     dataset = pathlib.Path(sys.argv[2])
+    max_seq_length = int(sys.argv[3])
 except Exception:
     logging.info("Loading from FMS_HF_TUNING configuration ...")
     fms_config = convert_dataset_helper.load_fms_hf_tuning_configuration()
 
     dataset = fms_config["training_data_path"]
     tokenizer_name = fms_config["tokenizer_name_or_path"]
+    max_seq_length = fms_config["max_seq_length"]
 
 logging.info("Loading the tokenizer ...")
 tokenizer = convert_dataset_helper.get_tokenizer(tokenizer_name)
+tokenizer_model_max_length = tokenizer.model_max_length
 
 logging.info("Parsing the dataset ...")
 token_counts = []
 with open(dataset, "r") as f:
     for line in f.readlines():
-        token_counts.append(convert_dataset_helper.get_token_count(tokenizer, line))
+        data = json.loads(line)
+        decoded = tokenizer.encode(data["output"], padding=True)
+        sample_token_count = convert_dataset_helper.get_token_count(tokenizer, line)
+        actual_token_count = min(sample_token_count, max_seq_length, tokenizer_model_max_length)
+        token_counts.append(actual_token_count)
 
 if len(token_counts) < 2:
     logging.warning("Not enough samples found in the dataset ...")
