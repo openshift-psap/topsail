17dfae3ae3595e99cba71d3c6342acbbdad58676
Kevin Pouget <kpouget@redhat.com>
2024-08-28 13:22:54 +0200

    [kserve] visualizations: kserve-llm: analyze: use the helper function

--- projects/kserve/visualizations/kserve-llm/analyze/__init__.py
+++ projects/kserve/visualizations/kserve-llm/analyze/__init__.py
@@ -1,35 +1,13 @@
-import pandas as pd
-
 import logging
 
-import matrix_benchmarking.common as common
+import projects.core.visualizations.helpers.analyze as core_helpers_analyze
+
 from ..store import _rewrite_settings
 
-COMPARISON_KEY = "rhoai_version"
+COMPARISON_KEYS = ["rhoai_version"]
 
-_IGNORED_KEYS = ["runtime_image", "ocp_version"]
+IGNORED_KEYS = ["runtime_image", "ocp_version"]
 
-def run():
-    logging.info("Running the regression analyses ...")
 
-    data = []
-    for ref_entry in common.Matrix.all_records():
-        lts_payload = ref_entry.results.lts
-
-        if not hasattr(lts_payload, "kpis"):
-            logging.warning("No KPIs available ...")
-            continue
-
-        kpis = lts_payload.kpis
-
-        current_row = dict(ref=ref_entry)
-
-        for entry in common.LTS_Matrix.similar_records(ref_entry.results.lts.metadata.settings,
-                                                       ignore_keys=([COMPARISON_KEY] + _IGNORED_KEYS),
-                                                       rewrite_settings=_rewrite_settings,
-                                                       gathered=True):
-            current_row[entry.settings.rhoai_version] = entry
-
-        data.append(current_row)
-
-    return pd.DataFrame(data)
+def prepare():
+    return core_helpers_analyze.prepare_regression_data(COMPARISON_KEYS, IGNORED_KEYS, _rewrite_settings)
