9eaf6af9004d9d9a2c16617615250c3859a74d7d
Kevin Pouget <kpouget@redhat.com>
2024-09-05 12:22:04 +0200

    [kserve] visualizations: kserve-llm: analyze/__init__: define the prefered sorting keys

--- projects/kserve/visualizations/kserve-llm/analyze/__init__.py
+++ projects/kserve/visualizations/kserve-llm/analyze/__init__.py
@@ -4,10 +4,15 @@ import projects.core.visualizations.helpers.analyze as core_helpers_analyze
 
 from ..store import _rewrite_settings
 
+# the setting (kpi labels) keys against which the historical regression should be performed
 COMPARISON_KEYS = ["rhoai_version"]
 
+# the setting (kpi labels) keys that should be ignored when searching for historical results
 IGNORED_KEYS = ["runtime_image", "ocp_version"]
 
+# the setting (kpi labels) keys *prefered* for sorting the entries in the regression report
+SORTING_KEYS = ["model_name", "virtual_users"]
 
 def prepare():
-    return core_helpers_analyze.prepare_regression_data(COMPARISON_KEYS, IGNORED_KEYS, _rewrite_settings)
+    return core_helpers_analyze.prepare_regression_data(COMPARISON_KEYS, IGNORED_KEYS, _rewrite_settings,
+                                                        sorting_keys=SORTING_KEYS)
