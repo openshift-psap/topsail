a2bbac8cf096f7862de26936908eb35c5202f21c
Kevin Pouget <kpouget@redhat.com>
2024-09-12 01:55:35 +0200

    [notebooks] visualizations: rhods-notebooks-performance: models/kpi: use the LowerBetter annotation

--- projects/notebooks/visualizations/rhods-notebooks-performance/models/kpi.py
+++ projects/notebooks/visualizations/rhods-notebooks-performance/models/kpi.py
@@ -6,20 +6,21 @@ import matrix_benchmarking.models as matbench_models
 
 KPIs = {} # populated by the @matbench_models.KPIMetadata decorator
 
-
-@matbench_models.KPIMetadata(help="minimum execution time of the benchmark", unit="seconds", lower_better=True)
+@matbench_models.LowerBetter
+@matbench_models.KPIMetadata(help="minimum execution time of the benchmark", unit="seconds")
 def notebook_performance_benchmark_min_time(lts_payload):
     return min(lts_payload.results.benchmark_measures.measures)
 
 
-@matbench_models.KPIMetadata(help="difference between the maximum and minimum execution time of the benchmark", unit="seconds", lower_better=True)
+@matbench_models.LowerBetter
+@matbench_models.KPIMetadata(help="difference between the maximum and minimum execution time of the benchmark", unit="seconds")
 def notebook_performance_benchmark_min_max_diff(lts_payload):
     min_value = min(lts_payload.results.benchmark_measures.measures)
     max_value = max(lts_payload.results.benchmark_measures.measures)
 
     return max_value - min_value
 
-
-@matbench_models.KPIMetadata(help="list of the execution time of the benchmark", unit="seconds", lower_better=True)
+@matbench_models.LowerBetter
+@matbench_models.KPIMetadata(help="list of the execution time of the benchmark", unit="seconds")
 def notebook_performance_benchmark_time(lts_payload):
     return lts_payload.results.benchmark_measures.measures
