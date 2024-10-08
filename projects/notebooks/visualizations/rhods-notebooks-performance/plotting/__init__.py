85212621e565e80b0516239499581b427ae8f324
Kevin Pouget <kpouget@redhat.com>
2024-10-08 17:57:47 +0200

    [projects] visualization: include the KPI_Table report

--- projects/notebooks/visualizations/rhods-notebooks-performance/plotting/__init__.py
+++ projects/notebooks/visualizations/rhods-notebooks-performance/plotting/__init__.py
@@ -4,6 +4,7 @@ from . import gating_report
 from . import notebook_performance
 
 import projects.matrix_benchmarking.visualizations.helpers.plotting.lts_documentation as lts_documentation
+import projects.matrix_benchmarking.visualizations.helpers.plotting.kpi_table as kpi_table
 
 def register():
     report.register()
@@ -11,3 +12,4 @@ def register():
     gating_report.register()
     notebook_performance.register()
     lts_documentation.register()
+    kpi_table.register()
