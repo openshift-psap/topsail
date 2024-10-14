d1d2a20d411de5148fb6a1fab9e34511a18f5c78
Kevin Pouget <kpouget@redhat.com>
2024-10-14 14:08:51 +0200

    [fine_tuning] testing: test_finetuning: use the Prom library

--- projects/fine_tuning/testing/test_finetuning.py
+++ projects/fine_tuning/testing/test_finetuning.py
@@ -15,6 +15,7 @@ from collections import defaultdict
 from projects.core.library import env, config, run, merge_dicts
 from projects.matrix_benchmarking.library import visualize, matbenchmark
 from projects.rhods.library import prepare_rhoai as prepare_rhoai_mod
+from projects.cluster.library import prom
 
 import prepare_finetuning
 
@@ -22,81 +23,6 @@ TESTING_THIS_DIR = pathlib.Path(__file__).absolute().parent
 TOPSAIL_DIR = pathlib.Path(config.__file__).parents[3]
 RUN_DIR = pathlib.Path(os.getcwd()) # for run_one_matbench
 os.chdir(TOPSAIL_DIR)
-
-
-def reset_prometheus(delay=60):
-    capture_prom = config.project.get_config("tests.capture_prom")
-    if not capture_prom:
-        logging.info("tests.capture_prom is disabled, skipping Prometheus DB reset")
-        return
-
-    prom_start_ts = datetime.datetime.now()
-
-    if capture_prom == "with-queries":
-        return prom_start_ts
-
-    if config.project.get_config("tests.dry_mode"):
-        logging.info("tests.dry_mode is enabled, skipping Prometheus DB reset")
-        return
-
-    with run.Parallel("cluster__reset_prometheus_dbs") as parallel:
-        parallel.delayed(run.run_toolbox, "cluster", "reset_prometheus_db", mute_stdout=True)
-        if config.project.get_config("tests.capture_prom_uwm"):
-            parallel.delayed(run.run_toolbox_from_config, "cluster", "reset_prometheus_db", suffix="uwm", artifact_dir_suffix="_uwm", mute_stdout=True)
-
-    logging.info(f"Wait {delay}s for Prometheus to restart collecting data ...")
-    time.sleep(delay)
-
-    # at the moment, only used when capture_prom == "with-queries".
-    # Returned for consistency.
-    return prom_start_ts
-
-
-def dump_prometheus(prom_start_ts, delay=60):
-    capture_prom = config.project.get_config("tests.capture_prom")
-    if not capture_prom:
-        logging.info("tests.capture_prom is disabled, skipping Prometheus DB dump")
-        return
-
-    if config.project.get_config("tests.dry_mode"):
-        logging.info("tests.dry_mode is enabled, skipping Prometheus DB dump")
-        return
-
-    if capture_prom == "with-queries":
-        if config.project.get_config("tests.capture_prom_uwm"):
-            logging.error("tests.capture_prom_uwm not supported with capture Prom with queries")
-
-        prom_end_ts = datetime.datetime.now()
-        args = dict(
-            duration_s = (prom_end_ts - prom_start_ts).total_seconds(),
-            promquery_file = TESTING_THIS_DIR / "metrics.txt",
-            dest_dir = env.ARTIFACT_DIR / "metrics",
-            namespace = config.project.get_config("tests.fine_tuning.namespace"),
-        )
-
-        with env.NextArtifactDir("cluster__dump_prometheus_dbs"):
-            run.run_toolbox("cluster", "query_prometheus_db", **args)
-            with env.NextArtifactDir("cluster__dump_prometheus_db"):
-                with open(env.ARTIFACT_DIR / "prometheus.tar.dummy", "w") as f:
-                    print(f"""This file is a dummy.
-Metrics have been queried from Prometheus and saved into {args['dest_dir']}.
-Keep this file here, so that 'projects/fine_tuning/visualizations/fine_tuning_prom/store/parsers.py' things Prometheus metrics have been captured,
-and it directly processes the cached files from the metrics directory.""", file=f)
-                nodes = run.run("oc get nodes -ojson", capture_stdout=True)
-                with open(env.ARTIFACT_DIR / "nodes.json", "w") as f:
-                    print(nodes.stdout.strip(), file=f)
-
-        return
-
-    # prom_start_ts not used when during full prometheus dump.
-
-    logging.info(f"Wait {delay}s for Prometheus to finish collecting data ...")
-    time.sleep(delay)
-
-    with run.Parallel("cluster__dump_prometheus_dbs") as parallel:
-        parallel.delayed(run.run_toolbox, "cluster", "dump_prometheus_db", mute_stdout=True)
-        if config.project.get_config("tests.capture_prom_uwm"):
-            parallel.delayed(run.run_toolbox_from_config, "cluster", "dump_prometheus_db", suffix="uwm", artifact_dir_suffix="_uwm", mute_stdout=True)
 
 
 def generate_prom_results(expe_name, prom_start_ts):
@@ -114,7 +40,8 @@ def generate_prom_results(expe_name, prom_start_ts):
     with open(env.ARTIFACT_DIR / "config.yaml", "w") as f:
         yaml.dump(config.project.config, f, indent=4)
 
-    dump_prometheus(prom_start_ts)
+    namespace = config.project.get_config("tests.fine_tuning.namespace")
+    prom.dump_prometheus(prom_start_ts, namespace)
 
     if (config.project.get_config("tests.capture_state")
         and not config.project.get_config("tests.dry_mode")
@@ -174,7 +101,7 @@ def _run_test(test_artifact_dir_p, test_override_values, job_index=None):
     _start_ts = datetime.datetime.now()
     start_ts = None
     if not do_multi_model:
-        prom_start_ts = reset_prometheus()
+        prom_start_ts = prom.reset_prometheus()
 
     test_dir_name = "evaluate_quality" if do_quality_evaluation \
         else "test_fine_tuning"
@@ -248,7 +175,7 @@ def _run_test_multi_model(test_artifact_dir_p):
             with lock:
                 failed = True
 
-    prom_start_ts = reset_prometheus()
+    prom_start_ts = prom.reset_prometheus()
 
     with env.NextArtifactDir("multi_model"):
         test_artifact_dir_p[0] = env.ARTIFACT_DIR
