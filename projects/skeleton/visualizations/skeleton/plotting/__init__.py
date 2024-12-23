47a8a7bff7ec121d9503feef6bdc3db245635d82
Kevin Pouget <kpouget@redhat.com>
2024-12-23 20:40:31 +0100

    [skeleton] visualizations: skeleton: plotting: add missing import

--- projects/skeleton/visualizations/skeleton/plotting/__init__.py
+++ projects/skeleton/visualizations/skeleton/plotting/__init__.py
@@ -1,6 +1,7 @@
 from . import error_report
 from ..store import prom
 from . import prom_report
+from . import report
 
 import projects.matrix_benchmarking.visualizations.helpers.plotting.lts_documentation as lts_documentation
 import projects.matrix_benchmarking.visualizations.helpers.plotting.kpi_table as kpi_table
