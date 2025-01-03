5b1269368c94c578502a74681b4a4426458479fb
Alberto Perdomo <aperdomo@redhat.com>
2025-01-03 08:14:01 +0000

    [fine_tuning]: remove launch_training command

--- projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
+++ projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
@@ -19,16 +19,3 @@ if [[ $WORLD_SIZE == 1 ]]; then
     time python /app/accelerate_launch.py
     exit 0
 fi
-echo "Running on $WORLD_SIZE machines with $NUM_GPUS GPUs each."
-
-time accelerate launch \
-     --debug \
-     --machine_rank $RANK \
-     --num_machines $WORLD_SIZE \
-     --num_processes $WORLD_SIZE \
-     --main_process_ip $MASTER_ADDR \
-     --main_process_port $MASTER_PORT \
-     --mixed_precision no \
-     --dynamo_backend no \
-     --multi_gpu \
-     launch_training.py
