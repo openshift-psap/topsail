104a214a73e44407c7981a8c87aacb7eca46ef13
Alberto Perdomo <aperdomo@redhat.com>
2025-01-07 09:20:48 +0000

    [fine_tuning]: simple refactor

--- projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
+++ projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
@@ -8,14 +8,17 @@ set -x
 
 export SFT_TRAINER_CONFIG_JSON_PATH=$CONFIG_JSON_PATH
 
-if [[ $WORLD_SIZE == 1 ]]; then
-    echo "Running on a single machine."
-
-    if [[ -z "${NUM_GPUS:-1}" || "${NUM_GPUS:-1}" == 1 ]]; then
-        echo "Running with a single GPU"
-    else
-        echo "Running with a $NUM_GPUS GPUs"
-    fi
-    time python /app/accelerate_launch.py
-    exit 0
+if [[ $WORLD_SIZE != 1 ]]; then
+    echo "Running with a multi-node configuration. This is not supported at the moment, aborting."
+    exit 1
 fi
+
+echo "Running on a single machine."
+
+if [[ -z "${NUM_GPUS:-1}" || "${NUM_GPUS:-1}" == 1 ]]; then
+    echo "Running with a single GPU"
+else
+    echo "Running with a $NUM_GPUS GPUs"
+fi
+
+time python /app/accelerate_launch.py
