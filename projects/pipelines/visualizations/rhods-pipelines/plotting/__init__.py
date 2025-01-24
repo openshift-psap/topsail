0699a48ecebf3b6319a3d3e43e2f8daabf3140d1
Kevin Pouget <kpouget@redhat.com>
2025-01-24 15:17:08 +0100

    [projects] visualizations: plotting: register the kpi_plot module

--- projects/pipelines/visualizations/rhods-pipelines/plotting/__init__.py
+++ projects/pipelines/visualizations/rhods-pipelines/plotting/__init__.py
@@ -9,6 +9,7 @@ from . import mapping
 
 import projects.matrix_benchmarking.visualizations.helpers.plotting.lts_documentation as lts_documentation
 import projects.matrix_benchmarking.visualizations.helpers.plotting.kpi_table as kpi_table
+import projects.matrix_benchmarking.visualizations.helpers.plotting.kpi_plot as kpi_plot
 
 def register():
     error_report.register()
@@ -21,3 +22,4 @@ def register():
 
     lts_documentation.register()
     kpi_table.register()
+    kpi_plot.register()
