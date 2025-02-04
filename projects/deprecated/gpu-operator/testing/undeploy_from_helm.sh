a48a5c3279a72caa09fc70bdc61ef4819b40851d
Carlos Camacho <carloscamachoucv@gmail.com>
2025-02-04 12:48:38 +0100

    Initial refactor part-1

--- projects/deprecated/gpu-operator/testing/undeploy_from_helm.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ${THIS_DIR}/../_common.sh
-
-source ${THIS_DIR}/_helm_common.sh
-
-exec bash -x "${HELM_DEPLOY_OPERATOR}" undeploy
--- projects/deprecated/load-aware/visualizations/load-aware/store/lts.py
+++
@@ -1,5 +0,0 @@
-def build_lts_payloads():
-    return []
-
-def register_lts_metric(cluster_role, metric):
-    pass
