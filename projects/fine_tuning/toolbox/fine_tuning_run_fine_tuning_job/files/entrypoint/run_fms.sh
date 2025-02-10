55b4169ad43e6715a1307f1665e14526236961a4
Alberto Perdomo <aperdomo@redhat.com>
2025-02-10 15:32:26 +0000

    [fine_tuning]: Test fast torchrun settings

--- projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
+++ projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
@@ -29,6 +29,25 @@ python -m accelerate.commands.launch \
   --dynamo_backend="no" \
   -m tuning.sft_trainer
 
+python -m torch.distributed.run \
+  --nproc_per_node=$NUM_GPUS \
+  --nnodes=$WORLD_SIZE \
+  --node_rank=$RANK \
+  --master_addr=$MASTER_ADDR \
+  --master_port=$MASTER_PORT \
+  --module tuning.sft_trainer \
+  --fsdp_auto_wrap_policy=TRANSFORMER_BASED_WRAP \
+  --fsdp_backward_prefetch=BACKWARD_PRE \
+  --fsdp_forward_prefetch=False \
+  --fsdp_offload_params=False \
+  --fsdp_state_dict_type=FULL_STATE_DICT \
+  --fsdp_sync_module_states=True \
+  --fsdp_use_orig_params=False \
+  --fsdp_backward_prefetch_policy=BACKWARD_PRE \
+  --fsdp_sharding_strategy=1 \
+  --fsdp_cpu_ram_efficient_loading=True \
+  --fsdp_sync_module_states=True
+
 if [[ -n "${RETRIEVE:-}" ]] && [[ "$RANK" -eq 0 ]]; then
     # NOTE: Write here the code to copy any file you want to export to the test artifacts
     cp /etc/os-release "$RETRIEVE"
