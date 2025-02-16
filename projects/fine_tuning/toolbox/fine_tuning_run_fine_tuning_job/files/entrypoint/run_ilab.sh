91248911d24e20b163379f604ad487193b5f0e2a
tosokin <tosokin@redhat.com>
2025-02-16 18:21:57 +0200

    debug a bug

--- projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_ilab.sh
+++ projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_ilab.sh
@@ -45,7 +45,7 @@ fi
 
 if [[ "${NCCL_SOCKET_IFNAME:-}" ]]; then
 
-
+    echo "$ARTIFACT_EXTRA_LOGS_DIR"
     MAPPING="$(cat "$ARTIFACT_EXTRA_LOGS_DIR/src/nodename_ip_mapping.yaml")"
     for ifname in $(echo $NCCL_SOCKET_IFNAME | tr , " "); do
         current_ip=$(ip route | grep "$ifname " | cut -d" " -f9)
