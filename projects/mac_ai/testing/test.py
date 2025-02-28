368ca3a68c2a7f937a891904850767b39aeb1c4e
Kevin Pouget <kpouget@redhat.com>
2025-02-28 10:38:39 +0100

    [mac_ai] testing: test: add workaround to avoid writting into /logs when running from OpenShift CI

--- projects/mac_ai/testing/test.py
+++ projects/mac_ai/testing/test.py
@@ -22,6 +22,30 @@ TESTING_THIS_DIR = pathlib.Path(__file__).absolute().parent
 
 CRC_MAC_AI_SECRET_PATH = pathlib.Path(os.environ.get("CRC_MAC_AI_SECRET_PATH", "/env/CRC_MAC_AI_SECRET_PATH/not_set"))
 
+def openshift_ci_update_artifact_dir():
+    if os.environ.get("OPENSHIFT_CI") != "true": return
+    if os.environ.get("TOPSAIL_JUMP_CI_INSIDE_JUMP_HOST") != "true": return
+
+    artifact_dir = os.environ["ARTIFACT_DIR"]
+    if not artifact_dir.startswith("/logs/artifacts"): return
+
+    # in OpenShift CI, the test starts with ARTIFACT_DIR=/logs/artifacts
+    # this ARTIFACT_DIR value cannot be used in the remote Mac (rootfs is readonly, so cannot cheat)
+    #
+    # below, we create a symlink from /tmp/topsail_$DATE to /logs/artifacts
+    # and define ARTIFACT_DIR=/tmp/topsail_$DATE
+    # so that the *jump-host* indirectly writes into /logs/artifacts
+    # (that's where its artifacts will be collected from )
+    # and the Mac writes into /tmp/topsail_$DATE
+    # (where it's allowed to write)
+
+    ts = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+    new_artifact_dir = pathlib.Path("/tmp") / f"topsail_{ts}"
+
+    logging.info(f"openshift_ci_update_artifact_dir: Creating a symlink {new_artifact_dir} --> {artifacts}")
+    new_artifact_dir.symlink_to(artifact_dir)
+    os.environ["ARTIFACT_DIR"] = str(new_artifact_dir)
+
 initialized = False
 def init(ignore_secret_path=False, apply_preset_from_pr_args=True):
     global initialized
@@ -32,6 +56,8 @@ def init(ignore_secret_path=False, apply_preset_from_pr_args=True):
 
     env.init()
     config.init(TESTING_THIS_DIR, apply_preset_from_pr_args)
+
+    openshift_ci_update_artifact_dir()
 
     if not ignore_secret_path:
         if not CRC_MAC_AI_SECRET_PATH.exists():
