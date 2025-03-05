6f85d9e56c89e6af9c3ace00719225f3b2bd89aa
Alberto Perdomo <aperdomo@redhat.com>
2025-03-05 14:22:31 +0000

    [fine_tuning]: Remove file retrieval now

--- projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
+++ projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_fms.sh
@@ -49,5 +49,4 @@ time python /app/accelerate_launch.py
 if [[ -n "${RETRIEVE:-}" ]] && [[ "$RANK" -eq 0 ]]; then
     # NOTE: Write here the code to copy any file you want to export to the test artifacts
     cp /etc/os-release "$RETRIEVE"
-    cp /tmp/profile_step_10_20.pickle "$RETRIEVE"
 fi
