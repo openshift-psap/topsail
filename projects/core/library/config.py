b470a262d7adeae23d83ba0f2c866bd0dfc98c94
tosokin <tosokin@redhat.com>
2025-03-09 14:03:24 +0200

    debug jsonpath bug

--- projects/core/library/config.py
+++ projects/core/library/config.py
@@ -119,6 +119,9 @@ class Config:
 
     def get_config(self, jsonpath, default_value=..., warn=True, print=True, handled_secretly=False):
         try:
+            logging.info(f"jsonpath: {jsonpath}")
+            logging.info(f"json_pars: {jsonpath_ng.parse(jsonpath)}")
+            logging.info(f"self.config: {self.config}")
             value = jsonpath_ng.parse(jsonpath).find(self.config)[0].value
         except IndexError as ex:
             if default_value != ...:
