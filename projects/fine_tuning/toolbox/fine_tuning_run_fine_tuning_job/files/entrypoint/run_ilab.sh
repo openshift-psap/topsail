f135ce379f9e49c5e44120c5f0fa995954a839af
Tanya Osokin <tosokin@redhat.com>
2025-05-26 15:46:31 +0300

    debug main_ds

--- projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_ilab.sh
+++ projects/fine_tuning/toolbox/fine_tuning_run_fine_tuning_job/files/entrypoint/run_ilab.sh
@@ -92,7 +92,7 @@ export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed s+/usr/local/cuda/compat++)
 echo "STARTING TORCHRUN | $(date)"
 SECONDS=0
 ret=0
-export LOCAL_RANK=0
+sed -i '2a print("    print("LOCAL_RANK: ", local_rank)")' ./opt/app-root/lib/python3.11/site-packages/instructlab/training/main_ds.py
 if ! torchrun \
     --node_rank "${RANK}" \
     --rdzv_endpoint "${MASTER_ADDR}:${MASTER_PORT}" \
