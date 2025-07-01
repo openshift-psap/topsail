825ec9c9c060c9ba337407fa5109b052cb7cf046
Kevin Pouget <kpouget@redhat.com>
2025-07-01 14:49:50 +0200

    [mac_ai] testing: scripts: make more reliable

--- projects/mac_ai/testing/scripts/podman_start_machine.api_remoting.sh
+++ projects/mac_ai/testing/scripts/podman_start_machine.api_remoting.sh
@@ -7,9 +7,19 @@ set -o errtrace
 
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
+export CONTAINERS_MACHINE_PROVIDER=libkrun
+echo ""
+echo "INFO: # for podman to load krunkit/libkrun and not vfkit"
+echo "INFO: Setting CONTAINERS_MACHINE_PROVIDER=$CONTAINERS_MACHINE_PROVIDER"
+echo
+
+if ! podman machine inspect 2>/dev/null >/dev/null; then
+    echo "ERROR: podman machine inspect not working. Did you run 'CONTAINERS_MACHINE_PROVIDER=libkrun podman machine init'?"
+fi
+
 if [[ ! -f "$SCRIPT_DIR/src_info/ramalama.image-info.txt" ]]; then
     echo "ERROR: ramalam.image-info not found in $SCRIPT_DIR/src_info"
-    echo "This is unexpected. Did you move this script from it tarball environment?"
+    echo "INFO: This is unexpected. Did you move this script from it tarball environment?"
     exit 1
 fi
 
@@ -22,50 +32,63 @@ if [[ ! -f "$SCRIPT_DIR/bin/krunkit" ]]; then
 fi
 
 # for krunkit to load the custom virglrenderer library
+echo ""
 export DYLD_LIBRARY_PATH="$SCRIPT_DIR/bin"
-echo "# for krunkit to load the custom virglrenderer library"
-echo "Setting DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH"
+echo "INFO: # for krunkit to load the custom virglrenderer library"
+echo "INFO: Setting DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH"
 echo
 
 # for Virglrendrerer to load the ggml-remotingbackend library
-echo "# for Virglrendrerer to load the ggml-remotingbackend library"
+echo ""
+echo "INFO: # for Virglrendrerer to load the ggml-remotingbackend library"
 export VIRGL_APIR_BACKEND_LIBRARY="$SCRIPT_DIR/bin/libggml-remotingbackend.dylib"
-echo "Setting VIRGL_APIR_BACKEND_LIBRARY=$VIRGL_APIR_BACKEND_LIBRARY"
+echo "INFO: Setting VIRGL_APIR_BACKEND_LIBRARY=$VIRGL_APIR_BACKEND_LIBRARY"
 echo
 
 # for llama.cpp remotingbackend to load the ggml-metal backend
+echo ""
 export APIR_LLAMA_CPP_GGML_LIBRARY_PATH="$SCRIPT_DIR/bin/libggml-metal.dylib"
-echo "# for llama.cpp remotingbackend to load the ggml-metal backend"
-echo "Setting APIR_LLAMA_CPP_GGML_LIBRARY_PATH=$APIR_LLAMA_CPP_GGML_LIBRARY_PATH"
+echo "INFO: # for llama.cpp remotingbackend to load the ggml-metal backend"
+echo "INFO: Setting APIR_LLAMA_CPP_GGML_LIBRARY_PATH=$APIR_LLAMA_CPP_GGML_LIBRARY_PATH"
 echo
 
-echo "# for llama.cpp remotingbackend to lookup the ggml-metal entrypoints"
+echo ""
+echo "INFO: # for llama.cpp remotingbackend to lookup the ggml-metal entrypoints"
 export APIR_LLAMA_CPP_GGML_LIBRARY_REG=ggml_backend_metal_reg
 export APIR_LLAMA_CPP_GGML_LIBRARY_INIT=ggml_backend_metal_init
-echo "Setting APIR_LLAMA_CPP_GGML_LIBRARY_REG=$APIR_LLAMA_CPP_GGML_LIBRARY_REG"
-echo "Setting APIR_LLAMA_CPP_GGML_LIBRARY_INIT=$APIR_LLAMA_CPP_GGML_LIBRARY_INIT"
+echo "INFO: Setting APIR_LLAMA_CPP_GGML_LIBRARY_REG=$APIR_LLAMA_CPP_GGML_LIBRARY_REG"
+echo "INFO: Setting APIR_LLAMA_CPP_GGML_LIBRARY_INIT=$APIR_LLAMA_CPP_GGML_LIBRARY_INIT"
 echo
 
-echo "# to ensure that podman loads libkrun"
-export CONTAINERS_MACHINE_PROVIDER=libkrun
-echo "Setting CONTAINERS_MACHINE_PROVIDER=$CONTAINERS_MACHINE_PROVIDER"
-echo
+export VIRGL_APIR_LOG_TO_FILE=/tmp/apir_virglrenderer.log
+export APIR_LLAMA_CPP_LOG_TO_FILE=/tmp/apir_llama_cpp.log
+echo ""
+echo "INFO: # to store the API Remoting hypervisor logs in a readable location"
+echo "INFO: Setting VIRGL_APIR_LOG_TO_FILE=$VIRGL_APIR_LOG_TO_FILE"
+echo "INFO: Setting APIR_LLAMA_CPP_LOG_TO_FILE=$APIR_LLAMA_CPP_LOG_TO_FILE"
 
-export CONTAINERS_HELPER_BINARY_DIR="$SCRIPT_DIR/krunkit"
+export CONTAINERS_HELPER_BINARY_DIR="$SCRIPT_DIR/bin/"
 
-echo "Restarting podman machine ..."
+echo ""
+echo "INFO: Restarting podman machine ..."
 podman machine stop
-podman machine start
+podman machine start --no-info
+
+echo "INFO: Verifying that krunkit has the API Remoting library ..."
+virglrenderer_path=$(lsof -c krunkit | grep "$USER" | grep libvirglrenderer | awk '{print $9;}')
+if ! echo "$virglrenderer_path" | grep $(basename $PWD) --color; then
+    echo "ERROR: wrong library loaded :/ ($virglrenderer_path)"
+    exit 1
+fi
 
 if [[ -z "$(which ramalama)" ]]; then
     echo "WARNING: ramalama isn't available ..."
 fi
 
-
 cat <<EOF
-All done!
+INFO: All done!
 
-Try the API Remoting GPU acceleration with RamaLama:
+INFO: Try the API Remoting GPU acceleration with RamaLama:
 \$ export CONTAINERS_MACHINE_PROVIDER=$CONTAINERS_MACHINE_PROVIDER
 \$ ramalama --image $ramalama_image run llama3.2
 EOF
--- projects/mac_ai/testing/scripts/update_krunkit.sh
+++ projects/mac_ai/testing/scripts/update_krunkit.sh
@@ -4,27 +4,124 @@ set -o pipefail
 set -o errexit
 set -o nounset
 set -o errtrace
-set -x
 
-if [[ ! -f podman_start_machine.api_remoting.sh ]]; then
-    echo "ERROR: podman_start_machine.api_remoting.sh not found in the current directory. Please run this in the llama.cpp API remoting release directory."
-    exit 1
-fi
+err_report() {
+    echo "Error on line $1"
+}
 
-cd bin
+trap 'err_report $LINENO' ERR
 
-# just in case they're already there ...
-rm -f krunkit libkrun-efi.dylib
+BREW_KRUNKIT_PATH=/opt/homebrew/bin/krunkit
+PODMAN_KRUNKIT_PATH=/opt/podman/bin/krunkit
 
-krunkit_path=/opt/homebrew/bin/krunkit # the code below does not work with all the krunkit builds, so don't use $(which krunkit)
-libkrun_current_path=$(otool -L "$krunkit_path" | grep libkrun | awk '{print $1;}')
-libvirglrenderer_current_path=$(otool -L "$libkrun_current_path" | grep libvirglrenderer.1.dylib | awk '{print $1;}')
+update_from_brew() {
+    # krunkit installed_from brew
+    krunkit_path=$1
+    if [[ ! -f "$krunkit_path" ]]; then
+        echo "ERROR: podman's krunkit not found, cannot proceed. (path: $krunkit_path)"
+        exit 1
+    fi
 
-cp "$krunkit_path" "$libkrun_current_path" .
+    libkrun_current_path=$(otool -L "$krunkit_path" | grep libkrun | awk '{print $1;}')
+    libvirglrenderer_current_path=$(otool -L "$libkrun_current_path" | grep libvirglrenderer.1.dylib | awk '{print $1;}')
 
-install_name_tool -change "$libkrun_current_path" @executable_path/libkrun-efi.dylib ./krunkit
-install_name_tool -change "$libvirglrenderer_current_path" @executable_path/libvirglrenderer.1.dylib ./libkrun-efi.dylib
+    cd bin
+    echo "INFO: copying krunkit and libkrun locally ..."
+    cp "$krunkit_path" "$libkrun_current_path" .
 
-codesign --force -s - ./krunkit ./libkrun-efi.dylib --entitlements ../entitlement/krunkit.entitlements
+    echo "INFO: updading the library references ..."
+    # krunkit -> libkrun
+    install_name_tool -change "$libkrun_current_path" @executable_path/libkrun-efi.dylib ./krunkit
 
-echo "All done!"
+    # libkrun -> libvirglrenderer
+    install_name_tool -change "$libvirglrenderer_current_path" @executable_path/libvirglrenderer.1.dylib ./libkrun-efi.dylib
+}
+
+update_from_podman() {
+    # krunkit installed from Podman Installer
+    krunkit_path=$1
+    libkrun_current_path=/opt/podman/lib/libkrun-efi.dylib
+
+    cd bin
+    echo "INFO: copying krunkit and libkrun locally ..."
+    cp -v "$krunkit_path" "$libkrun_current_path" .
+
+    echo "INFO: updading the library references ..."
+    # krunkit -> libkrun
+    install_name_tool -change "@rpath/libkrun-efi.dylib" "@executable_path/libkrun-efi.dylib" ./krunkit \
+        2>&1 | (grep -v "invalidate the code signature" || true)
+
+    # libkrun -> libvirglrenderer
+    install_name_tool -change "@rpath/libvirglrenderer.1.dylib" "@executable_path/libvirglrenderer.1.dylib" ./libkrun-efi.dylib
+
+    # libkrun -> self (?)
+    install_name_tool -id $PWD/libkrun-efi.dylib ./libkrun-efi.dylib
+
+    # virglrenderer -> libMoltenVK
+    install_name_tool -change "/opt/homebrew/opt/molten-vk-krunkit/lib/libMoltenVK.dylib" "/opt/podman/lib/libMoltenVK.dylib" ./libvirglrenderer.1.dylib
+
+    # virglrenderer -> libepoxy
+    install_name_tool -change "/opt/homebrew/opt/libepoxy/lib/libepoxy.0.dylib" "/opt/podman/lib/libepoxy.0.dylib" ./libvirglrenderer.1.dylib
+}
+
+
+main() {
+    if [[ ! -f podman_start_machine.api_remoting.sh ]]; then
+        echo "ERROR: podman_start_machine.api_remoting.sh not found in the current directory. Please run this in the llama.cpp API remoting release directory."
+        exit 1
+    fi
+
+    krunkit_path=$(which krunkit)
+
+    if [[ -z "$krunkit_path" ]]; then
+        echo "ERROR: krunkit not available ..."
+        echo "Please install it from brew (slp/krunkit) or via PodMan Installer"
+        exit 1
+    fi
+
+    # just in case they're already there ...
+    rm -f bin/krunkit bin/libkrun-efi.dylib
+
+    pushd $PWD
+    if [[ "$krunkit_path" == "$BREW_KRUNKIT_PATH" ]]; then
+        echo "INFO: krunkit installed from brew ..."
+        echo "$krunkit_path"
+        update_from_brew "$krunkit_path"
+    elif [[ "$krunkit_path" == "$PODMAN_KRUNKIT_PATH" ]]; then
+        echo "INFO: krunkit installed from podman ..."
+        echo "$krunkit_path"
+        update_from_podman "$krunkit_path"
+    else
+        echo "ERROR: krunkit path ($krunkit_path) not recognized ..."
+        echo "Should be installed from brew or PodMan Installer."
+        exit 1
+    fi
+
+    popd >/dev/null
+
+    if [[ ! -f bin/krunkit ]]; then
+        echo "ERROR: ./bin/krunkit hasn't be generated. This isn't expected."
+        exit 1
+    fi
+
+    if [[ ! -f bin/libkrun-efi.dylib ]]; then
+        echo "ERROR: ./bin/libkrun-efi.dylib hasn't be generated. This isn't expected."
+        exit 1
+    fi
+
+    echo "INFO: Regenerating krunkit and libkrun code signatures ..."
+    codesign --force -s - ./bin/krunkit ./bin/libkrun-efi.dylib --entitlements ./entitlement/krunkit.entitlements
+
+    # ensure that krunkit loads
+    echo "INFO Checking that krunkit loads ..."
+    if ! ./bin/krunkit --version; then
+        echo "ERROR: ./bin/krunkit not working :/"
+        exit 1
+    fi
+
+    echo "INFO: All done!"
+
+    echo "INFO: Run ./podman_start_machine.api_remoting.sh to restart the PodMan machine with the API Remoting libraries."
+}
+
+main
