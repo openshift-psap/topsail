f6207f71544ace8462786729a93131bd91e756d7
Kevin Pouget <kpouget@redhat.com>
2025-08-04 16:06:22 +0200

    [mac_ai] testing: scripts: check_podman_machine_status: make more reliable

--- projects/mac_ai/testing/scripts/check_podman_machine_status.sh
+++ projects/mac_ai/testing/scripts/check_podman_machine_status.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+# checks the status of the Podman Machine for API Remoting
+# return values:
+#  0 ==> running with API Remoting support
+# 10 ==> running vfkit VM instead of krunkit
+# 11 ==> krunkit not running
+# 12 ==> krunkit running without API Remoting
+# 2x ==> script cannot run correctly
+
 set -o pipefail
 set -o errexit
 set -o nounset
@@ -14,16 +22,28 @@ if [[ -z "$version" ]]; then
   exit 20
 fi
 
-# don't fail on this one ;-)
-LSOF_VIRGL=$(lsof -c krunkit || true | grep virglrenderer || true)
-
-if [[ "$LSOF_VIRGL" == *"$version"* ]]; then
-  echo "krunkit running with API Remoting support"
-  exit 0
-elif [[ "$LSOF_VIRGL" == *"krunkit"* ]]; then
-  echo "krunkit running WITHOUT API Remoting support"
-  exit 11
-else
-  echo "krunkit not running"
-  exit 12
+if lsof -c vfkit > /dev/null; then
+    echo "PodMan Machine running with VFKit (not compatible with the API Remoting support)"
+    exit 10
 fi
+
+lsof_virgl=$(lsof -c krunkit || true | grep virglrenderer || true)
+
+if [[ -z "$lsof_virgl" ]]; then
+    echo "krunkit not running"
+    exit 11
+fi
+
+# using the version identifier to test the API Remoting support:
+#
+# if the version identifier is found, then we assume that the right
+# krunkit/libkrun/libvirglrenderer are running.
+
+if [[ "$lsof_virgl" != *"$version"* ]]; then
+    echo "krunkit running WITHOUT API Remoting support"
+    exit 12
+fi
+
+echo "krunkit running with API Remoting support"
+
+exit 0
