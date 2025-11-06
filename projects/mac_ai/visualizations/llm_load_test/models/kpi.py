3878fc12771ce040313916ef5281e22b8e8d773f
Kevin Pouget <kpouget@redhat.com>
2025-11-06 09:46:56 +0100

    [mac_ai] visualizations: llm_load_test: models/kpi: don't crash on missing data

--- projects/mac_ai/visualizations/llm_load_test/models/kpi.py
+++ projects/mac_ai/visualizations/llm_load_test/models/kpi.py
@@ -10,21 +10,33 @@ KPIs = {} # populated by the @matbench_models.KPIMetadata decorator
 @matbench_models.HigherBetter
 @matbench_models.KPIMetadata(help="Prompt processing throughput", unit="tokens/s")
 def pp_throughput(lts_payload):
+    if not lts_payload.results.prompt_processing:
+        return None
+
     return lts_payload.results.prompt_processing.throughput
 
 
 @matbench_models.HigherBetter
 @matbench_models.KPIMetadata(help="Token generation throughput", unit="token/s")
 def tg_throughput(lts_payload):
+    if not lts_payload.results.token_generation:
+        return None
+
     return lts_payload.results.token_generation.throughput
 
 @matbench_models.LowerBetter
 @matbench_models.KPIMetadata(help="Median time to first token", unit="ms")
 def llm_load_test_ttft(lts_payload):
+    if not lts_payload.results.llm_load_test:
+        return None
+
     return lts_payload.results.llm_load_test.ttft.median
 
 
 @matbench_models.LowerBetter
 @matbench_models.KPIMetadata(help="Median iter-token latency", unit="ms")
 def llm_load_test_itl(lts_payload):
+    if not lts_payload.results.llm_load_test:
+        return None
+
     return lts_payload.results.llm_load_test.itl.median
